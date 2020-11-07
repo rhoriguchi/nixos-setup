758873d522e40fdcdf9b47388f4a248ade1faadb
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2020-11-07 12:32:27 +0100

    Rename dir

--- configuration/common/pkgs/default.nix
+++ configuration/common/pkgs/default.nix
@@ -1,6 +1,6 @@
 [
   (self: super: {
     mach-nix = import ./mach-nix.nix { pkgs = super; };
-    python3 = import ./python { pkgs = super; };
+    python3 = import ./python3 { pkgs = super; };
   })
 ]
