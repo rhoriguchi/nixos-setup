fe7ef2f5a65bc5ffca3e7e378767037f7b4734a4
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2020-11-09 14:09:15 +0100

    Add missing package to wifi

--- configuration/common/pkgs/python3/wifi.nix
+++ configuration/common/pkgs/python3/wifi.nix
@@ -1,5 +1,19 @@
+# TODO move this to glances
 { pkgs }:
-(pkgs.buildPythonPackage rec {
+let
+  pbkdf2 = pkgs.buildPythonPackage rec {
+    pname = "pbkdf2";
+    version = "1.3";
+    name = "${pname}-${version}";
+
+    doCheck = false;
+
+    src = pkgs.fetchPypi {
+      inherit pname version;
+      sha256 = "0yb99rl2mbsaamj571s1mf6vgniqh23v98k4632150hjkwv9fqxc";
+    };
+  };
+in pkgs.buildPythonPackage rec {
   pname = "wifi";
   version = "0.3.8";
   name = "${pname}-${version}";
@@ -10,4 +24,6 @@
     inherit pname version;
     sha256 = "09kzil9jf783rg2mj7b4wnx1nm1g588wh9k89haj117aj4p0p259";
   };
-})
+
+  propagatedBuildInputs = [ pbkdf2 ];
+}
