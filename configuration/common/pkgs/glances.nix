5d92bf42a5aa68c6199ae2b1c0930ebbb1095216
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2020-11-11 21:30:00 +0100

    Improve glances overlay

--- configuration/common/pkgs/glances.nix
+++ configuration/common/pkgs/glances.nix
@@ -1,9 +1,29 @@
-{ glances, python3Packages, lib, stdenv }:
+{ glances, hddtemp, python3Packages, fetchFromGitHub, lib, stdenv }:
 let
+  py-cpuinfo = python3Packages.py-cpuinfo.overridePythonAttrs (oldAttrs: rec {
+    version = "7.0.0";
+
+    src = fetchFromGitHub {
+      owner = "workhorsy";
+      repo = "py-cpuinfo";
+      rev = "v${version}";
+      sha256 = "10qfaibyb2syiwiyv74l7d97vnmlk079qirgnw3ncklqjs0s3gbi";
+    };
+  });
+
+  pySMART_smartx = python3Packages.buildPythonPackage rec {
+    pname = "pySMART.smartx";
+    version = "0.3.10";
+
+    src = python3Packages.fetchPypi {
+      inherit pname version;
+      sha256 = "16chrzqz3ykpkikfdi71z1g31hm8pij5gs9p6fsxjd6r3awxj1zr";
+    };
+  };
+
   pymdstat = python3Packages.buildPythonPackage rec {
     pname = "pymdstat";
     version = "0.4.2";
-    name = "${pname}-${version}";
 
     src = python3Packages.fetchPypi {
       inherit pname version;
@@ -13,8 +33,17 @@ let
 in with lib;
 glances.overrideAttrs (oldAttrs: {
   # TODO add override for log file location https://glances.readthedocs.io/en/stable/config.html#logging
-  # TODO fix "Missing Python Lib (No module named 'cpuinfo'), Quicklook plugin will not display CPU info"
-  propagatedBuildInputs = with python3Packages;
-    oldAttrs.propagatedBuildInputs ++ [ requests docker ]
+
+  # TODO find solution to only add (docker pySMART_smartx pymdstat requests) and replace py-cpuinfo
+  propagatedBuildInputs = (with python3Packages; [
+    bottle
+    future
+    netifaces
+    psutil
+    pySMART_smartx
+    pysnmp
+    requests
+    setuptools
+  ]) ++ [ py-cpuinfo ] ++ optional stdenv.isLinux hddtemp
     ++ optional stdenv.isLinux pymdstat;
 })
