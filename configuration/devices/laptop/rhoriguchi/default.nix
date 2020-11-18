40b3fac96b5db1f3a4e286083d17443ef9cffc32
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2020-11-18 02:41:38 +0100

    Add language and keyboard config

--- configuration/devices/laptop/rhoriguchi/default.nix
+++ configuration/devices/laptop/rhoriguchi/default.nix
@@ -9,5 +9,20 @@ in {
     isNormalUser = true;
   };
 
+  home-manager.users."${userName}".home = {
+    language = {
+      base = "en_US.UTF-8";
+      numeric = "de_CH.UTF-8";
+      time = "de_CH.UTF-8";
+      collate = "en_US.UTF-8";
+      monetary = "de_CH.UTF-8";
+      messages = "en_US.UTF-8";
+      paper = "de_CH.UTF-8";
+      measurement = "de_CH.UTF-8";
+    };
+
+    keyboard.layout = "ch";
+  };
+
   # TODO make firefox default browser https://askubuntu.com/questions/16621/how-to-set-the-default-browser-from-the-command-line
 }
