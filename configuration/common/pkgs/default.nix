4d7d55711d3490d1bbcceb8168e9de42db4c0fcf
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2020-11-27 15:59:16 +0100

    Remove resilio overlay

--- configuration/common/pkgs/default.nix
+++ configuration/common/pkgs/default.nix
@@ -9,8 +9,6 @@
       super.callPackage ./gnomeExtensions { inherit (super) gnomeExtensions; };
     log2ram = super.callPackage ./log2ram.nix { };
     mal_export = super.callPackage ./mal_export.nix { };
-    resilio-sync =
-      super.callPackage ./resilio-sync.nix { inherit (super) resilio-sync; };
     terraform =
       super.callPackage ./terraform.nix { inherit (super) terraform; };
     tv_time_export = super.callPackage ./tv_time_export.nix { };
