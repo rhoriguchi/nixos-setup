be310642230406031497309a3b688b026f6ff5b5
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2020-12-23 21:37:35 +0100

    Move local config to defafault

--- configuration/common/default.nix
+++ configuration/common/default.nix
@@ -9,6 +9,20 @@ with lib; {
 
   time.timeZone = "Europe/Zurich";
 
+  i18n = {
+    defaultLocale = "en_US.UTF-8";
+
+    extraLocaleSettings = {
+      LC_COLLATE = "en_US.UTF-8";
+      LC_MEASUREMENT = "de_CH.UTF-8";
+      LC_MESSAGES = "en_US.UTF-8";
+      LC_MONETARY = "de_CH.UTF-8";
+      LC_NUMERIC = "de_CH.UTF-8";
+      LC_PAPER = "de_CH.UTF-8";
+      LC_TIME = "de_CH.UTF-8";
+    };
+  };
+
   nixpkgs = {
     config.allowUnfree = true;
     overlays = import ./pkgs;
--- configuration/devices/laptop/rhoriguchi/default.nix
+++ configuration/devices/laptop/rhoriguchi/default.nix
@@ -29,21 +29,6 @@
     isNormalUser = true;
   };
 
-  home-manager.users.rhoriguchi.home = {
-    language = {
-      base = "en_US.UTF-8";
-      numeric = "de_CH.UTF-8";
-      time = "de_CH.UTF-8";
-      collate = "en_US.UTF-8";
-      monetary = "de_CH.UTF-8";
-      messages = "en_US.UTF-8";
-      paper = "de_CH.UTF-8";
-      measurement = "de_CH.UTF-8";
-    };
-
-    keyboard.layout = "ch";
-  };
-
   # TODO set up gnome calendar
   # TODO make firefox default browser https://askubuntu.com/questions/16621/how-to-set-the-default-browser-from-the-command-line
 }
