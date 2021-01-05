ea5b4e8656f72bf88f6a50c07bb08e529e391e19
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2021-01-05 03:00:47 +0100

    Add TODO

--- configuration/devices/laptop/rhoriguchi/default.nix
+++ configuration/devices/laptop/rhoriguchi/default.nix
@@ -25,5 +25,6 @@ in {
     ./theme
   ];
 
+  # TODO make eog default image viewer
   # TODO make firefox default browser https://askubuntu.com/questions/16621/how-to-set-the-default-browser-from-the-command-line
 }
