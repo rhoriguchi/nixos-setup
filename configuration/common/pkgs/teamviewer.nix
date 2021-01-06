fa9c6495155bb6ebb969379c78ded44790b9b57b
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2021-01-06 04:27:49 +0100

    Remove overlay to add icon to teamviewer

--- configuration/common/pkgs/teamviewer.nix
+++ configuration/common/pkgs/teamviewer.nix
@@ -1,13 +1,6 @@
 { teamviewer }:
 teamviewer.overrideAttrs (oldAttrs: {
   installPhase = oldAttrs.installPhase + ''
-    for i in 16 20 24 32 48 256; do
-      size=$i"x"$i
-
-      mkdir -p $out/share/icons/hicolor/$size/apps
-      ln -s $out/share/teamviewer/tv_bin/desktop/teamviewer_$i.png $out/share/icons/hicolor/$size/apps/TeamViewer.png
-    done;
-
     wrapQtApp $out/share/teamviewer/tv_bin/script/teamviewer
   '';
 })
