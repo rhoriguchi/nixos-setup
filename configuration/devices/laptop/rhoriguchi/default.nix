25bf251ff4e3abe4d11a947b2beee4f683057021
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2021-01-06 15:25:33 +0100

    Restructure

--- configuration/devices/laptop/rhoriguchi/default.nix
+++ configuration/devices/laptop/rhoriguchi/default.nix
@@ -1,7 +1,16 @@
 { pkgs, config, ... }:
 let home = config.users.users.rhoriguchi.home;
 in {
-  imports = [ ./dconf ./dotfiles ];
+  imports = [
+    ./alacritty.nix
+    ./dconf
+    ./dircolors
+    ./git
+    ./neofetch
+    ./ssh
+    ./vscode.nix
+    ./zsh.nix
+  ];
 
   # TODO make eog default image viewer
   # TODO make firefox default browser https://askubuntu.com/questions/16621/how-to-set-the-default-browser-from-the-command-line
