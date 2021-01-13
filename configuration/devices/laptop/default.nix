3a3b493c3a620e089d3ee171b6e47941e79edbab
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2021-01-13 01:38:11 +0100

    Install cheese

--- configuration/devices/laptop/default.nix
+++ configuration/devices/laptop/default.nix
@@ -138,7 +138,6 @@ in {
     # https://wiki.archlinux.org/index.php/Desktop_entries#Hide_desktop_entries
     gnome3.excludePackages = (with pkgs; [ gnome-connections gnome-photos ])
       ++ (with pkgs.gnome3; [
-        cheese
         geary
         gnome-calendar
         gnome-characters
