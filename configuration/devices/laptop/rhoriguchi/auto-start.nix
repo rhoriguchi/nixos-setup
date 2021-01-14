507ddf432ea0a111aae369e26033725a3a204b5e
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2021-01-14 17:26:45 +0100

    Add signal to autostart and improve

--- configuration/devices/laptop/rhoriguchi/auto-start.nix
+++ configuration/devices/laptop/rhoriguchi/auto-start.nix
@@ -1,8 +1,10 @@
 { pkgs, ... }: {
-  home-manager.users.rhoriguchi.home.file = {
-    ".config/autostart/org.flameshot.Flameshot.desktop.desktop".source =
-      "${pkgs.flameshot}/share/applications/org.flameshot.Flameshot.desktop";
-    ".config/autostart/megasync.desktop".source =
-      "${pkgs.megasync}/share/applications/megasync.desktop";
+  home-manager.users.rhoriguchi.home.file = with pkgs; {
+    ".config/autostart/${flameshot.pname}.desktop".source =
+      "${flameshot}/share/applications/org.flameshot.Flameshot.desktop";
+    ".config/autostart/${megasync.pname}.desktop".source =
+      "${megasync}/share/applications/megasync.desktop";
+    ".config/autostart/${signal-desktop.pname}.desktop".source =
+      "${signal-desktop}/share/applications/signal-desktop.desktop";
   };
 }
