e632390738806beb09acd431bb99a6861140f3ad
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2021-01-15 13:55:24 +0100

    Simplify terraform overlay

--- configuration/common/pkgs/terraform.nix
+++ configuration/common/pkgs/terraform.nix
@@ -1,13 +1,13 @@
-{ terraform, coreutils, installShellFiles }:
+{ terraform, installShellFiles }:
 terraform.overrideAttrs (oldAttrs: {
   nativeBuildInputs = oldAttrs.nativeBuildInputs ++ [ installShellFiles ];
 
   postInstall = oldAttrs.postInstall + ''
-    ${coreutils}/bin/echo "complete -C $out/bin/terraform terraform" > terraform.bash
+    echo "complete -C $out/bin/terraform terraform" > terraform.bash
     installShellCompletion --bash terraform.bash
 
     # TODO does not work
-    # ${coreutils}/bin/echo "complete -o nospace -C $out/bin/terraform terraform" > terraform.zsh
+    # echo "complete -o nospace -C $out/bin/terraform terraform" > terraform.zsh
     # installShellCompletion --zsh terraform.zsh
   '';
 })
