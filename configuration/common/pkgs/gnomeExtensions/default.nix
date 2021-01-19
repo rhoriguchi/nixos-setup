e50d48e0a533103b9ad1696cdbb92bbbf48c45de
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2021-01-19 02:32:46 +0100

    Add dynamic panel transparency

--- configuration/common/pkgs/gnomeExtensions/default.nix
+++ configuration/common/pkgs/gnomeExtensions/default.nix
@@ -1,4 +1,5 @@
 { gnomeExtensions, callPackage }:
 gnomeExtensions // {
   fuzzy-app-search = callPackage ./fuzzy-app-search { };
+  dynamic-panel-transparency = callPackage ./dynamic-panel-transparency.nix { };
 }
