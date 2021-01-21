59e49833d32f0981534aae4c619d6ce199262e93
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2021-01-21 15:05:59 +0100

    Rename dir

--- configuration/configuration.nix
+++ configuration/configuration.nix
@@ -2,7 +2,7 @@
   imports = [
     ./common
     # ./devices/laptop
-    # ./devices/servers/raspberry-rain-town
+    # ./devices/servers/raspberry-pi-rain-town
     # ./devices/servers/server
   ];
 }
