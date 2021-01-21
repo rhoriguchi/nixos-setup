1604a396f1612917f89fde1b98497cb1f9423fb6
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2021-01-21 14:52:33 +0100

    Improve display link assert

--- configuration/common/pkgs/displaylink/default.nix
+++ configuration/common/pkgs/displaylink/default.nix
@@ -1,4 +1,7 @@
-{ displaylink, evdi }:
+{ displaylink, evdi, stdenv }:
+
 displaylink.overrideAttrs (oldAttrs: {
-  src = assert oldAttrs.version == "5.3.1.34"; ./displaylink_5.3.1.zip;
+  src = assert stdenv.lib.versions.majorMinor oldAttrs.version == "5.3";
+    assert stdenv.lib.versions.patch oldAttrs.version == "1";
+    ./displaylink_5.3.1.zip;
 })
