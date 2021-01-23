4627edff584415223094f823a2edccac6e6f07d9
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2021-01-23 21:15:48 +0100

    Remove .dircolors

--- configuration/devices/laptop/rhoriguchi/default.nix
+++ configuration/devices/laptop/rhoriguchi/default.nix
@@ -5,7 +5,6 @@ in {
     ./accounts-service
     # TODO does not work
     # ./auto-start.nix
-    ./dircolors
     ./flameshot.nix
     ./git
     ./gnome
--- configuration/devices/laptop/rhoriguchi/dircolors/default.nix
+++
@@ -1,7 +0,0 @@
-{ ... }: {
-  # TODO does not work
-  home-manager.users.rhoriguchi.programs.dircolors = {
-    enable = true;
-    extraConfig = builtins.readFile ./dircolors;
-  };
-}
