8f08bd9661469a0f83b90b5ae89602e73123b7ea
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2021-01-26 11:52:02 +0100

    Replace overlay with nixpkgs

--- configuration/common/pkgs/gnomeExtensions/default.nix
+++ configuration/common/pkgs/gnomeExtensions/default.nix
@@ -1,5 +1,4 @@
 { gnomeExtensions, callPackage }:
 gnomeExtensions // {
   fuzzy-app-search = callPackage ./fuzzy-app-search { };
-  dynamic-panel-transparency = callPackage ./dynamic-panel-transparency.nix { };
 }
