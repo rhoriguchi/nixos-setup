62a3806eca7ba3f2e6bc9c2be7a53aa12a30c35b
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2021-03-17 02:15:28 +0100

    Add fan control config

--- configuration/devices/servers/server/default.nix
+++ configuration/devices/servers/server/default.nix
@@ -26,6 +26,28 @@ in {
     wireless.enable = true;
   };
 
+  # hwmon1/pwm(2|3) = cpu fans
+  # hwmon1/pwm4 = case fans
+  # hwmon1/pwm6 = motherboard chip fan
+  # TODO figure out how to control case fans
+  hardware.fancontrol = {
+    enable = true;
+
+    config = ''
+      INTERVAL=1
+      DEVPATH=hwmon0=devices/pci0000:00/0000:00:18.3 hwmon1=devices/platform/nct6775.656
+      DEVNAME=hwmon0=k10temp hwmon1=nct6798
+      FCTEMPS=hwmon1/pwm2=hwmon0/temp2_input hwmon1/pwm3=hwmon0/temp2_input
+      FCFANS=hwmon1/pwm2=hwmon1/fan2_input hwmon1/pwm3=hwmon1/fan3_input
+      MINTEMP=hwmon1/pwm2=65 hwmon1/pwm3=65
+      MAXTEMP=hwmon1/pwm2=80 hwmon1/pwm3=80
+      MINSTART=hwmon1/pwm2=20 hwmon1/pwm3=20
+      MINSTOP=hwmon1/pwm2=25 hwmon1/pwm3=25
+      MINPWM=hwmon1/pwm2=25 hwmon1/pwm3=25
+      MAXPWM=hwmon1/pwm2=160 hwmon1/pwm3=160
+    '';
+  };
+
   # TODO install https://grafana.com/docs/loki/latest/clients/docker-driver/
   virtualisation.docker.enable = true;
 
