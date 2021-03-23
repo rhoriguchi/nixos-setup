3aa02f2a3c29b6e55b838e4acdbda888574451a6
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2021-03-23 02:50:04 +0100

    Remove builtins prefix

--- configuration/channels/home-manager.nix
+++ configuration/channels/home-manager.nix
@@ -1,7 +1,7 @@
 {
   imports = [
     "${
-      builtins.fetchTarball {
+      fetchTarball {
         url =
           "https://github.com/nix-community/home-manager/archive/ddcd476603dfd3388b1dc8234fa9d550156a51f5.tar.gz";
         sha256 = "0amckzfnpldw8hpzgcdp8qqbd91g15dhgaw8mkkb3sghvig0380k";
