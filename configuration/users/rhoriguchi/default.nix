d42967d01f7ac9936642c9f14bb65638a18619c2
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2021-03-25 09:57:24 +0100

    Add a common file to import home-manager and set settings

--- configuration/users/rhoriguchi/default.nix
+++ configuration/users/rhoriguchi/default.nix
@@ -1,12 +1,6 @@
 {
   imports = [
-    "${
-      fetchTarball {
-        url =
-          "https://github.com/nix-community/home-manager/archive/ddcd476603dfd3388b1dc8234fa9d550156a51f5.tar.gz";
-        sha256 = "0amckzfnpldw8hpzgcdp8qqbd91g15dhgaw8mkkb3sghvig0380k";
-      }
-    }/nixos"
+    ../default.nix
 
     ./accounts-service
     ./alacritty.nix
@@ -20,9 +14,4 @@
     ./vscode.nix
     ./zsh.nix
   ];
-
-  home-manager = {
-    useGlobalPkgs = true;
-    useUserPackages = true;
-  };
 }
