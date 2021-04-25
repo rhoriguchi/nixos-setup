dffdb90b66201440def2b3f24fe283c6fe94c930
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2021-04-25 11:43:54 +0200

    Update display ink zip

--- configuration/overlays/displaylink/default.nix
+++ configuration/overlays/displaylink/default.nix
@@ -1,6 +1,6 @@
 { displaylink, evdi, lib }:
 displaylink.overrideAttrs (oldAttrs: {
-  src = assert lib.versions.majorMinor oldAttrs.version == "5.3";
-    assert lib.versions.patch oldAttrs.version == "1";
-    ./displaylink_5.3.1.zip;
+  src = assert lib.versions.majorMinor oldAttrs.version == "5.4";
+    assert lib.versions.patch oldAttrs.version == "0";
+    ./displaylink_5.4.0.zip;
 })
