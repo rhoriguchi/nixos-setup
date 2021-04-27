2a735eb2900bc618af23ed15e80bf3e50db17dd7
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2021-04-27 21:40:19 +0200

    Add TODO

--- configuration/overlays/displaylink/default.nix
+++ configuration/overlays/displaylink/default.nix
@@ -1,5 +1,6 @@
 { displaylink, evdi, lib }:
 displaylink.overrideAttrs (oldAttrs: {
+  # TODO check with derivation hash
   src = assert lib.versions.majorMinor oldAttrs.version == "5.4";
     assert lib.versions.patch oldAttrs.version == "0";
     ./displaylink_5.4.0.zip;
