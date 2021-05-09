6d8cc908fea489f74ce26dbec525ea78d84b64e7
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2021-05-09 22:10:33 +0200

    Add vs code extension for file history

--- configuration/rhoriguchi/vscode.nix
+++ configuration/rhoriguchi/vscode.nix
@@ -35,6 +35,7 @@
       pkgs.vscode-extensions.tyriar.sort-lines
       pkgs.vscode-extensions.vincaslt.highlight-matching-tag
       pkgs.vscode-extensions.wholroyd.jinja
+      pkgs.vscode-extensions.xyz.local-history
     ];
 
     userSettings = {
@@ -58,6 +59,7 @@
         "**/.git-crypt" = true;
         "**/.git" = true;
         "**/.hg" = true;
+        "**/.history" = true;
         "**/.idea" = true;
         "**/.project" = true;
         "**/.settings" = true;
@@ -70,6 +72,7 @@
       "gitlens.advanced.messages"."suppressShowKeyBindingsNotice" = true;
       "gitlens.defaultDateFormat" = "DD.MM.YYYY HH:MM";
       "gitlens.defaultDateShortFormat" = "DD.MM.YYYY";
+      "local-history.dateLocale" = "de-CH";
       "prettier.arrowParens" = "always";
       "prettier.printWidth" = 120;
       "prettier.singleQuote" = true;
