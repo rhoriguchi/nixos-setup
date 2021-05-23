e86654a364654b4ed61aa4319be1dead8bb6f1ea
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2021-05-23 11:46:27 +0200

    Upgrade mach-nix

--- configuration/overlays/default.nix
+++ configuration/overlays/default.nix
@@ -7,8 +7,8 @@
       mach-nix = import (super.fetchFromGitHub {
         owner = "DavHau";
         repo = "mach-nix";
-        rev = "3.2.0";
-        sha256 = "0qhg36l3c1i6p0p2l346fpj9zsh5kl0xpjmyasi1qcn7mbdfjb0m";
+        rev = "3.3.0";
+        sha256 = "105d6b6kgvn8kll639vx5adh5hp4gjcl4bs9rjzzyqz7367wbxj6";
       }) { pkgs = super; };
 
       nixops = (import (super.fetchFromGitHub {
