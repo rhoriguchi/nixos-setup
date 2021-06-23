bf807e223cefb6d4a2974809429e29934356a55b
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2021-06-23 14:44:41 +0200

    Remove nginx for adGuard

--- configuration/devices/headless/raspberry-pi-4-b-8gb/adguard/default.nix
+++ configuration/devices/headless/raspberry-pi-4-b-8gb/adguard/default.nix
@@ -3,26 +3,8 @@
 
   networking.hostName = "XXLPitu-AdGuard";
 
-  services = {
-    nginx = {
-      enable = true;
-
-      recommendedGzipSettings = true;
-      recommendedOptimisation = true;
-      recommendedProxySettings = true;
-      recommendedTlsSettings = true;
-
-      virtualHosts."${config.networking.hostName}" = {
-        forceSSL = true;
-        listen = [{ port = 3000; }];
-        locations."/".proxyPass = "http://localhost:3000";
-        basicAuth.admin = (import ../../../../secrets.nix).services.adguard.password;
-      };
-    };
-
-    # TODO create initial config yaml and than create with nix
-    adguardhome.enable = true;
-  };
+  # TODO create initial config yaml and than create with nix
+  services.adguardhome.enable = true;
 
   networking.firewall = {
     allowedTCPPorts = [ 53 3000 ];
