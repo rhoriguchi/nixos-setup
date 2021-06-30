358d2c6d783d3931f492d55f1548ab2cc7253f48
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2021-06-30 14:17:21 +0200

    Update hardware configuration

--- configuration/devices/headless/raspberry-pi-4-b-8gb/rain-town/hardware-configuration.nix
+++ configuration/devices/headless/raspberry-pi-4-b-8gb/rain-town/hardware-configuration.nix
@@ -14,11 +14,6 @@
     fsType = "ext4";
   };
 
-  fileSystems."/boot" = {
-    device = "/dev/disk/by-uuid/2178-694E";
-    fsType = "vfat";
-  };
-
   fileSystems."/media/Data" = {
     device = "/dev/disk/by-uuid/dbe57b40-53ca-5249-8160-d89e87b5aca6";
     fsType = "ext4";
