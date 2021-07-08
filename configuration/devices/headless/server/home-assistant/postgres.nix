cbd65243624e9955ee22aef8101d9ebdcf802112
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2021-07-08 17:28:01 +0200

    Change postgres port

--- configuration/devices/headless/server/home-assistant/postgres.nix
+++ configuration/devices/headless/server/home-assistant/postgres.nix
@@ -1,9 +1,11 @@
-{ ... }: {
+{ config, ... }: {
   services = {
-    home-assistant.config.recorder.db_url = "postgresql://@/hass";
+    home-assistant.config.recorder.db_url = "postgresql://:${toString config.services.postgresql.port}/hass";
 
     postgresql = {
       enable = true;
+
+      port = 5433;
 
       ensureDatabases = [ "hass" ];
       ensureUsers = [{
