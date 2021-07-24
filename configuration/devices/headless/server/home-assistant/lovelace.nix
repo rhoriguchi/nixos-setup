3e0792434a60384471aff15124b4fe2432aa4486
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2021-07-25 00:21:09 +0200

    Add custom cards to home assistant and add theme

--- configuration/devices/headless/server/home-assistant/lovelace.nix
+++ configuration/devices/headless/server/home-assistant/lovelace.nix
@@ -1,90 +1,170 @@
-{ ... }: {
-  # TODO HOME-ASSISTANT use https://github.com/thomasloven/lovelace-fold-entity-row
-  # - light.entrance
-  #   - light.entrance_window
-  #   - light.entrance_hallway
-  # - sensor.unifi_wifi
-  #   - sensor.unifi_wifi_default
-  #   - sensor.unifi_wifi_guest
-  #   - sensor.unifi_wifi_iot
+{ pkgs, ... }:
+let
+  miniGraphCardVersion = "v0.10.0";
+  miniGraphCard = pkgs.fetchurl {
+    url = "https://github.com/kalkih/mini-graph-card/releases/download/${miniGraphCardVersion}/mini-graph-card-bundle.js";
+    sha256 = "04m8zk6j3hkd2q230j97b6w0f6y1bxqc4xv8ab94a0h9vfji2pl1";
+  };
 
-  services.home-assistant.lovelaceConfig = {
-    title = "Home";
-    views = [{
-      title = "Default";
-      cards = [
-        {
-          type = "thermostat";
-          entity = "climate.netatmo_home";
-        }
-        # TODO HOME-ASSISTANT use https://github.com/kalkih/mini-graph-card
-        {
-          type = "history-graph";
-          title = "Temperature";
-          hours_to_show = 24 * 7;
-          refresh_interval = 60;
-          entities = [
-            {
-              name = "Current temperature";
-              entity = "sensor.netatmo_current_temperature";
-            }
-            {
-              name = "Target temperature";
-              entity = "sensor.netatmo_target_temperature";
-            }
-          ];
-        }
-        {
-          type = "entities";
-          title = "Lights";
-          show_header_toggle = true;
-          entities = [ "light.entrance" "light.entrance_window" "light.entrance_hallway" "light.living_room" ];
-        }
-        {
-          type = "entities";
-          title = "Network";
-          entities = [
-            {
-              name = "Total";
-              entity = "sensor.unifi_total";
-            }
-            {
-              name = "Wired";
-              entity = "sensor.unifi_wired";
-            }
-            {
-              name = "WiFi";
-              entity = "sensor.unifi_wifi";
-            }
-            {
-              name = "63466727";
-              entity = "sensor.unifi_wifi_default";
-            }
-            {
-              name = "63466727-Guest";
-              entity = "sensor.unifi_wifi_guest";
-            }
-            {
-              name = "63466727-IoT";
-              entity = "sensor.unifi_wifi_iot";
-            }
-          ];
-        }
-        {
-          type = "entities";
-          title = "Battery";
-          # TODO HOME-ASSISTANT add myStrom buttons
-          entities = [{
-            name = "Netatmo";
-            entity = "sensor.netatmo_battery";
+  foldEntityRowVersion = "20.0.4";
+  foldEntityRow = pkgs.fetchurl {
+    url = "https://raw.githubusercontent.com/thomasloven/lovelace-fold-entity-row/${foldEntityRowVersion}/fold-entity-row.js";
+    sha256 = "0bpympqgvnbddhi29qgfmnj12j1mhaqh339draxd1gwi54x2r29s";
+  };
+
+  themes = let version = "1.0.3";
+  in pkgs.fetchurl {
+    url = "https://raw.githubusercontent.com/liri/lovelace-themes/${version}/themes/google-home.yaml";
+    sha256 = "0f9z5qc4wjc8npc2xq8h8j1hlff6dn9ax9p0c6nbn04jidp6vpsr";
+  };
+in {
+  systemd.tmpfiles.rules = [
+    "d /run/hass 0700 nginx nginx"
+    "L+ /run/hass/mini-graph-card-bundle.js - - - - ${miniGraphCard}"
+    "L+ /run/hass/fold-entity-row.js - - - - ${foldEntityRow}"
+  ];
+
+  services = {
+    nginx.virtualHosts."home-assistant" = { locations."/local/" = { alias = "/run/hass/"; }; };
+
+    home-assistant = {
+      config = {
+        frontend.themes = "!include ${themes}";
+
+        automation = [{
+          alias = "Set theme at startup";
+          initial_state = true;
+          trigger = {
+            platform = "homeassistant";
+            event = "start";
+          };
+          action = [{
+            service = "frontend.set_theme";
+            data.name = "Google - Light";
           }];
-        }
-        {
-          type = "entities";
-          title = "Persons";
-          entities = [ "person.ryan" "person.giulia" ];
-        }
-      ];
-    }];
+        }];
+      };
+
+      lovelaceConfig = {
+        title = "Home";
+
+        resources = [
+          {
+            url = "/local/mini-graph-card-bundle.js?v=${miniGraphCardVersion}";
+            type = "module";
+          }
+          {
+            url = "/local/fold-entity-row.js?v=${foldEntityRowVersion}";
+            type = "module";
+          }
+        ];
+
+        views = [{
+          title = "Default";
+          cards = [
+            {
+              type = "thermostat";
+              entity = "climate.netatmo_home";
+            }
+            {
+              type = "custom:mini-graph-card";
+              name = "Temperature";
+
+              hours_to_show = 24 * 7;
+              points_per_hour = 0.5;
+              update_interval = 60;
+              line_width = 3;
+              line_color = "var(--mdc-theme-primary)";
+
+              show = {
+                icon = false;
+                fill = false;
+              };
+
+              entities = [{
+                name = "Current temperature";
+                entity = "sensor.netatmo_current_temperature";
+              }];
+            }
+            {
+              # TODO HOME-ASSISTANT toggle is broken
+              type = "entities";
+              title = "Lights";
+              show_header_toggle = true;
+              entities = [
+                {
+                  type = "custom:fold-entity-row";
+                  head = "light.entrance";
+                  entities = [
+                    {
+                      name = "Hallway";
+                      entity = "light.entrance_hallway";
+                    }
+                    {
+                      name = "Window";
+                      entity = "light.entrance_window";
+                    }
+                  ];
+                }
+                "light.living_room"
+              ];
+            }
+            {
+              type = "entities";
+              title = "Network";
+              entities = [
+                {
+                  name = "Total";
+                  entity = "sensor.unifi_total";
+                }
+                { type = "divider"; }
+                {
+                  name = "Wired";
+                  entity = "sensor.unifi_wired";
+                }
+                {
+                  type = "custom:fold-entity-row";
+                  head = {
+                    name = "WiFi";
+                    entity = "sensor.unifi_wifi";
+                  };
+
+                  open = true;
+
+                  entities = [
+                    {
+                      name = "63466727";
+                      entity = "sensor.unifi_wifi_default";
+                    }
+                    {
+                      name = "63466727-Guest";
+                      entity = "sensor.unifi_wifi_guest";
+                    }
+                    {
+                      name = "63466727-IoT";
+                      entity = "sensor.unifi_wifi_iot";
+                    }
+                  ];
+                }
+              ];
+            }
+            {
+              type = "entities";
+              title = "Battery";
+              # TODO HOME-ASSISTANT add myStrom buttons with "custom:fold-entity-row"
+              entities = [{
+                name = "Netatmo";
+                entity = "sensor.netatmo_battery";
+              }];
+            }
+            {
+              type = "entities";
+              title = "Persons";
+              entities = [ "person.ryan" "person.giulia" ];
+            }
+          ];
+        }];
+      };
+    };
   };
 }
