38e8e353177a02750cef042fdcabfe271bd143d5
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2021-07-25 19:49:03 +0200

    Add proxy to adguard

--- configuration/devices/headless/raspberry-pi-4-b-8gb/adguard/default.nix
+++ configuration/devices/headless/raspberry-pi-4-b-8gb/adguard/default.nix
@@ -1,13 +1,38 @@
-{ ... }: {
+{ config, ... }:
+let adGuardPort = 80;
+in {
   imports = [ ../common.nix ./hardware-configuration.nix ];
 
   networking.hostName = "XXLPitu-AdGuard";
 
-  # TODO create initial config yaml and than create with nix
-  services.adguardhome.enable = true;
+  services = {
+    nginx = {
+      enable = true;
+
+      virtualHosts."adguardhome" = {
+        # TODO commented
+        # forceSSL = true;
+        # enableACME = true;
+
+        # TODO only allow request from local network
+
+        listen = [{
+          addr = "0.0.0.0";
+          port = adGuardPort;
+        }];
+
+        locations."/" = {
+          proxyPass = "http://127.0.0.1:${toString config.services.adguardhome.port}";
+          proxyWebsockets = true;
+        };
+      };
+    };
+
+    adguardhome.enable = true;
+  };
 
   networking.firewall = {
-    allowedTCPPorts = [ 53 3000 ];
+    allowedTCPPorts = [ 53 adGuardPort ];
     allowedUDPPorts = [ 53 ];
   };
 }
