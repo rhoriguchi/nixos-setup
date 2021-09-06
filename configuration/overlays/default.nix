6f1cd0dd3364d19e3f4a39340657e03aadc54c0e
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2021-09-06 15:45:30 +0200

    Small variable scoping change

--- configuration/overlays/default.nix
+++ configuration/overlays/default.nix
@@ -1,23 +1,22 @@
 [
-  (self: super:
-    let nur = import (fetchTarball "https://github.com/nix-community/NUR/archive/master.tar.gz") { pkgs = super; };
-    in {
-      firefox-addons = nur.repos.rycee.firefox-addons;
+  (self: super: {
+    firefox-addons = let nur = import (fetchTarball "https://github.com/nix-community/NUR/archive/master.tar.gz") { pkgs = super; };
+    in nur.repos.rycee.firefox-addons;
 
-      mach-nix = import (super.fetchFromGitHub {
-        owner = "DavHau";
-        repo = "mach-nix";
-        rev = "3.3.0";
-        sha256 = "105d6b6kgvn8kll639vx5adh5hp4gjcl4bs9rjzzyqz7367wbxj6";
-      }) { pkgs = super; };
+    mach-nix = import (super.fetchFromGitHub {
+      owner = "DavHau";
+      repo = "mach-nix";
+      rev = "3.3.0";
+      sha256 = "105d6b6kgvn8kll639vx5adh5hp4gjcl4bs9rjzzyqz7367wbxj6";
+    }) { pkgs = super; };
 
-      nixops = (import (super.fetchFromGitHub {
-        owner = "NixOS";
-        repo = "nixops";
-        rev = "45256745cef246dabe1ae8a7d109988f190cd7ef";
-        sha256 = "0ni1v8ppg5cf35gq7nzd50kajxzp5zkbzhf022in0fgbjcprlzr2";
-      })).default;
-    })
+    nixops = (import (super.fetchFromGitHub {
+      owner = "NixOS";
+      repo = "nixops";
+      rev = "45256745cef246dabe1ae8a7d109988f190cd7ef";
+      sha256 = "0ni1v8ppg5cf35gq7nzd50kajxzp5zkbzhf022in0fgbjcprlzr2";
+    })).default;
+  })
   (self: super: {
     discord = super.callPackage ./discord.nix { inherit (super) discord; };
     displaylink = super.callPackage ./displaylink {
