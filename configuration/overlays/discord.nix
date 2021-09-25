cec867dfb9b3fe2b133a81ef7cec8b80d00676d0
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2021-09-25 15:04:56 +0200

    Upgrade discord to version 0.0.16

--- configuration/overlays/discord.nix
+++ configuration/overlays/discord.nix
@@ -1,9 +1,9 @@
 { discord, fetchurl }:
 discord.overrideAttrs (_: rec {
-  version = "0.0.15";
+  version = "0.0.16";
 
   src = fetchurl {
     url = "https://dl.discordapp.net/apps/linux/${version}/discord-${version}.tar.gz";
-    sha256 = "0pn2qczim79hqk2limgh88fsn93sa8wvana74mpdk5n6x5afkvdd";
+    sha256 = "1s9qym58cjm8m8kg3zywvwai2i3adiq6sdayygk2zv72ry74ldai";
   };
 })
