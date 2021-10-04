55769ed19a62f8660ead60256532769e677f7ad1
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2021-10-04 10:36:34 +0200

    Fix autostart config that it works if package is not installed

--- configuration/rhoriguchi/autostart.nix
+++ configuration/rhoriguchi/autostart.nix
@@ -2,18 +2,23 @@
 let packageNames = lib.unique (map (package: package.name) (config.environment.systemPackages ++ config.users.users.rhoriguchi.packages));
 in {
   home-manager.users.rhoriguchi.xdg.configFile = {
-    "autostart/${pkgs.discord.pname}.desktop".source =
-      lib.mkIf (lib.elem pkgs.discord.name packageNames) "${pkgs.discord}/share/applications/discord.desktop";
+    "autostart/${pkgs.discord.pname}.desktop" =
+      lib.mkIf (lib.elem pkgs.discord.name packageNames) { source = "${pkgs.discord}/share/applications/discord.desktop"; };
 
-    "autostart/${pkgs.flameshot.pname}.desktop".text = let
-      content = lib.readFile "${pkgs.flameshot}/share/applications/org.flameshot.Flameshot.desktop";
-      fixedContent = builtins.replaceStrings [ "/usr/bin/flameshot" ] [ "flameshot" ] content;
-    in lib.mkIf (lib.elem pkgs.flameshot.name packageNames) fixedContent;
+    "autostart/${pkgs.flameshot.pname}.desktop" = lib.mkIf (lib.elem pkgs.flameshot.name packageNames) {
+      text = let content = lib.readFile "${pkgs.flameshot}/share/applications/org.flameshot.Flameshot.desktop";
+      in builtins.replaceStrings [ "/usr/bin/flameshot" ] [ "flameshot" ] content;
+    };
 
-    "autostart/${pkgs.protonvpn-gui.pname}.desktop".source =
-      lib.mkIf (lib.elem pkgs.protonvpn-gui.name packageNames) "${pkgs.protonvpn-gui}/share/applications/protonvpn-tray.desktop";
+    "autostart/${pkgs.protonvpn-gui.pname}.desktop" = lib.mkIf (lib.elem pkgs.protonvpn-gui.name packageNames) {
+      source = "${pkgs.protonvpn-gui}/share/applications/protonvpn-tray.desktop";
+    };
 
-    "autostart/${pkgs.signal-desktop.pname}.desktop".source =
-      lib.mkIf (lib.elem pkgs.signal-desktop.name packageNames) "${pkgs.signal-desktop}/share/applications/signal-desktop.desktop";
+    "autostart/${pkgs.signal-desktop.pname}.desktop" = lib.mkIf (lib.elem pkgs.signal-desktop.name packageNames) {
+      source = "${pkgs.signal-desktop}/share/applications/signal-desktop.desktop";
+    };
+
+    "autostart/${pkgs.solaar.pname}.desktop" =
+      lib.mkIf (lib.elem pkgs.solaar.name packageNames) { source = "${pkgs.solaar}/share/applications/solaar.desktop"; };
   };
 }
