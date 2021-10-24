617f20e4fcc7c65f3a5a6dc442f3e66f9f9c2506
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2021-10-24 14:14:13 +0200

    Use python script for adguard

--- configuration/devices/headless/server/home-assistant/adguard.nix
+++ configuration/devices/headless/server/home-assistant/adguard.nix
@@ -1,31 +1,48 @@
 { pkgs, lib, ... }:
 let
-  authorization = (import ../../../../secrets.nix).services.home-assistant.config.adguard.authorization;
+  username = (import ../../../../secrets.nix).services.home-assistant.config.adguard.username;
+  password = (import ../../../../secrets.nix).services.home-assistant.config.adguard.password;
 
   # TODO get hostname from "configuration/devices/headless/raspberry-pi-4-b-8gb/adguard/default.nix"
-  apiUrl = "http://XXLPitu-AdGuard.local/control";
+  hostname = "XXLPitu-AdGuard.local";
 
-  setAdguardProtection = state: ''
-    ${pkgs.curl}/bin/curl --request POST "${apiUrl}/dns_config" \
-      --header "Authorization: Basic ${authorization}" \
-      --data '{"protection_enabled": ${state}}'
+  pythonWithPackages = pkgs.python3.withPackages (pythonPackages: [ pythonPackages.adguardhome ]);
+
+  script = methodCall: pkgs.writeText "adguard_${methodCall}.py" ''
+    import asyncio
+
+    from adguardhome import AdGuardHome
+
+
+    async def get_status():
+        async with AdGuardHome("${hostname}", username="${username}", password="${password}", port=80) as adguard:
+            print(await adguard.protection_enabled())
+
+
+    async def set_status(status):
+        async with AdGuardHome("${hostname}", username="${username}", password="${password}", port=80) as adguard:
+            if status:
+                await adguard.enable_protection()
+            else:
+                await adguard.disable_protection()
+
+
+    if __name__ == "__main__":
+        loop = asyncio.get_event_loop()
+        loop.run_until_complete(${methodCall})
   '';
 in {
   services.home-assistant.config = {
     shell_command = {
-      disable_adguard_protection = setAdguardProtection "false";
-      enable_adguard_protection = setAdguardProtection "true";
+      disable_adguard_protection = "${pythonWithPackages}/bin/python ${script "set_status(False)"}";
+      enable_adguard_protection = "${pythonWithPackages}/bin/python ${script "set_status(True)"}";
     };
 
     sensor = [{
       platform = "command_line";
       name = "AdGuard protection";
       scan_interval = 5;
-      command = ''
-        ${pkgs.curl}/bin/curl "${apiUrl}/status" \
-          --header "Authorization: Basic ${authorization}"
-      '';
-      value_template = "{{ value_json.protection_enabled }}";
+      command = "${pythonWithPackages}/bin/python ${script "get_status()"}";
     }];
 
     switch = [{
