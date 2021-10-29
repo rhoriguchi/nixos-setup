d543ededc7a2bef1a7617e03edfce3ea03352cc0
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2021-10-29 15:29:37 +0200

    Set theme to yaru-light

--- configuration/rhoriguchi/gnome/default.nix
+++ configuration/rhoriguchi/gnome/default.nix
@@ -263,7 +263,7 @@ in {
         window-buttons-placement = "auto";
         window-buttons-theme = "yaru";
       };
-      "org/gnome/shell/extensions/user-theme".name = "Yaru";
+      "org/gnome/shell/extensions/user-theme".name = "Yaru-light";
       "org/gnome/shell/extensions/windowIsReady_Remover".prevent-disable = true;
       "org/gnome/settings-daemon/plugins/media-keys" = {
         area-screenshot = [ ];
