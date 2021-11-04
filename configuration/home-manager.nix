38199c753dce3551203098d99eff5ea22a15111b
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2021-11-04 12:14:30 +0100

    Update home-manager commit version

--- configuration/home-manager.nix
+++ configuration/home-manager.nix
@@ -1,8 +1,8 @@
 {
   imports = [
     (let
-      commit = "275f955db979e069fbe7a1fd7cfcb5ec030a2096";
-      sha256 = "1zjspmxyp4h75z1f51imnzdv54lnjqafdpisinf48bzd1mxlcvar";
+      commit = "70c5b268e10025c70823767f4fb49e240b40151d";
+      sha256 = "09si7wq89qpyzfxgs1862s7lv8sl0ikwx7cy9vavf1hd5y6m13s4";
     in "${
       fetchTarball {
         url = "https://github.com/nix-community/home-manager/archive/${commit}.tar.gz";
