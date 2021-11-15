a953e77f44c84bef13019b3084dc887c518f2cd5
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2021-11-15 11:39:46 +0100

    Update displaylink

--- configuration/overlays/displaylink/default.nix
+++ configuration/overlays/displaylink/default.nix
@@ -1,5 +1,4 @@
 { displaylink, evdi, lib }:
 displaylink.overrideAttrs (oldAttrs: {
-  # TODO remove old zip when updated to 5.4.1
-  src = assert oldAttrs.version == "5.4.0-55.153"; ./displaylink_5.4.0-55.153.zip;
+  src = assert oldAttrs.version == "5.4.1-55.174"; ./displaylink_5.4.1-55.174.zip;
 })
