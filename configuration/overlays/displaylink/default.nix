3ea8a7dc40a96c4e68c6d57dfe177c0ada44b93b
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2021-11-21 19:37:04 +0100

    Format

--- configuration/overlays/displaylink/default.nix
+++ configuration/overlays/displaylink/default.nix
@@ -1,4 +1,2 @@
 { displaylink, evdi, lib }:
-displaylink.overrideAttrs (oldAttrs: {
-  src = assert oldAttrs.version == "5.4.1-55.174"; ./displaylink_5.4.1-55.174.zip;
-})
+displaylink.overrideAttrs (oldAttrs: { src = assert oldAttrs.version == "5.4.1-55.174"; ./displaylink_5.4.1-55.174.zip; })
