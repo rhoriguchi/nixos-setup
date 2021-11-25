cfcf826280d1d014f65b79e474cdd9d3d2edcd2e
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2021-11-25 23:04:09 +0100

    Use corrected tag

--- configuration/overlays/hs/lovelace-module/fold-entity-row.nix
+++ configuration/overlays/hs/lovelace-module/fold-entity-row.nix
@@ -1,7 +1,7 @@
 { stdenv, fetchFromGitHub }:
 stdenv.mkDerivation rec {
   pname = "hs-lovelace-module-fold-entity-row";
-  version = "2.0.12";
+  version = "20.0.12";
 
   src = fetchFromGitHub {
     owner = "thomasloven";
