5835104e96543c38762005a411e80c503c5a48ee
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2021-12-16 23:41:02 +0100

    htop desktop entry set terminal=false

--- configuration/overlays/htop.nix
+++ configuration/overlays/htop.nix
@@ -2,6 +2,7 @@
 htop.overrideAttrs (oldAttrs: {
   postInstall = (oldAttrs.postInstall or "") + ''
     substituteInPlace $out/share/applications/htop.desktop \
+      --replace "Terminal=true" "Terminal=false" \
       --replace "Exec=htop" "Exec=${alacritty}/bin/alacritty -e htop"
   '';
 })
