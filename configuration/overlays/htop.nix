8caedcf0658056e5e07334ee07b83c785705ed4b
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2021-12-28 12:58:25 +0100

    Don't add alacrity to htop derivation

--- configuration/overlays/htop.nix
+++ configuration/overlays/htop.nix
@@ -1,8 +1,8 @@
-{ htop, alacritty }:
+{ htop }:
 htop.overrideAttrs (oldAttrs: {
   postInstall = (oldAttrs.postInstall or "") + ''
     substituteInPlace $out/share/applications/htop.desktop \
       --replace "Terminal=true" "Terminal=false" \
-      --replace "Exec=htop" "Exec=${alacritty}/bin/alacritty -e htop"
+      --replace "Exec=htop" "Exec=alacritty -e htop || htop"
   '';
 })
