c24e9cb3f6e25679a229755fdb7a7f8133c181e0
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2022-01-02 11:46:51 +0100

    Add stuff added by nixops to configurations.nix

--- configuration/configuration.nix
+++ configuration/configuration.nix
@@ -1,7 +1,13 @@
-{
+{ pkgs, ... }: {
   imports = [
     ./common.nix
 
     ./devices/laptop
   ];
+
+  services.openssh.openFirewall = false;
+
+  boot.binfmt.emulatedSystems = [ "aarch64-linux" ];
+
+  environment.systemPackages = [ pkgs.nixopsUnstable ];
 }
