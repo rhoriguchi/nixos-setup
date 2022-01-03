147aa2a999b415a312243af2564e933538ace30d
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2022-01-03 12:19:28 +0100

    Improve .zshrc config creation

--- configuration/devices/headless/common.nix
+++ configuration/devices/headless/common.nix
@@ -55,9 +55,10 @@ in {
     openssh.authorizedKeys.keys = import ../../authorized-keys.nix;
   };
 
-  system.activationScripts.create-zshrc = ''
-    touch ${config.users.users.xxlpitu.home}/.zshrc
-  '';
+  system.activationScripts.createZshrc = lib.mkIf config.programs.zsh.enable (let
+    normalUsers = lib.filter (user: user.isNormalUser == true) (lib.attrValues config.users.users);
+    commands = map (user: "touch ${user.home}/.zshrc") normalUsers;
+  in lib.concatStringsSep "\n" commands);
 
   services.nginx = {
     recommendedGzipSettings = true;
