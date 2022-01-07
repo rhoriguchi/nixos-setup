6f7176f6691cd8f88a81deabbb1d28949defb664
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2022-01-07 11:13:16 +0100

    Formatting

--- configuration/devices/headless/raspberry-pi-4-b-8gb/common.nix
+++ configuration/devices/headless/raspberry-pi-4-b-8gb/common.nix
@@ -4,11 +4,6 @@
   nixpkgs.system = "aarch64-linux";
 
   boot = {
-    kernelPackages = pkgs.linuxPackages_rpi4;
-    kernelParams = [ "8250.nr_uarts=1" "console=ttyAMA0,115200" "console=tty1" "cma=128M" ];
-
-    tmpOnTmpfs = true;
-
     loader = {
       generic-extlinux-compatible.enable = true;
       grub.enable = false;
@@ -18,6 +13,11 @@
         version = 4;
       };
     };
+
+    kernelPackages = pkgs.linuxPackages_rpi4;
+    kernelParams = [ "8250.nr_uarts=1" "console=ttyAMA0,115200" "console=tty1" "cma=128M" ];
+
+    tmpOnTmpfs = true;
   };
 
   hardware.enableRedistributableFirmware = true;
