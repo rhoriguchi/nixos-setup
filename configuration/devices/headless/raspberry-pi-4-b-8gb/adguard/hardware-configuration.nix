3dd9846cb5a09222f9b487934b2b8b684a8e1275
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2022-01-11 19:40:47 +0100

    Update hardware config

--- configuration/devices/headless/raspberry-pi-4-b-8gb/adguard/hardware-configuration.nix
+++ configuration/devices/headless/raspberry-pi-4-b-8gb/adguard/hardware-configuration.nix
@@ -4,7 +4,7 @@
 { config, lib, pkgs, modulesPath, ... }: {
   imports = [ (modulesPath + "/installer/scan/not-detected.nix") ];
 
-  boot.initrd.availableKernelModules = [ ];
+  boot.initrd.availableKernelModules = [ "xhci_pci" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ ];
   boot.extraModulePackages = [ ];
