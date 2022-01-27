30d9bd6ea4414c53d5bdf27c67630ed3e7d0e6fb
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2022-01-27 21:55:50 +0100

    Upgrade fold-entity-row

--- configuration/overlays/hs/lovelace-module/fold-entity-row.nix
+++ configuration/overlays/hs/lovelace-module/fold-entity-row.nix
@@ -1,13 +1,13 @@
 { stdenv, fetchFromGitHub }:
 stdenv.mkDerivation rec {
   pname = "hs-lovelace-module-fold-entity-row";
-  version = "20.0.12";
+  version = "2.0.13";
 
   src = fetchFromGitHub {
     owner = "thomasloven";
     repo = "lovelace-fold-entity-row";
     rev = version;
-    hash = "sha256-Ib2DjNcLucWI/pt4Z+4HVtPxXAJclpFLv+WASIoiWnw=";
+    hash = "sha256-ZwALUVjncSQKBs7n+ixUBd3bUN2vxxC5ZwlLpIbAPnY=";
   };
 
   installPhase = ''
