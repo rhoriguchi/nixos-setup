8964b9941f37811ab9d9bdb0898a395ab978f287
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2022-02-04 13:29:12 +0100

    Upgrade mach-nix

--- configuration/overlays/default.nix
+++ configuration/overlays/default.nix
@@ -3,16 +3,9 @@
     mach-nix = import (super.fetchFromGitHub {
       owner = "DavHau";
       repo = "mach-nix";
-      rev = "3.3.0";
-      hash = "sha256-RvbFjxnnY/+/zEkvQpl85MICmyp9p2EoncjuN80yrYA=";
-    }) {
-      pkgs = super;
-
-      # TODO remove when mach-nix updated
-      # Taken from https://github.com/DavHau/pypi-deps-db/commits/master
-      pypiDataRev = "f251b0ec37b796d7ac4125251d65724427870ef0";
-      pypiDataSha256 = "1zgfn5zy44pm5m8qn4nisdjw35lp94wa5v8g2hn6r46wmx3b11f8";
-    };
+      rev = "3.4.0";
+      hash = "sha256-CJDg/RpZdUVyI3QIAXUqIoYDl7VkxFtNE4JWih0ucKc=";
+    }) { pkgs = super; };
   })
   (self: super:
     let nur = import (fetchTarball "https://github.com/nix-community/NUR/archive/master.tar.gz") { pkgs = super; };
