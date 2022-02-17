1db7e0283f64a4c46036a7c4e539ea429926d955
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2022-02-17 10:09:33 +0100

    Fix hwmon id

--- configuration/devices/headless/server/fancontrol/default.nix
+++ configuration/devices/headless/server/fancontrol/default.nix
@@ -59,7 +59,7 @@
     # grep -H . /sys/class/hwmon/hwmon*/fan*_input
 
     # hwmon2/pwm(2) = cpu fans
-    # hwmon5/pwm(1|2|3|6)   = case fans
+    # hwmon4/pwm(1|2|3|6)   = case fans
     # UNKNOWN               = motherboard chip fan
 
     # hwmon1/temp1 = Tctl (cpu)
@@ -67,16 +67,16 @@
 
     config = ''
       INTERVAL=1
-      DEVPATH=hwmon1=devices/pci0000:00/0000:00:18.3 hwmon2=devices/platform/nct6775.656 hwmon5=devices/pci0000:00/0000:00:01.2/0000:02:00.0/0000:03:08.0/0000:07:00.3/usb3/3-5/3-5.1/3-5.1:1.0/0003:1B1C:0C10.0005
-      DEVNAME=hwmon1=k10temp hwmon2=nct6798 hwmon5=corsaircpro
-      FCTEMPS= hwmon2/pwm2=hwmon1/temp1_input hwmon5/pwm1=hwmon2/temp1_input hwmon5/pwm2=hwmon2/temp1_input hwmon5/pwm3=hwmon2/temp1_input hwmon5/pwm6=hwmon2/temp1_input
-      FCFANS=  hwmon2/pwm2=hwmon2/fan2_input  hwmon5/pwm1=hwmon5/fan1_input  hwmon5/pwm2=hwmon5/fan2_input  hwmon5/pwm3=hwmon5/fan3_input  hwmon5/pwm6=hwmon5/fan6_input
-      MINTEMP= hwmon2/pwm2=40                 hwmon5/pwm1=45                 hwmon5/pwm2=45                 hwmon5/pwm3=45                 hwmon5/pwm6=45
-      MAXTEMP= hwmon2/pwm2=80                 hwmon5/pwm1=60                 hwmon5/pwm2=60                 hwmon5/pwm3=60                 hwmon5/pwm6=60
-      MINSTART=hwmon2/pwm2=20                 hwmon5/pwm1=30                 hwmon5/pwm2=30                 hwmon5/pwm3=30                 hwmon5/pwm6=30
-      MINSTOP= hwmon2/pwm2=25                 hwmon5/pwm1=35                 hwmon5/pwm2=35                 hwmon5/pwm3=35                 hwmon5/pwm6=35
-      MINPWM=  hwmon2/pwm2=25                 hwmon5/pwm1=35                 hwmon5/pwm2=35                 hwmon5/pwm3=35                 hwmon5/pwm6=35
-      MAXPWM=  hwmon2/pwm2=160                hwmon5/pwm1=160                hwmon5/pwm2=160                hwmon5/pwm3=160                hwmon5/pwm6=160
+      DEVPATH=hwmon1=devices/pci0000:00/0000:00:18.3 hwmon2=devices/platform/nct6775.656 hwmon4=devices/pci0000:00/0000:00:01.2/0000:02:00.0/0000:03:08.0/0000:07:00.3/usb3/3-5/3-5.1/3-5.1:1.0/0003:1B1C:0C10.0005
+      DEVNAME=hwmon1=k10temp hwmon2=nct6798 hwmon4=corsaircpro
+      FCTEMPS= hwmon2/pwm2=hwmon1/temp1_input hwmon4/pwm1=hwmon2/temp1_input hwmon4/pwm2=hwmon2/temp1_input hwmon4/pwm3=hwmon2/temp1_input hwmon4/pwm6=hwmon2/temp1_input
+      FCFANS=  hwmon2/pwm2=hwmon2/fan2_input  hwmon4/pwm1=hwmon4/fan1_input  hwmon4/pwm2=hwmon4/fan2_input  hwmon4/pwm3=hwmon4/fan3_input  hwmon4/pwm6=hwmon4/fan6_input
+      MINTEMP= hwmon2/pwm2=40                 hwmon4/pwm1=45                 hwmon4/pwm2=45                 hwmon4/pwm3=45                 hwmon4/pwm6=45
+      MAXTEMP= hwmon2/pwm2=80                 hwmon4/pwm1=60                 hwmon4/pwm2=60                 hwmon4/pwm3=60                 hwmon4/pwm6=60
+      MINSTART=hwmon2/pwm2=20                 hwmon4/pwm1=30                 hwmon4/pwm2=30                 hwmon4/pwm3=30                 hwmon4/pwm6=30
+      MINSTOP= hwmon2/pwm2=25                 hwmon4/pwm1=35                 hwmon4/pwm2=35                 hwmon4/pwm3=35                 hwmon4/pwm6=35
+      MINPWM=  hwmon2/pwm2=25                 hwmon4/pwm1=35                 hwmon4/pwm2=35                 hwmon4/pwm3=35                 hwmon4/pwm6=35
+      MAXPWM=  hwmon2/pwm2=160                hwmon4/pwm1=160                hwmon4/pwm2=160                hwmon4/pwm3=160                hwmon4/pwm6=160
     '';
   };
 }
