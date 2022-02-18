e0a33b93af3bf7273a557217cb93b2fbe9edf12e
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2022-02-18 11:00:05 +0100

    Fix hwmon id

--- configuration/devices/headless/server/fancontrol/default.nix
+++ configuration/devices/headless/server/fancontrol/default.nix
@@ -67,7 +67,7 @@
 
     config = ''
       INTERVAL=1
-      DEVPATH=hwmon1=devices/pci0000:00/0000:00:18.3 hwmon2=devices/platform/nct6775.656 hwmon4=devices/pci0000:00/0000:00:01.2/0000:02:00.0/0000:03:08.0/0000:07:00.3/usb3/3-5/3-5.1/3-5.1:1.0/0003:1B1C:0C10.0005
+      DEVPATH=hwmon1=devices/pci0000:00/0000:00:18.3 hwmon2=devices/platform/nct6775.656 hwmon4=devices/pci0000:00/0000:00:01.2/0000:02:00.0/0000:03:08.0/0000:07:00.3/usb3/3-5/3-5.1/3-5.1:1.0/0003:1B1C:0C10.0002
       DEVNAME=hwmon1=k10temp hwmon2=nct6798 hwmon4=corsaircpro
       FCTEMPS= hwmon2/pwm2=hwmon1/temp1_input hwmon4/pwm1=hwmon2/temp1_input hwmon4/pwm2=hwmon2/temp1_input hwmon4/pwm3=hwmon2/temp1_input hwmon4/pwm6=hwmon2/temp1_input
       FCFANS=  hwmon2/pwm2=hwmon2/fan2_input  hwmon4/pwm1=hwmon4/fan1_input  hwmon4/pwm2=hwmon4/fan2_input  hwmon4/pwm3=hwmon4/fan3_input  hwmon4/pwm6=hwmon4/fan6_input
