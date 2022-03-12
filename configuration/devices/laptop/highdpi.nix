c9c3d6bc57c39e22f6eb5c0379ea849b68a39888
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2022-03-12 13:32:11 +0100

    Use lowPrio function for device specific overrides

--- configuration/devices/laptop/highdpi.nix
+++ configuration/devices/laptop/highdpi.nix
@@ -1,21 +1,21 @@
-{ pkgs, ... }: {
+{ pkgs, lib, ... }: {
   # TODO boot selection is extremely small
 
   nixpkgs.overlays = [
     (self: super: {
-      gimp = super.gimp.overrideAttrs (oldAttrs: {
+      gimp = lib.lowPrio (super.gimp.overrideAttrs (oldAttrs: {
         postInstall = (oldAttrs.postInstall or "") + ''
           substituteInPlace $out/etc/gimp/2.0/gimprc \
             --replace "# (icon-size auto)" "(icon-size huge)"
         '';
-      });
+      }));
 
-      spotify-unwrapped = super.spotify-unwrapped.overrideAttrs (oldAttrs: {
+      spotify-unwrapped = lib.lowPrio (super.spotify-unwrapped.overrideAttrs (oldAttrs: {
         postInstall = (oldAttrs.postInstall or "") + ''
           wrapProgram $out/bin/spotify \
             --add-flags "--force-device-scale-factor=2"
         '';
-      });
+      }));
     })
   ];
 }
