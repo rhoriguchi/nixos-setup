0a731d55d5651d5b9e357e4b9adf705a7a292e20
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2022-04-05 15:00:07 +0200

    Install python 3.10

--- configuration/devices/laptop/default.nix
+++ configuration/devices/laptop/default.nix
@@ -178,6 +178,7 @@ in {
       pkgs.postman
       pkgs.protonvpn-gui
       pkgs.python3
+      pkgs.python310 # TODO remove once python 3.10 is default python3
       pkgs.python3Packages.pip
       pkgs.qbittorrent
       pkgs.signal-desktop
