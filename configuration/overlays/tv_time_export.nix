269ffd098acd419cf71631b4bd67f0a5155288c9
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2022-04-10 11:59:09 +0200

    Fix tv_time_export

--- configuration/overlays/tv_time_export.nix
+++ configuration/overlays/tv_time_export.nix
@@ -9,4 +9,12 @@ mach-nix.buildPythonApplication rec {
     rev = version;
     hash = "sha256-D5u6aat/3nlMZEEDCBdDRomUnS4pmkt46S1nwsy1akI=";
   };
+
+  # TODO remove once requirements get detected correctly
+  requirements = ''
+    beautifulsoup4==4.10.0
+    PyYAML==6.0
+    requests==2.27.1
+    retrying==1.3.3
+  '';
 }
