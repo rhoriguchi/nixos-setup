cacae0549f49e8ff8f962cb90cc3d9e97204f49c
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2022-04-17 18:18:51 +0200

    Extract fancy-motd config

---
+++ configuration/configs/fancy-motd.nix
@@ -0,0 +1,28 @@
+{ pkgs, ... }:
+let
+  fancyMotdConfig = pkgs.writeText "fancy-motd-config" ''
+    # Max width used for components in second column
+    WIDTH=75
+
+    # Services to show
+    declare -A services
+    services["adguardhome"]="AdGuard Home"
+    services["docker"]="Docker"
+    services["fancontrol"]="Fancontrol"
+    services["home-assistant"]="Home Assistant"
+    services["libvirtd"]="libvirtd"
+    services["nginx"]="NGINX"
+    services["plex"]="Plex"
+    services["postgresql"]="PostgreSQL"
+    services["resilio"]="Resilio Sync"
+    services["sshd"]="SSH"
+    services["tautulli"]="Tautulli"
+    services["zfs-zed"]="ZFS Event Daemon"
+  '';
+in {
+  environment.shellInit = ''
+    if (( EUID != 0 )) && [[ $- == *i* ]]; then
+      ${pkgs.fancy-motd}/bin/motd ${fancyMotdConfig}
+    fi
+  '';
+}
--- configuration/devices/headless/common.nix
+++ configuration/devices/headless/common.nix
@@ -1,26 +1,5 @@
-{ pkgs, lib, config, ... }:
-let
-  fancyMotdConfig = pkgs.writeText "fancy-motd-config" ''
-    # Max width used for components in second column
-    WIDTH=75
-
-    # Services to show
-    declare -A services
-    services["adguardhome"]="AdGuard Home"
-    services["docker"]="Docker"
-    services["fancontrol"]="Fancontrol"
-    services["home-assistant"]="Home Assistant"
-    services["libvirtd"]="libvirtd"
-    services["nginx"]="NGINX"
-    services["plex"]="Plex"
-    services["postgresql"]="PostgreSQL"
-    services["resilio"]="Resilio Sync"
-    services["sshd"]="SSH"
-    services["tautulli"]="Tautulli"
-    services["zfs-zed"]="ZFS Event Daemon"
-  '';
-in {
-  imports = [ ../../configs/nginx.nix ];
+{ pkgs, lib, config, ... }: {
+  imports = [ ../../configs/fancy-motd.nix ../../configs/nginx.nix ];
 
   nix.gc = {
     automatic = true;
@@ -34,12 +13,6 @@ in {
     ethernet.macAddress = "permanent";
     wifi.macAddress = "permanent";
   };
-
-  programs.zsh.shellInit = ''
-    if (( EUID != 0 )) && [[ $- == *i* ]]; then
-      ${pkgs.fancy-motd}/bin/motd ${fancyMotdConfig}
-    fi
-  '';
 
   environment.systemPackages = [ pkgs.glances pkgs.htop ];
 
