3842bf090fd7730f2a891b244540909ccffb075b
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2022-04-18 23:25:38 +0200

    Simplify

--- configuration/configs/zsh.nix
+++ configuration/configs/zsh.nix
@@ -2,7 +2,7 @@
   programs.zsh.enable = true;
 
   system.activationScripts.createZshrc = lib.mkIf config.programs.zsh.enable (let
-    normalUsers = lib.filter (user: user.isNormalUser == true) (lib.attrValues config.users.users);
+    normalUsers = lib.filter (user: user.isNormalUser) (lib.attrValues config.users.users);
     commands = map (user: "touch ${user.home}/.zshrc") normalUsers;
   in lib.concatStringsSep "\n" commands);
 
