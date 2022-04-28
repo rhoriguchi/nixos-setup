9f58f75d1cf04e88a003b3f65673c1843fb76639
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2022-04-28 17:34:50 +0200

    Enable auto save in gedit

--- configuration/rhoriguchi/gedit.nix
+++ configuration/rhoriguchi/gedit.nix
@@ -5,6 +5,8 @@
     enable = true;
 
     settings."org/gnome/gedit/preferences/editor" = {
+      auto-save = true;
+      auto-save-interval = 1;
       display-line-numbers = true;
       editor-font = "JetBrainsMono Nerd Font";
       insert-spaces = true;
