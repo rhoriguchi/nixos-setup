12804f1ed8f3df9d4684c6f78303171b921066bc
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2022-05-29 15:58:01 +0200

    Unset all services from fancy-motd

--- configuration/configs/fancy-motd.nix
+++ configuration/configs/fancy-motd.nix
@@ -5,20 +5,7 @@ let
     WIDTH=75
 
     # Services to show
-    declare -A services
-    services["adguardhome"]="AdGuard Home"
-    services["docker"]="Docker"
-    services["factorio"]="Factorio"
-    services["fancontrol"]="Fancontrol"
-    services["home-assistant"]="Home Assistant"
-    services["libvirtd"]="libvirtd"
-    services["nginx"]="NGINX"
-    services["plex"]="Plex"
-    services["postgresql"]="PostgreSQL"
-    services["resilio"]="Resilio Sync"
-    services["sshd"]="SSH"
-    services["tautulli"]="Tautulli"
-    services["zfs-zed"]="ZFS Event Daemon"
+    unset services
   '';
 in {
   environment.shellInit = ''
