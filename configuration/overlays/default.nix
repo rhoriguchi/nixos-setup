2a171be11c4cbe85dcf5002e8930214a9638e7d1
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2022-06-24 14:24:44 +0200

    Add nixops overlay

--- configuration/overlays/default.nix
+++ configuration/overlays/default.nix
@@ -19,6 +19,16 @@
           sha256 = "sha256-JLMti3Y4dW7RFif+BgbYI1Q/wYLEHuw9Xs8d14xbktE=";
         }
       }/pkgs/applications/networking/sync/onedrive") { };
+
+    # TODO remove when merged https://nixpk.gs/pr-tracker.html?pr=178811
+    nixopsUnstable = super.callPackage (import "${
+        super.fetchFromGitHub {
+          owner = "NixOS";
+          repo = "nixpkgs";
+          rev = "ea7336a084a37d99548781ddeb4de41f394ca3f1";
+          sha256 = "sha256-wa/IIm7o93DwRfKF6w5YRI3qSG9T7dxC8iNiisdJA04=";
+        }
+      }/pkgs/applications/networking/cluster/nixops") { };
   })
   (self: super: {
     mach-nix = import (super.fetchFromGitHub {
