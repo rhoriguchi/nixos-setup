54d7bd3ca59a3b0947725b4bc7b55ba1b8940196
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2022-06-24 13:44:46 +0200

    Remove temp overlays

--- configuration/overlays/default.nix
+++ configuration/overlays/default.nix
@@ -1,25 +1,5 @@
 [
   (self: super: {
-    # TODO remove when merged https://nixpk.gs/pr-tracker.html?pr=175497
-    fancy-motd = super.callPackage (import "${
-        super.fetchFromGitHub {
-          owner = "NixOS";
-          repo = "nixpkgs";
-          rev = "43629f3d2e57da46b403b681909c324a7614aa1d";
-          sha256 = "sha256-fqelKP+5oXcMLvdQmXluW8738IZqHups0GezqkgHncs=";
-        }
-      }/pkgs/tools/system/fancy-motd") { };
-
-    # TODO remove when merged https://nixpk.gs/pr-tracker.html?pr=175504
-    tautulli = super.python3Packages.callPackage (import "${
-        super.fetchFromGitHub {
-          owner = "NixOS";
-          repo = "nixpkgs";
-          rev = "e673666c66d4c1491048f6cacfca2b3ad3977356";
-          sha256 = "sha256-21iV/lNT5l14HhjqHtiuQvNtV8k6L1EiafBLmYHxczM=";
-        }
-      }/pkgs/servers/tautulli") { };
-
     # TODO remove when merged https://nixpk.gs/pr-tracker.html?pr=178522
     flameshot = super.libsForQt5.callPackage (import "${
         super.fetchFromGitHub {
