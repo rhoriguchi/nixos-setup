b69249dfcd07ae375445ce2e4be67d4224a4bb7d
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2022-07-17 16:50:28 +0200

    Add extra favorite to nautilus

--- configuration/rhoriguchi/nautilus.nix
+++ configuration/rhoriguchi/nautilus.nix
@@ -2,6 +2,7 @@
   environment.shellAliases.open = "nautilus";
 
   home-manager.users.rhoriguchi.xdg.configFile."gtk-3.0/bookmarks".text = ''
+    file://${config.users.users.rhoriguchi.home}/Downloads/Browser
     ${lib.optionalString config.services.resilio.enable "file://${config.services.resilio.syncPath}"}
   '';
 }
