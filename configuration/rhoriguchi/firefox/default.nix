eae7117a3ff5b9a58c23499a6f27799eb55f475e
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2022-07-22 15:57:48 +0200

    Revert "Make firefox bookmarks idempotent"
    This reverts commit 3cadc8a033f68a2e2ed1ff7fc15580f97a088859.

--- configuration/rhoriguchi/firefox/default.nix
+++ configuration/rhoriguchi/firefox/default.nix
@@ -1,165 +1,167 @@
 { pkgs, lib, config, ... }: {
-  home-manager.users.rhoriguchi.programs.firefox = {
-    enable = true;
+  home-manager.users.rhoriguchi = {
+    home.file.".mozilla/firefox/default/bookmarks.html".source = ./bookmarks.html;
 
-    extensions = [
-      pkgs.firefox-addons.bitwarden
-      pkgs.firefox-addons.bypass-paywalls-clean
-      pkgs.firefox-addons.export-tabs-urls-and-titles
-      pkgs.firefox-addons.facebook-container
-      pkgs.firefox-addons.grammarly
-      pkgs.firefox-addons.https-everywhere
-      pkgs.firefox-addons.metamask
-      pkgs.firefox-addons.octolinker
-      pkgs.firefox-addons.open-in-browser
-      pkgs.firefox-addons.privacy-badger
-      pkgs.firefox-addons.rabattcorner
-      pkgs.firefox-addons.reddit-comment-collapser
-      pkgs.firefox-addons.reddit-enhancement-suite
-      pkgs.firefox-addons.tab-session-manager
-      pkgs.firefox-addons.ublock-origin
-      pkgs.firefox-addons.view-image
-      pkgs.firefox-addons.wappalyzer
-    ];
+    programs.firefox = {
+      enable = true;
 
-    profiles = {
-      default = {
-        id = 0;
+      extensions = [
+        pkgs.firefox-addons.bitwarden
+        pkgs.firefox-addons.bypass-paywalls-clean
+        pkgs.firefox-addons.export-tabs-urls-and-titles
+        pkgs.firefox-addons.facebook-container
+        pkgs.firefox-addons.grammarly
+        pkgs.firefox-addons.https-everywhere
+        pkgs.firefox-addons.metamask
+        pkgs.firefox-addons.octolinker
+        pkgs.firefox-addons.open-in-browser
+        pkgs.firefox-addons.privacy-badger
+        pkgs.firefox-addons.rabattcorner
+        pkgs.firefox-addons.reddit-comment-collapser
+        pkgs.firefox-addons.reddit-enhancement-suite
+        pkgs.firefox-addons.tab-session-manager
+        pkgs.firefox-addons.ublock-origin
+        pkgs.firefox-addons.view-image
+        pkgs.firefox-addons.wappalyzer
+      ];
 
-        settings = {
-          "app.shield.optoutstudies.enabled" = false;
-          "browser.aboutConfig.showWarning" = false;
-          "browser.bookmarks.file" = "${./bookmarks.html}";
-          "browser.bookmarks.restore_default_bookmarks" = false;
-          "browser.bookmarks.showMobileBookmarks" = false;
-          "browser.contentblocking.category" = "standard";
-          "browser.discovery.enabled" = false;
-          "browser.download.autohideButton" = false;
-          "browser.download.dir" = "${config.users.users.rhoriguchi.home}/Downloads/Browser";
-          "browser.download.folderList" = 2;
-          "browser.newtabpage.activity-stream.feeds.section.highlights" = false;
-          "browser.newtabpage.activity-stream.feeds.snippets" = false;
-          "browser.newtabpage.activity-stream.feeds.topsites" = false;
-          "browser.newtabpage.activity-stream.showSearch" = false;
-          "browser.newtabpage.enabled" = false;
-          "browser.places.importBookmarksHTML" = true;
-          "browser.rights.3.shown" = true;
-          "browser.search.region" = "CH";
-          "browser.search.suggest.enabled.private" = true;
-          "browser.startup.homepage" = lib.concatStringsSep "|" [
-            "https://todoist.com/app/today"
-            "https://web.whatsapp.com"
-            "https://mail.google.com/mail/u/0/#inbox"
-            "https://www.tvtime.com/en/to-watch"
-            "https://status.nixos.org"
-            "https://github.com/notifications"
-          ];
-          "browser.startup.page" = 1;
-          "browser.tabs.warnOnClose" = false;
-          "browser.toolbars.bookmarks.showOtherBookmarks" = false;
-          "browser.toolbars.bookmarks.visibility" = "always";
-          "browser.uiCustomization.state" = builtins.toJSON (let
-            bitwarden = "_446900e4-71c2-419f-a6a7-df9c091e268b_-browser-action";
-            bypass-paywalls-clean = "_d133e097-46d9-4ecc-9903-fa6a722a6e0e_-browser-action";
-            export-tabs-urls-and-titles = "_17165bd9-9b71-4323-99a5-3d4ce49f3d75_-browser-action";
-            facebook-container = "_contain-facebook-browser-action";
-            grammarly = "87677a2c52b84ad3a151a4a72f5bd3c4_jetpack-browser-action";
-            https-everywhere = "https-everywhere_eff_org-browser-action";
-            metamask = "webextension_metamask_io-browser-action";
-            privacy-badger = "jid1-mnnxcxisbpnsxq_jetpack-browser-action";
-            tab-session-manager = "tab-session-manager_sienori-browser-action";
-            ublock-origin = "ublock0_raymondhill_net-browser-action";
-            view-image = "_287dcf75-bec6-4eec-b4f6-71948a2eea29_-browser-action";
-            wappalyzer = "wappalyzer_crunchlabz_com-browser-action";
-          in {
-            "placements" = {
-              "widget-overflow-fixed-list" = [ ];
-              "nav-bar" = [
-                "back-button"
-                "forward-button"
-                "stop-reload-button"
-                "home-button"
-                "urlbar-container"
-                "downloads-button"
+      profiles = {
+        default = {
+          id = 0;
+
+          settings = {
+            "app.shield.optoutstudies.enabled" = false;
+            "browser.aboutConfig.showWarning" = false;
+            "browser.bookmarks.restore_default_bookmarks" = false;
+            "browser.bookmarks.showMobileBookmarks" = false;
+            "browser.contentblocking.category" = "standard";
+            "browser.discovery.enabled" = false;
+            "browser.download.autohideButton" = false;
+            "browser.download.dir" = "${config.users.users.rhoriguchi.home}/Downloads/Browser";
+            "browser.download.folderList" = 2;
+            "browser.newtabpage.activity-stream.feeds.section.highlights" = false;
+            "browser.newtabpage.activity-stream.feeds.snippets" = false;
+            "browser.newtabpage.activity-stream.feeds.topsites" = false;
+            "browser.newtabpage.activity-stream.showSearch" = false;
+            "browser.newtabpage.enabled" = false;
+            "browser.rights.3.shown" = true;
+            "browser.search.region" = "CH";
+            "browser.search.suggest.enabled.private" = true;
+            "browser.startup.homepage" = lib.concatStringsSep "|" [
+              "https://todoist.com/app/today"
+              "https://web.whatsapp.com"
+              "https://mail.google.com/mail/u/0/#inbox"
+              "https://www.tvtime.com/en/to-watch"
+              "https://status.nixos.org"
+              "https://github.com/notifications"
+            ];
+            "browser.startup.page" = 1;
+            "browser.tabs.warnOnClose" = false;
+            "browser.toolbars.bookmarks.showOtherBookmarks" = false;
+            "browser.toolbars.bookmarks.visibility" = "always";
+            "browser.uiCustomization.state" = builtins.toJSON (let
+              bitwarden = "_446900e4-71c2-419f-a6a7-df9c091e268b_-browser-action";
+              bypass-paywalls-clean = "_d133e097-46d9-4ecc-9903-fa6a722a6e0e_-browser-action";
+              export-tabs-urls-and-titles = "_17165bd9-9b71-4323-99a5-3d4ce49f3d75_-browser-action";
+              facebook-container = "_contain-facebook-browser-action";
+              grammarly = "87677a2c52b84ad3a151a4a72f5bd3c4_jetpack-browser-action";
+              https-everywhere = "https-everywhere_eff_org-browser-action";
+              metamask = "webextension_metamask_io-browser-action";
+              privacy-badger = "jid1-mnnxcxisbpnsxq_jetpack-browser-action";
+              tab-session-manager = "tab-session-manager_sienori-browser-action";
+              ublock-origin = "ublock0_raymondhill_net-browser-action";
+              view-image = "_287dcf75-bec6-4eec-b4f6-71948a2eea29_-browser-action";
+              wappalyzer = "wappalyzer_crunchlabz_com-browser-action";
+            in {
+              "placements" = {
+                "widget-overflow-fixed-list" = [ ];
+                "nav-bar" = [
+                  "back-button"
+                  "forward-button"
+                  "stop-reload-button"
+                  "home-button"
+                  "urlbar-container"
+                  "downloads-button"
+                  bitwarden
+                  privacy-badger
+                  metamask
+                  export-tabs-urls-and-titles
+                  tab-session-manager
+                  ublock-origin
+                  bypass-paywalls-clean
+                  wappalyzer
+                ];
+                "toolbar-menubar" = [ "menubar-items" ];
+                "TabsToolbar" = [ "tabbrowser-tabs" "new-tab-button" "alltabs-button" ];
+                "PersonalToolbar" = [ "personal-bookmarks" ];
+              };
+              "seen" = [
                 bitwarden
-                privacy-badger
-                metamask
+                bypass-paywalls-clean
                 export-tabs-urls-and-titles
+                facebook-container
+                grammarly
+                https-everywhere
+                metamask
+                privacy-badger
                 tab-session-manager
                 ublock-origin
-                bypass-paywalls-clean
+                view-image
                 wappalyzer
+
+                "developer-button"
               ];
-              "toolbar-menubar" = [ "menubar-items" ];
-              "TabsToolbar" = [ "tabbrowser-tabs" "new-tab-button" "alltabs-button" ];
-              "PersonalToolbar" = [ "personal-bookmarks" ];
-            };
-            "seen" = [
-              bitwarden
-              bypass-paywalls-clean
-              export-tabs-urls-and-titles
-              facebook-container
-              grammarly
-              https-everywhere
-              metamask
-              privacy-badger
-              tab-session-manager
-              ublock-origin
-              view-image
-              wappalyzer
+              "dirtyAreaCache" = [ "nav-bar" "toolbar-menubar" "TabsToolbar" "PersonalToolbar" ];
 
-              "developer-button"
-            ];
-            "dirtyAreaCache" = [ "nav-bar" "toolbar-menubar" "TabsToolbar" "PersonalToolbar" ];
-
-            "currentVersion" = 17;
-            "newElementCount" = 0;
-          });
-          "browser.urlbar.openViewOnFocus" = false;
-          "browser.urlbar.placeholderName" = "Google";
-          "browser.urlbar.placeholderName.private" = "Google";
-          "browser.urlbar.showSearchSuggestionsFirst" = false;
-          "browser.urlbar.suggest.openpage" = false;
-          "devtools.aboutdebugging.collapsibilities.processes" = false;
-          "devtools.accessibility.enabled" = false;
-          "devtools.application.enabled" = false;
-          "devtools.chrome.enabled" = true;
-          "devtools.debugger.remote-enabled" = true;
-          "devtools.memory.enabled" = false;
-          "devtools.performance.enabled" = false;
-          "devtools.performance.popup.intro-displayed" = true;
-          "devtools.screenshot.audio.enabled" = false;
-          "devtools.screenshot.clipboard.enabled" = true;
-          "devtools.styleeditor.enabled" = false;
-          "extensions.activeThemeID" = "default-theme@mozilla.org";
-          "extensions.autoDisableScopes" = 0;
-          "extensions.pocket.enabled" = false;
-          "extensions.ui.extension.hidden" = false;
-          "extensions.ui.locale.hidden" = true;
-          "extensions.update.enabled" = false;
-          "findbar.highlightAll" = true;
-          "layout.spellcheckDefault" = 0;
-          "media.eme.enabled" = true;
-          "network.dns.disablePrefetch" = true;
-          "network.predictor.enabled" = false;
-          "network.prefetch-next" = false;
-          "network.trr.mode" = 2;
-          "pref.downloads.disable_button.edit_actions" = false;
-          "privacy.donottrackheader.enabled" = true;
-          "privacy.trackingprotection.enabled" = true;
-          "privacy.userContext.enabled" = true;
-          "privacy.userContext.extension" = "@contain-facebook";
-          "privacy.userContext.ui.enabled" = true;
-          "services.sync.engine.addons" = false;
-          "services.sync.engine.bookmarks" = true;
-          "services.sync.engine.creditcards" = false;
-          "services.sync.engine.history" = true;
-          "services.sync.engine.passwords" = false;
-          "services.sync.engine.prefs" = false;
-          "services.sync.engine.tabs" = false;
-          "signon.rememberSignons" = false;
-          "toolkit.telemetry.reportingpolicy.firstRun" = false;
-          "trailhead.firstrun.didSeeAboutWelcome" = true;
+              "currentVersion" = 17;
+              "newElementCount" = 0;
+            });
+            "browser.urlbar.openViewOnFocus" = false;
+            "browser.urlbar.placeholderName" = "Google";
+            "browser.urlbar.placeholderName.private" = "Google";
+            "browser.urlbar.showSearchSuggestionsFirst" = false;
+            "browser.urlbar.suggest.openpage" = false;
+            "devtools.aboutdebugging.collapsibilities.processes" = false;
+            "devtools.accessibility.enabled" = false;
+            "devtools.application.enabled" = false;
+            "devtools.chrome.enabled" = true;
+            "devtools.debugger.remote-enabled" = true;
+            "devtools.memory.enabled" = false;
+            "devtools.performance.enabled" = false;
+            "devtools.performance.popup.intro-displayed" = true;
+            "devtools.screenshot.audio.enabled" = false;
+            "devtools.screenshot.clipboard.enabled" = true;
+            "devtools.styleeditor.enabled" = false;
+            "extensions.activeThemeID" = "default-theme@mozilla.org";
+            "extensions.autoDisableScopes" = 0;
+            "extensions.pocket.enabled" = false;
+            "extensions.ui.extension.hidden" = false;
+            "extensions.ui.locale.hidden" = true;
+            "extensions.update.enabled" = false;
+            "findbar.highlightAll" = true;
+            "layout.spellcheckDefault" = 0;
+            "media.eme.enabled" = true;
+            "network.dns.disablePrefetch" = true;
+            "network.predictor.enabled" = false;
+            "network.prefetch-next" = false;
+            "network.trr.mode" = 2;
+            "pref.downloads.disable_button.edit_actions" = false;
+            "privacy.donottrackheader.enabled" = true;
+            "privacy.trackingprotection.enabled" = true;
+            "privacy.userContext.enabled" = true;
+            "privacy.userContext.extension" = "@contain-facebook";
+            "privacy.userContext.ui.enabled" = true;
+            "services.sync.engine.addons" = false;
+            "services.sync.engine.bookmarks" = true;
+            "services.sync.engine.creditcards" = false;
+            "services.sync.engine.history" = true;
+            "services.sync.engine.passwords" = false;
+            "services.sync.engine.prefs" = false;
+            "services.sync.engine.tabs" = false;
+            "signon.rememberSignons" = false;
+            "toolkit.telemetry.reportingpolicy.firstRun" = false;
+            "trailhead.firstrun.didSeeAboutWelcome" = true;
+          };
         };
       };
     };
