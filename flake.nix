87ec4e6ab7f2153b534462b182fe43aeaf8aa06a
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2022-07-29 17:38:44 +0200

    Extract devices

--- flake.nix
+++ flake.nix
@@ -47,56 +47,7 @@
             secrets = import ./configuration/secrets.nix;
           };
         };
-
-        # TODO extract devices to network.nix https://releases.nixos.org/nixops/nixops-1.3.1/manual/manual.html#idm140737318969936
-
-        ###################### Devices ######################
-
-        Laptop = {
-          deployment.targetHost = "127.0.0.1";
-
-          imports = [ ./configuration/devices/laptop ];
-
-          services.openssh.openFirewall = false;
-
-          boot.binfmt.emulatedSystems = [ "aarch64-linux" ];
-        };
-
-        #####################################################
-
-        Server = {
-          deployment.targetHost = "home.00a.ch";
-
-          imports = [ ./configuration/devices/headless/server ];
-        };
-
-        AdGuard = {
-          deployment.targetHost = "xxlpitu-adguard";
-
-          imports = [ ./configuration/devices/headless/raspberry-pi-4-b-8gb/adguard ];
-        };
-
-        JDH-Server = {
-          deployment.targetHost = "jdh-server";
-
-          imports = [ ./configuration/devices/headless/jdh-server ];
-        };
-
-        #####################################################
-
-        Horgen = {
-          deployment.targetHost = "xxlpitu-grimmjow";
-
-          imports = [ ./configuration/devices/headless/raspberry-pi-4-b-8gb/grimmjow ];
-        };
-
-        # TODO commented
-        # Placeholder = {
-        #   deployment.targetHost = "192.168.1.128";
-
-        #   imports = [ ./configuration/devices/headless/raspberry-pi-4-b-8gb/ulquiorra ];
-        # };
-      };
+      } // (import ./network.nix);
     } // flake-utils.lib.eachDefaultSystem
     (system: let pkgs = pkgsFor system; in { devShell = pkgs.mkShell { buildInputs = [ pkgs.nix pkgs.nixopsUnstable ]; }; });
 }
