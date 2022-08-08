ac6fdc7175e3a9fcbbf1fb6ba63608e394d396be
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2022-08-08 21:44:17 +0200

    Remove python 3.10 pinning

--- templates/python.nix
+++ templates/python.nix
@@ -1,7 +1,1 @@
-{ pkgs, ... }: {
-  environment.systemPackages = [
-    pkgs.pipenv
-    pkgs.python310 # TODO remove once python 3.10 is default python3
-    pkgs.python3Packages.pip
-  ];
-}
+{ pkgs, ... }: { environment.systemPackages = [ pkgs.pipenv pkgs.python3 pkgs.python3Packages.pip ]; }
