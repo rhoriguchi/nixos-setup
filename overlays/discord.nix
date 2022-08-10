16446522b8493eb8c35c178f31a6f1d9a5721c9c
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2022-08-10 21:51:10 +0200

    Upgrade discord

--- overlays/discord.nix
+++ overlays/discord.nix
@@ -1,9 +1,9 @@
 { discord, fetchurl }:
 discord.overrideAttrs (_: rec {
-  version = "0.0.18";
+  version = "0.0.19";
 
   src = fetchurl {
     url = "https://dl.discordapp.net/apps/linux/${version}/discord-${version}.tar.gz";
-    hash = "sha256-BBc4n6Q3xuBE13JS3gz/6EcwdOWW57NLp2saOlwOgMI=";
+    hash = "sha256-GfSyddbGF8WA6JmHo4tUM27cyHV5kRAyrEiZe1jbA5A=";
   };
 })
