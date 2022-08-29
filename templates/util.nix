b367b0fe3a70d94a2671844caa3b826333876e7a
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2022-08-29 23:41:01 +0200

    Add nmap to util template

--- templates/util.nix
+++ templates/util.nix
@@ -1,4 +1,4 @@
 { pkgs, ... }: {
   environment.systemPackages =
-    [ pkgs.curl pkgs.deadnix pkgs.file pkgs.jq pkgs.nixfmt pkgs.openssl pkgs.sshpass pkgs.tree pkgs.unzip pkgs.zip ];
+    [ pkgs.curl pkgs.deadnix pkgs.file pkgs.jq pkgs.nixfmt pkgs.nmap pkgs.openssl pkgs.sshpass pkgs.tree pkgs.unzip pkgs.zip ];
 }
