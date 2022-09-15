d8c988bcef11895e37d9f632757fdfda38c3090c
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2022-09-15 14:32:01 +0200

    Add speedtest cli to util template

--- templates/util.nix
+++ templates/util.nix
@@ -1,4 +1,16 @@
 { pkgs, ... }: {
-  environment.systemPackages =
-    [ pkgs.curl pkgs.deadnix pkgs.file pkgs.jq pkgs.nixfmt pkgs.nmap pkgs.openssl pkgs.sshpass pkgs.tree pkgs.unzip pkgs.zip ];
+  environment.systemPackages = [
+    pkgs.curl
+    pkgs.deadnix
+    pkgs.file
+    pkgs.jq
+    pkgs.nixfmt
+    pkgs.nmap
+    pkgs.openssl
+    pkgs.speedtest-cli
+    pkgs.sshpass
+    pkgs.tree
+    pkgs.unzip
+    pkgs.zip
+  ];
 }
