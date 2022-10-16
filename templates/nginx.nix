a224b8d51bf3b94fdb4bb1760ac622ea3da6cfeb
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2022-10-16 22:02:23 +0200

    Improve nginx template

--- templates/nginx.nix
+++ templates/nginx.nix
@@ -1,4 +1,15 @@
-{
+{ lib, config, ... }:
+let
+  nginxEnabled = config.services.nginx.enable;
+  nginxVirtualHostValues = lib.attrValues config.services.nginx.virtualHosts;
+
+  virtualHostsWithAcme = lib.filter (virtualHost: virtualHost.enableACME) nginxVirtualHostValues;
+  openPort80 = nginxEnabled && lib.length virtualHostsWithAcme > 0;
+
+  virtualHostWithSSL = lib.filter (virtualHost: virtualHost.addSSL || virtualHost.enableSSL || virtualHost.forceSSL || virtualHost.onlySSL)
+    nginxVirtualHostValues;
+  openPort443 = nginxEnabled && lib.length virtualHostWithSSL > 0;
+in {
   security.acme = {
     acceptTerms = true;
     defaults.email = "contact@00a.ch";
@@ -10,4 +21,6 @@
     recommendedProxySettings = true;
     recommendedTlsSettings = true;
   };
+
+  networking.firewall.allowedTCPPorts = (lib.optional openPort80 80) ++ (lib.optional openPort443 443);
 }
