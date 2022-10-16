d501ec6c55b97348f311f71009eff0513c2885b5
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2022-10-16 22:02:59 +0200

    Enable ssl for home-assistant.00a.ch

--- configuration/devices/headless/server/home-assistant/nginx.nix
+++ configuration/devices/headless/server/home-assistant/nginx.nix
@@ -5,27 +5,30 @@ in {
     nginx = {
       enable = true;
 
-      # TODO add certificate
-      virtualHosts."home-assistant.00a.ch" = {
-        # TODO HOME-ASSISTANT commented
-        # useACMEHost = null;
+      virtualHosts = let proxyPass = "http://127.0.0.1:${toString config.services.home-assistant.config.http.server_port}";
+      in {
+        "home-assistant.00a.ch" = {
+          enableACME = true;
+          forceSSL = true;
 
-        # enableSSL = true;
-        # forceSSL = true;
+          locations."/" = {
+            inherit proxyPass;
+            proxyWebsockets = true;
+          };
 
-        listen = [{
-          addr = "0.0.0.0";
-          port = homeAssistantPort;
-        }];
-
-        locations."/" = {
-          proxyPass = "http://127.0.0.1:${toString config.services.home-assistant.config.http.server_port}";
-          proxyWebsockets = true;
+          extraConfig = ''
+            proxy_buffering off;
+          '';
         };
 
-        extraConfig = ''
-          proxy_buffering off;
-        '';
+        "${config.networking.hostName}.local" = {
+          listen = [{
+            addr = "0.0.0.0";
+            port = homeAssistantPort;
+          }];
+
+          locations."/".proxyPass = proxyPass;
+        };
       };
     };
 
@@ -35,9 +38,6 @@ in {
       use_x_forwarded_for = true;
     };
   };
-
-  # TODO HOME-ASSISTANT commented
-  # security.acme.certs."home-assistant.00a.ch".dnsProvider = "XXXXXX";
 
   networking.firewall.allowedTCPPorts = [ homeAssistantPort ];
 }
