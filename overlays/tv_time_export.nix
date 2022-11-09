9247eb8f357da3c08435f6a22750f992f7499b34
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2022-11-09 18:17:17 +0100

    Upgrade tv_time_export

--- overlays/tv_time_export.nix
+++ overlays/tv_time_export.nix
@@ -1,12 +1,12 @@
 { mach-nix, fetchFromGitHub }:
 mach-nix.buildPythonApplication rec {
   pname = "tv_time_export";
-  version = "1.0.18";
+  version = "1.0.19";
 
   src = fetchFromGitHub {
     owner = "rhoriguchi";
     repo = pname;
     rev = version;
-    hash = "sha256-s+ggdipaA1g3rj3X7BCzm4GxLjfv/Rb7fE9/XyHYN8I=";
+    hash = "sha256-ev02l0ZTj71i63A9UI3xzCX0o98VFNJwf8+e3Cye5eM=";
   };
 }
