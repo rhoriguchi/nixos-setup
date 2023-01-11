d3293aa83ed36336985a6836336ae8d8f8b3032c
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2023-01-11 10:41:51 +0100

    Upgrade flaresolverr

--- configuration/devices/headless/server/sonarr/prowlarr.nix
+++ configuration/devices/headless/server/sonarr/prowlarr.nix
@@ -26,7 +26,7 @@
   };
 
   virtualisation.oci-containers.containers.flaresolverr = {
-    image = "flaresolverr/flaresolverr:v3.0.1";
+    image = "flaresolverr/flaresolverr:v3.0.2";
 
     ports = [ "8191:8191" ];
 
