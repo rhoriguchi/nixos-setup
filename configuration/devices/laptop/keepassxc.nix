28d52865f0278720e23c5e7dad9324faeaab4ebe
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2023-01-25 00:18:44 +0100

    Remove useless lib.lowPrio

--- configuration/devices/laptop/keepassxc.nix
+++ configuration/devices/laptop/keepassxc.nix
@@ -1,14 +1,14 @@
-{ pkgs, config, lib, ... }:
+{ pkgs, config, ... }:
 let
   script = pkgs.writeShellScript "keepassxc.sh" ''
     ${pkgs.libsecret}/bin/secret-tool lookup KeePass 2FA | keepassxc --pw-stdin "${config.services.resilio.syncPath}/KeePass/2FA.kdbx"
   '';
 
-  keepassxc = lib.lowPrio (pkgs.keepassxc.overrideAttrs (oldAttrs: {
+  keepassxc = pkgs.keepassxc.overrideAttrs (oldAttrs: {
     postInstall = (oldAttrs.postInstall or "") + ''
       substituteInPlace $out/share/applications/org.keepassxc.KeePassXC.desktop \
         --replace "Exec=keepassxc %f" "Exec=${script}" \
         --replace "TryExec=keepassxc" "TryExec=${script}"
     '';
-  }));
+  });
 in { environment.systemPackages = [ keepassxc ]; }
