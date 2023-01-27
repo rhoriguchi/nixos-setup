f6fba90e070afc9d71940a25ebdd5049011ad6c0
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2023-01-27 10:51:35 +0100

    Remove unneded lib.lowPrio

--- modules/profiles/hidpi.nix
+++ modules/profiles/hidpi.nix
@@ -1,23 +1,23 @@
-{ lib, ... }: {
+{
   # TODO boot selection is extremely small
 
   hardware.video.hidpi.enable = true;
 
   nixpkgs.overlays = [
     (_: super: {
-      gimp = lib.lowPrio (super.gimp.overrideAttrs (oldAttrs: {
+      gimp = super.gimp.overrideAttrs (oldAttrs: {
         postInstall = (oldAttrs.postInstall or "") + ''
           substituteInPlace $out/etc/gimp/2.0/gimprc \
             --replace "# (icon-size auto)" "(icon-size huge)"
         '';
-      }));
+      });
 
-      spotify-unwrapped = lib.lowPrio (super.spotify-unwrapped.overrideAttrs (oldAttrs: {
+      spotify-unwrapped = super.spotify-unwrapped.overrideAttrs (oldAttrs: {
         postInstall = (oldAttrs.postInstall or "") + ''
           wrapProgram $out/bin/spotify \
             --add-flags "--force-device-scale-factor=2"
         '';
-      }));
+      });
     })
   ];
 }
