d431996b27d64813fecb8cc494033de63d3194a4
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2023-02-27 21:32:43 +0100

    Start wpa_gui in tray

--- modules/home-manager/autostart.nix
+++ modules/home-manager/autostart.nix
@@ -10,6 +10,8 @@
 
     "autostart/${pkgs.signal-desktop.pname}.desktop".source = "${pkgs.signal-desktop}/share/applications/signal-desktop.desktop";
 
-    "autostart/${pkgs.wpa_supplicant_gui.pname}.desktop".source = "${pkgs.wpa_supplicant_gui}/share/applications/wpa_gui.desktop";
+    "autostart/${pkgs.wpa_supplicant_gui.pname}.desktop".text =
+      let content = lib.readFile "${pkgs.wpa_supplicant_gui}/share/applications/wpa_gui.desktop";
+      in lib.replaceStrings [ "Exec=wpa_gui" ] [ "Exec=wpa_gui -t" ] content;
   };
 }
