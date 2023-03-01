f6e7d85ca9565dbe97a78d368c19c943db7ce490
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2023-03-01 23:59:37 +0100

    Configure spotify

---
+++ modules/home-manager/spotify.nix
@@ -0,0 +1,8 @@
+{
+  xdg.configFile."spotify/Users/r.horiguchi-user/prefs".text = ''
+    audio.crossfade_v2=true
+    audio.crossfade.time_v2=${toString (6 * 1000)}
+    audio.play_bitrate_non_metered_migrated=true
+    ui.track_notifications_enabled=false
+  '';
+}
