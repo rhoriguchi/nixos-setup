a0a5dfdcce119ec9f9d75bb529f57bd9e7051a89
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2023-03-02 16:31:20 +0100

    In override don't use symlinkJoin

--- modules/profiles/hidpi.nix
+++ modules/profiles/hidpi.nix
@@ -5,27 +5,21 @@
 
   nixpkgs.overlays = [
     (_: super: {
-      gimp = super.symlinkJoin {
-        inherit (super.gimp) name;
-        paths = [ super.gimp ];
-
-        postBuild = ''
-          cp --remove-destination $(realpath $out/etc/gimp/2.0/gimprc) $out/etc/gimp/2.0/gimprc
+      gimp = super.gimp.overrideAttrs (oldAttrs: {
+        postInstall = (oldAttrs.postInstall or "") + ''
           substituteInPlace $out/etc/gimp/2.0/gimprc \
             --replace "# (icon-size auto)" "(icon-size huge)"
         '';
-      };
+      });
 
-      spotify = super.symlinkJoin {
-        inherit (super.spotify) name;
-        paths = [ super.spotify ];
-        nativeBuildInputs = [ super.makeWrapper ];
+      spotify = super.spotify.overrideAttrs (oldAttrs: {
+        nativeBuildInputs = oldAttrs.nativeBuildInputs ++ [ super.makeWrapper ];
 
-        postBuild = ''
+        postInstall = (oldAttrs.postInstall or "") + ''
           wrapProgram $out/bin/spotify \
             --add-flags "--force-device-scale-factor=2"
         '';
-      };
+      });
     })
   ];
 }
