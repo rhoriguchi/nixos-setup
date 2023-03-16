b0bae8870c6febfa5bbbe5f9d18f39a1644d71a6
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2023-03-16 17:32:04 +0100

    Make sure gnome extensions are always enabled

--- modules/home-manager/gnome/default.nix
+++ modules/home-manager/gnome/default.nix
@@ -131,6 +131,7 @@ in {
       "org/gnome/shell" = {
         app-picker-layout = [ ];
         disable-extension-version-validation = true;
+        disable-user-extensions = false;
         enabled-extensions =
           [ "launch-new-instance@gnome-shell-extensions.gcampax.github.com" "user-theme@gnome-shell-extensions.gcampax.github.com" ]
           ++ map (extension: if lib.hasAttr "extensionUuid" extension then extension.extensionUuid else extension.uuid) extensions;
