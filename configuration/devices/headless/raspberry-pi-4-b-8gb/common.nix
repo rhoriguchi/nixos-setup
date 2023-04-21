2f65af90f2779d613bca5d8e04de1f284dc180a5
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2023-04-22 01:17:02 +0200

    Add a workaround for raspberries if they only boot if hdmi pluged

--- configuration/devices/headless/raspberry-pi-4-b-8gb/common.nix
+++ configuration/devices/headless/raspberry-pi-4-b-8gb/common.nix
@@ -1,6 +1,10 @@
 { pkgs, ... }: {
   imports = [ ../common.nix ./hardware-configuration.nix ];
 
+  boot.loader.raspberryPi.firmwareConfig = ''
+    hdmi_force_hotplug=1
+  '';
+
   networking.interfaces = {
     eth0.useDHCP = true; # Ethernet
     wlan0.useDHCP = true; # WiFi
