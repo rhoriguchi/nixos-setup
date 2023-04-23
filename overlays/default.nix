9fab1607c12c7f83c12cbdeeaeff568164b5870b
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2023-04-23 21:56:00 +0200

    Update flake.lock

--- overlays/default.nix
+++ overlays/default.nix
@@ -19,26 +19,6 @@
           sha256 = "sha256-u/m0E6r62pm8lC4ftt9yvSx7u5VlvZTvw+3y7r3YMm0=";
         }
       }/pkgs/applications/networking/resilio-sync") { };
-
-    # TODO remove when merged https://nixpk.gs/pr-tracker.html?pr=226916
-    plexRaw = super.python3Packages.callPackage (import "${
-        super.fetchFromGitHub {
-          owner = "NixOS";
-          repo = "nixpkgs";
-          rev = "5294d2a3791b0bdf9f56b6df24bcf9769d629369";
-          sha256 = "sha256-rGrIclG7AyzoHHpA4QIBF+3F3EkyC2BF7LelNY+MofY=";
-        }
-      }/pkgs/servers/plex/raw.nix") { };
-
-    # TODO remove when merged https://nixpk.gs/pr-tracker.html?pr=226927
-    adguardhome = super.callPackage (import "${
-        super.fetchFromGitHub {
-          owner = "NixOS";
-          repo = "nixpkgs";
-          rev = "fb9d3a21e853de64e5a2d89c1386cb4d924abee5";
-          sha256 = "sha256-aLhpOlZRkn1u7YMcvJAKChT5v4yvPtG+fmpiFi+r5L4=";
-        }
-      }/pkgs/servers/adguardhome") { };
   })
   (_: super: {
     discord = super.callPackage ./discord.nix { inherit (super) discord; };
