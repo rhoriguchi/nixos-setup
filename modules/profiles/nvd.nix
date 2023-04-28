fee898301a46c3d808c98c6dc6507b8a5743f078
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2023-04-28 17:01:38 +0200

    Handle changes alias better

--- modules/profiles/nvd.nix
+++ modules/profiles/nvd.nix
@@ -2,6 +2,7 @@
   environment = {
     systemPackages = [ pkgs.nvd ];
 
-    shellAliases."changes" = "nvd diff $(ls -dv /nix/var/nix/profiles/system-*-link | tail -2)";
+    shellAliases."changes" =
+      ''nvd diff $(ls -dv /nix/var/nix/profiles/system-*-link | tail -2) 2> /dev/null || echo "No generations to compare"'';
   };
 }
