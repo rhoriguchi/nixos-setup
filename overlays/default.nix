584a13d87445716e4f5fde124145564658ff71fb
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2023-06-23 03:47:53 +0200

    Update flake.lock

--- overlays/default.nix
+++ overlays/default.nix
@@ -9,36 +9,6 @@
           sha256 = "sha256-VlvxAWOKZ/ih7bxKxD2qfp2S8skGABS90hfmP8aV2w0=";
         }
       }/pkgs/tools/system/fancy-motd") { };
-
-    # TODO remove when merged https://nixpk.gs/pr-tracker.html?pr=238137
-    adguardhome = super.callPackage (import "${
-        super.fetchFromGitHub {
-          owner = "NixOS";
-          repo = "nixpkgs";
-          rev = "d4a2d8b4cdca9b475301e33da3b1970e81e2d24a";
-          sha256 = "sha256-2f6w65Z8VpNFgEZEBgHRXmhcFI3uvfCKQKmWKYcTLJ0=";
-        }
-      }/pkgs/servers/adguardhome") { };
-
-    # TODO remove when merged https://nixpk.gs/pr-tracker.html?pr=238640
-    gitkraken = super.callPackage (import "${
-        super.fetchFromGitHub {
-          owner = "NixOS";
-          repo = "nixpkgs";
-          rev = "d280b8b7b7cd84561a4a2056d79bcb1b569f7a8c";
-          sha256 = "sha256-MidLEiuM8L/2X2TqUmPMCNUCwCPYvPvjcH/qcp/AQN4=";
-        }
-      }/pkgs/applications/version-management/gitkraken") { };
-
-    # TODO remove when merged https://nixpk.gs/pr-tracker.html?pr=238653
-    plexRaw = super.python3Packages.callPackage (import "${
-        super.fetchFromGitHub {
-          owner = "NixOS";
-          repo = "nixpkgs";
-          rev = "692f285ae8567990c3320eb26412cd31b9e582e7";
-          sha256 = "sha256-iYgy0WCEpuUJsNokww3qnMJSjI3pfFDcCdpHBEEJapc=";
-        }
-      }/pkgs/servers/plex/raw.nix") { };
   })
   (_: super: {
     discord = super.callPackage ./discord.nix { inherit (super) discord; };
