cec0913741b9bcb64b28e4a985003aae546b50b1
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2023-09-08 14:20:35 +0200

    Add coreutils to direnv config file

--- modules/home-manager/direnv.nix
+++ modules/home-manager/direnv.nix
@@ -1,4 +1,4 @@
-{
+{ pkgs, ... }: {
   programs.direnv = {
     enable = true;
 
@@ -9,10 +9,10 @@
       : ''${XDG_CACHE_HOME:=$HOME/.cache}
       declare -A direnv_layout_dirs
       direnv_layout_dir() {
-          echo "''${direnv_layout_dirs[$PWD]:=$(
-              echo -n "$XDG_CACHE_HOME"/direnv/layouts/
-              echo -n "$PWD" | sha1sum | cut -d ' ' -f 1
-          )}"
+        echo "''${direnv_layout_dirs[$PWD]:=$(
+          echo -n "$XDG_CACHE_HOME"/direnv/layouts/
+          echo -n "$PWD" | ${pkgs.coreutils}/bin/sha1sum | cut -d ' ' -f 1
+        )}"
       }
     '';
   };
