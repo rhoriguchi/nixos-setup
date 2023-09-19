3f148702108a053d2b7af745668e33e9596f6815
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2023-09-19 16:42:40 +0200

    Add dns entry for local connection from vm

--- configuration/devices/headless/server/libvirtd/default.nix
+++ configuration/devices/headless/server/libvirtd/default.nix
@@ -1,4 +1,4 @@
-{
+{ config, ... }: {
   imports = [ ./guest.nix ./network.nix ./storage.nix ];
 
   security.polkit.enable = true;
@@ -7,5 +7,9 @@
 
   services.py-kms.enable = true;
 
-  networking.hosts."172.16.1.1" = [ "kms.00a.ch" ];
+  networking.hosts."172.16.1.1" = [
+    "${config.networking.hostName}.local"
+
+    "kms.00a.ch"
+  ];
 }
