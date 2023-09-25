28b87cdf21d09554a0c925e8f79bacfa9aa52f73
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2023-09-25 18:02:00 +0200

    Revert "Comment sanlitun in minecraft white list"
    This reverts commit ab899d774609a5daaeb4516307c3dd066d37260a.

--- configuration/devices/headless/server/minecraft-server.nix
+++ configuration/devices/headless/server/minecraft-server.nix
@@ -39,7 +39,7 @@
           nielsx3 = "a2d4ac95-9a30-4f88-946c-05b60cfaf28b"; # Niels
           Papierschorle = "fd048705-cb86-4e31-a251-1a99d5d2483f"; # Ramon
           Probefahrer = "bbc822d0-2ad2-44f1-94c3-e185cf4d74b4"; # Cyrill
-          # sanlitun = "9c645c2c-1a1b-4207-ab9e-1ea0ca75a127"; # Isler
+          sanlitun = "9c645c2c-1a1b-4207-ab9e-1ea0ca75a127"; # Isler
           Sirkii = "5e14c507-16cd-4a0d-84bd-ec82c1792e03"; # Keanu
           ThatOneSlave = "ee4c4772-23d4-4cc9-9f70-092aa1ddc7c2"; # Janik
           XXLPitu = "91469f95-dded-484b-acde-1da375f88aed"; # Ryan
