bc824e7ee66bb9740e4c7c966dd4532ed89b38bf
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2023-10-17 23:57:04 +0200

    Add some more packages to modules

--- modules/profiles/python.nix
+++ modules/profiles/python.nix
@@ -1,1 +1,11 @@
-{ pkgs, ... }: { environment.systemPackages = [ pkgs.pipenv pkgs.poetry pkgs.python3 pkgs.python3Packages.pip ]; }
+{ pkgs, ... }: {
+  environment.systemPackages = [
+    pkgs.pipenv
+    pkgs.poetry
+    pkgs.python3
+    pkgs.python3Packages.flake8
+    pkgs.python3Packages.pip
+    pkgs.python3Packages.pylint
+    pkgs.python3Packages.pytest
+  ];
+}
