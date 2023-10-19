748258e6bae34700a09986ef745a66a66aabbd28
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2023-10-19 22:05:36 +0200

    Add ts-node to javascript package

--- modules/profiles/javascript.nix
+++ modules/profiles/javascript.nix
@@ -1,5 +1,5 @@
 { pkgs, ... }: {
   programs.npm.enable = true;
 
-  environment.systemPackages = [ pkgs.nodejs pkgs.nodePackages.eslint pkgs.nodePackages.prettier pkgs.yarn ];
+  environment.systemPackages = [ pkgs.nodejs pkgs.nodePackages.eslint pkgs.nodePackages.prettier pkgs.nodePackages.ts-node pkgs.yarn ];
 }
