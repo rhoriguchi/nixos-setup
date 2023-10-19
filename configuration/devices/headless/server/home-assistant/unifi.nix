ac1e1184a7a7f247f97e6a86acd66f7ac56c4790
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2023-10-20 00:57:00 +0200

    Fix hs fix network device counter

--- configuration/devices/headless/server/home-assistant/unifi.nix
+++ configuration/devices/headless/server/home-assistant/unifi.nix
@@ -4,38 +4,37 @@
       {
         name = "UniFi total";
         icon = "mdi:cellphone-link";
-        state =
-          "{{ states.device_tracker | list | selectattr('state', 'eq', 'home') | rejectattr('attributes.is_wired', 'eq', Null) | list | length }}";
+        state = "{{ states.device_tracker | selectattr('state', 'eq', 'home') | list | length }}";
       }
       {
         name = "UniFi wired";
         icon = "mdi:cable-data";
         state =
-          "{{ states.device_tracker | list | selectattr('state', 'eq', 'home') | selectattr('attributes.is_wired', 'eq', True) | list | length }}";
+          "{{ states.device_tracker | selectattr('state', 'eq', 'home') | selectattr('attributes.essid', 'eq', Null) | list | length }}";
       }
       {
         name = "UniFi WiFi";
         icon = "mdi:wifi";
         state =
-          "{{ states.device_tracker | list | selectattr('state', 'eq', 'home') | selectattr('attributes.is_wired', 'eq', False) | list | length }}";
+          "{{ states.device_tracker | selectattr('state', 'eq', 'home') | rejectattr('attributes.essid', 'eq', Null) | list | length }}";
       }
       {
         name = "UniFi WiFi default";
         icon = "mdi:wifi";
         state =
-          "{{ states.device_tracker | list | selectattr('state', 'eq', 'home') | selectattr('attributes.essid', 'eq', '63466727') | list | length }}";
+          "{{ states.device_tracker | selectattr('state', 'eq', 'home') | selectattr('attributes.essid', 'eq', '63466727') | list | length }}";
       }
       {
         name = "UniFi WiFi guest";
         icon = "mdi:wifi";
         state =
-          "{{ states.device_tracker | list | selectattr('state', 'eq', 'home') | selectattr('attributes.essid', 'eq', '63466727-Guest') | list | length }}";
+          "{{ states.device_tracker | selectattr('state', 'eq', 'home') | selectattr('attributes.essid', 'eq', '63466727-Guest') | list | length }}";
       }
       {
         name = "UniFi WiFi IoT";
         icon = "mdi:wifi";
         state =
-          "{{ states.device_tracker | list | selectattr('state', 'eq', 'home') | selectattr('attributes.essid', 'eq', '63466727-IoT') | list | length }}";
+          "{{ states.device_tracker | selectattr('state', 'eq', 'home') | selectattr('attributes.essid', 'eq', '63466727-IoT') | list | length }}";
       }
     ];
   }];
