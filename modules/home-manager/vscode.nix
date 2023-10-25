6e6f756339f9433271567fbddeea6360a77b368b
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2023-10-25 19:50:29 +0200

    Update flake.lock

--- modules/home-manager/vscode.nix
+++ modules/home-manager/vscode.nix
@@ -43,7 +43,8 @@
       pkgs.vscode-extensions.johnpapa.vscode-peacock
       pkgs.vscode-extensions.ms-azuretools.vscode-docker
       pkgs.vscode-extensions.ms-python.isort
-      pkgs.vscode-extensions.ms-python.python
+      # TODO fails to build
+      # pkgs.vscode-extensions.ms-python.python
       pkgs.vscode-extensions.ms-python.vscode-pylance
       pkgs.vscode-extensions.ms-vscode-remote.remote-containers
       pkgs.vscode-extensions.pkief.material-icon-theme
