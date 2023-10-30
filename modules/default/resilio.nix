6db818e311969ddcf118aed7aeb8b25a0a38c420
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2023-10-30 14:15:40 +0100

    Revert "Remove ${pkgs.coreutils}/bin/"
    This reverts commit 005d1a0e3bc05fb42d5570aa544687eb7236e483.

--- modules/default/resilio.nix
+++ modules/default/resilio.nix
@@ -255,7 +255,7 @@ in {
                 command = "${pkgs.jq}/bin/jq '${lib.concatStringsSep " | " operations}' ${configFile} > ${runConfigPath}";
               in ''${pkgs.bashInteractive}/bin/sh -c "${command}"'')
 
-              ''mkdir -p "%h/${userSyncPath}" "%h/${userStoragePath}"''
+              ''${pkgs.coreutils}/bin/mkdir -p "%h/${userSyncPath}" "%h/${userStoragePath}"''
             ];
           };
         };
