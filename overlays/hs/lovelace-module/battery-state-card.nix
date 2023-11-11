c5a7420803be53b6fe27f334bce1a46cfcf7b50c
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2023-11-11 02:01:14 +0100

    Small improvement

--- overlays/hs/lovelace-module/battery-state-card.nix
+++ overlays/hs/lovelace-module/battery-state-card.nix
@@ -6,9 +6,10 @@ stdenv.mkDerivation rec {
   src = let
     owner = "maxwroc";
     repo = "battery-state-card";
+    rev = "v${version}";
     hash = "sha256-5QIVcCio0CUIap3MDsJ22ww9QBAbkKXLhan4ICMypNQ=";
   in fetchurl {
-    url = "https://github.com/${owner}/${repo}/releases/download/v${version}/battery-state-card.js";
+    url = "https://github.com/${owner}/${repo}/releases/download/${rev}/battery-state-card.js";
     inherit hash;
   };
 
--- overlays/hs/lovelace-module/mini-graph-card.nix
+++ overlays/hs/lovelace-module/mini-graph-card.nix
@@ -6,9 +6,10 @@ stdenv.mkDerivation rec {
   src = let
     owner = "kalkih";
     repo = "mini-graph-card";
+    rev = "v${version}";
     hash = "sha256-ujWSekx/DRS6fQzDyL79ZKGne5VHJwHBT6c88WECc1I=";
   in fetchurl {
-    url = "https://github.com/${owner}/${repo}/releases/download/v${version}/mini-graph-card-bundle.js";
+    url = "https://github.com/${owner}/${repo}/releases/download/${rev}/mini-graph-card-bundle.js";
     inherit hash;
   };
 
