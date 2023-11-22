307fb04d3eee9f0b75faccd803eee47982bfdff4
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2023-11-22 04:06:45 +0100

    Tmux tweaks

--- modules/home-manager/tmux.nix
+++ modules/home-manager/tmux.nix
@@ -5,7 +5,7 @@
     zsh = {
       shellAliases = let inherit (config.programs.tmux) package;
       in {
-        attach = "${package}/bin/tmux attach-session -t 'default' || ${package}/bin/tmux new-session -s 'default'";
+        attach = "${package}/bin/tmux attach-session -t 'Default' || ${package}/bin/tmux new-session -s 'Default'";
         clear = "clear && ${package}/bin/tmux clear-history 2> /dev/null";
         detach = "${package}/bin/tmux detach-client";
       };
@@ -13,9 +13,9 @@
       initExtra = ''
         if [ "$TMUX" = ''' ]; then
           if [ "$XDG_SESSION_TYPE" = 'tty' ]; then
-            tmux attach-session -t "$(basename $(tty))" || tmux new-session -s "$(basename $(tty))"
+            tmux attach-session -t "TTY $(basename $(tty))" || tmux new-session -s "TTY $(basename $(tty))"
           elif [ "$TERM_PROGRAM" != 'vscode' ] && [ "$TERMINAL_EMULATOR" != 'JetBrains-JediTerm' ]; then
-            tmux attach-session -t 'default' || tmux new-session -s 'default'
+            tmux attach-session -t 'Default' || tmux new-session -s 'Default'
           fi
         fi
       '';
@@ -44,10 +44,18 @@
         set -g message-style bg='${colors.normal.green}',fg='${colors.normal.black}'
         set -g mode-style 'reverse'
         set -g pane-active-border-style fg='${colors.normal.accent}'
-        set -g status-bg '${colors.normal.accent}'
-        set -g status-fg '${colors.normal.black}'
+        set -g status-bg '#3d3d3d'
+        set -g status-fg '${colors.normal.accent}'
         set -g status-interval 1
-        set -g status-right ""
+        set -g status-justify 'centre'
+        set -g status-left '#{?#{==:#S,Default},,[#S]}'
+        set -g status-left-length 20
+        set -g status-right '''
+        set -g window-status-current-format ' #I:#W '
+        set -g window-status-current-style bg='${colors.normal.accent}',fg='#3d3d3d'
+        set -g window-status-format ' #I:#W '
+        set -g window-status-separator '''
+        set -g window-status-style bg='#3d3d3d',fg='${colors.normal.accent}'
       '';
     };
   };
