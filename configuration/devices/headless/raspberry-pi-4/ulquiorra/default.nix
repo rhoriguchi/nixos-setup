bac7885bf4e58046225ac81c958b594836e9e473
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2023-11-22 22:37:54 +0100

    Update ulquiorra disk uuid

--- configuration/devices/headless/raspberry-pi-4/ulquiorra/default.nix
+++ configuration/devices/headless/raspberry-pi-4/ulquiorra/default.nix
@@ -4,7 +4,7 @@
   networking.hostName = "XXLPitu-Ulquiorra";
 
   fileSystems."/mnt/Data" = {
-    device = "/dev/disk/by-uuid/f7653cf6-815e-4b4a-8376-5ecbeaf35ae8";
+    device = "/dev/disk/by-uuid/5d51a426-a305-4158-8cc6-ae907145df64";
     fsType = "ext4";
     options = [ "defaults" "errors=continue" "nofail" ];
   };
