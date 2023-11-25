31964193418ba84c3b1ad8c5e551f91f5c6d9fb1
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2023-11-25 01:48:53 +0100

    Update flake.lock

--- configuration/devices/headless/server/home-assistant/postgres.nix
+++ configuration/devices/headless/server/home-assistant/postgres.nix
@@ -1,4 +1,4 @@
-{ lib, config, ... }: {
+{ config, ... }: {
   services = {
     home-assistant.config.recorder.db_url = "postgresql://:${toString config.services.postgresql.port}/hass";
 
@@ -8,14 +8,8 @@
       ensureDatabases = [ "hass" ];
       ensureUsers = [{
         name = "hass";
-        ensurePermissions."DATABASE hass" = "ALL PRIVILEGES";
+        ensureDBOwnership = true;
       }];
     };
   };
-
-  # TODO remove when fixed https://github.com/NixOS/nixpkgs/issues/216989
-  # workaround from https://github.com/NixOS/nixpkgs/pull/262741
-  systemd.services.postgresql.postStart = lib.mkAfter ''
-    $PSQL -tAc 'ALTER DATABASE hass OWNER TO hass;'
-  '';
 }
