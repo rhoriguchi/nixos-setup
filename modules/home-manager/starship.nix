cb5b5da58ca07b8c879e7bc39c52d1a2fca617d2
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2023-12-16 02:54:12 +0100

    Starship improvment

--- modules/home-manager/starship.nix
+++ modules/home-manager/starship.nix
@@ -6,7 +6,7 @@
       add_newline = false;
       scan_timeout = 10;
 
-      format = lib.concatStrings [ "$nix_shell" "$directory" "$git_state" "$git_branch" "$character" ];
+      format = lib.concatStrings [ "$nix_shell" "$directory" "$git_state" "$git_branch" "$git_status" "$character" ];
 
       nix_shell = {
         disabled = false;
@@ -26,6 +26,17 @@
       git_state.style = "${colors.normal.green} bold";
 
       git_branch.style = "${colors.normal.accent} bold";
+
+      git_status = {
+        disabled = false;
+
+        format = "[$ahead_behind]($style)";
+        style = "${colors.normal.red} bold";
+
+        ahead = "⇡$count ";
+        behind = "⇣$count ";
+        diverged = "⇡$ahead_count ⇣$behind_count ";
+      };
 
       character = {
         success_symbol = "[❯](${colors.normal.white} bold)";
