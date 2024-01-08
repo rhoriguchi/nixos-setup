6b16b5bb69e8d6a673e0854f8c95c105e1900ec7
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2024-01-09 00:00:58 +0100

    Upgrade battery-state-card

--- overlays/hs/lovelace-module/battery-state-card.nix
+++ overlays/hs/lovelace-module/battery-state-card.nix
@@ -1,13 +1,13 @@
 { stdenv, fetchurl }:
 stdenv.mkDerivation rec {
   pname = "hs-lovelace-module-battery-state-card";
-  version = "3.1.0";
+  version = "3.1.2";
 
   src = let
     owner = "maxwroc";
     repo = "battery-state-card";
     rev = "v${version}";
-    hash = "sha256-n0qQUZdYuF1GB8vFsAYyv9zVOLQ0wmM+F89hncrnJY0=";
+    hash = "sha256-kNlFvBY3cvzqQz4nOeI8gvhZKt1/LT52jeL6fNB1faU=";
   in fetchurl {
     url = "https://github.com/${owner}/${repo}/releases/download/${rev}/battery-state-card.js";
     inherit hash;
