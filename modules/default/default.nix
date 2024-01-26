cf6430de66150d0e442ec441a97039acd835b9a4
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2024-01-26 13:04:31 +0100

    Improve import filter

--- modules/default/default.nix
+++ modules/default/default.nix
@@ -1,5 +1,10 @@
 { lib, ... }:
 let
   getFiles = dir: lib.attrNames (builtins.readDir dir);
-  getImports = dir: map (file: dir + "/${file}") (lib.filter (file: !(lib.hasSuffix ".md" file) && file != "default.nix") (getFiles dir));
+  filter = file:
+    if lib.pathIsDirectory file then
+      builtins.elem "default.nix" (getFiles file)
+    else
+      lib.hasSuffix ".nix" file && !(lib.hasSuffix "default.nix" file);
+  getImports = dir: lib.filter filter (map (file: dir + "/${file}") (getFiles dir));
 in { imports = getImports ./.; }
