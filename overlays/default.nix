a3f993687051718c9e0b042bb80ad194e9b409ef
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2024-02-19 12:31:19 +0100

    Update flake.lock

--- overlays/default.nix
+++ overlays/default.nix
@@ -19,26 +19,6 @@
           sha256 = "sha256-z+1bm/Qs3WQTlz50ZD0L5GxxhFQJnYqotfHFF2EXtag=";
         }
       }/pkgs/servers/tautulli") { };
-
-    # TODO remove when merged https://nixpk.gs/pr-tracker.html?pr=288529
-    prowlarr = super.callPackage (import "${
-        super.fetchFromGitHub {
-          owner = "NixOS";
-          repo = "nixpkgs";
-          rev = "cc54759e5f3ea6213cadca42b5e2b10ac89a3395";
-          sha256 = "sha256-pjZxaMbgulHN4F5C5HSORBMCEwez7zTs3AEAJXl04fc=";
-        }
-      }/pkgs/servers/prowlarr") { };
-
-    # TODO remove when merged https://nixpk.gs/pr-tracker.html?pr=288664
-    plexRaw = super.python3Packages.callPackage (import "${
-        super.fetchFromGitHub {
-          owner = "NixOS";
-          repo = "nixpkgs";
-          rev = "4b8b97e3dd4c386cb469fe6b8559a10e2dc33f80";
-          sha256 = "sha256-yE7YVlnDD6/byHB+4V+HEyWqY8m4+c5ZAIVEltCn5Gg=";
-        }
-      }/pkgs/servers/plex/raw.nix") { };
   })
   (_: super: {
     discord = super.callPackage ./discord.nix { inherit (super) discord; };
