4298b5bce0821258ba9b5ef17693ead2c03b28b8
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2024-03-18 23:54:43 +0100

    Remove merged overlays

--- overlays/default.nix
+++ overlays/default.nix
@@ -9,26 +9,6 @@
           hash = "sha256-VlvxAWOKZ/ih7bxKxD2qfp2S8skGABS90hfmP8aV2w0=";
         }
       }/pkgs/tools/system/fancy-motd") { };
-
-    # TODO remove when merged https://nixpk.gs/pr-tracker.html?pr=272722
-    tautulli = super.python3Packages.callPackage (import "${
-        super.fetchFromGitHub {
-          owner = "NixOS";
-          repo = "nixpkgs";
-          rev = "8d06988a1a76f17d7f9df9a81c3a32f04550990e";
-          sha256 = "sha256-z+1bm/Qs3WQTlz50ZD0L5GxxhFQJnYqotfHFF2EXtag=";
-        }
-      }/pkgs/servers/tautulli") { };
-
-    # TODO remove when merged https://nixpk.gs/pr-tracker.html?pr=222640
-    sonarr = super.callPackage (import "${
-        super.fetchFromGitHub {
-          owner = "NixOS";
-          repo = "nixpkgs";
-          rev = "45f5989acdfa4e42f24f1fc0f8d86d1088e52e7a";
-          sha256 = "sha256-s/ph/PGVGeG6hO4bIHDoIrOx9HEjKQcEyXdkMNtgBz4=";
-        }
-      }/pkgs/servers/sonarr") { };
   })
   (_: super: {
     discord = super.callPackage ./discord.nix { inherit (super) discord; };
