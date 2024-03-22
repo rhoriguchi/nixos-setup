144eef495379073e07eaeaf9733bdf8ed58c77cf
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2024-03-22 23:18:48 +0100

    Add prowlarr overlay

--- overlays/default.nix
+++ overlays/default.nix
@@ -9,6 +9,16 @@
           hash = "sha256-VlvxAWOKZ/ih7bxKxD2qfp2S8skGABS90hfmP8aV2w0=";
         }
       }/pkgs/tools/system/fancy-motd") { };
+
+    # TODO remove when merged https://nixpk.gs/pr-tracker.html?pr=296914
+    prowlarr = super.callPackage (import "${
+        super.fetchFromGitHub {
+          owner = "NixOS";
+          repo = "nixpkgs";
+          rev = "d044fad77ef709c881df343de61c92cfcb1d2337";
+          sha256 = "sha256-jCH//q53yADNY2AEjtJ1bMQPrva7saA1BfcY8zJYrSM=";
+        }
+      }/pkgs/servers/prowlarr") { };
   })
   (_: super: {
     discord = super.callPackage ./discord.nix { inherit (super) discord; };
