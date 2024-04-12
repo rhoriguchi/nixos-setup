61c7b9fac1c148b83d5b7c2d671ad606b38816fd
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2024-04-12 19:35:11 +0200

    Rename variable

--- modules/home-manager/git/template.nix
+++ modules/home-manager/git/template.nix
@@ -21,13 +21,13 @@
       readonly COMMIT_MSG_FILE="$1"
 
       function check_commit_msg_length {
-          readonly MAX_MSG_LENGTH=72
+          readonly MAX_TITLE_LENGTH=72
 
           local title=$(${pkgs.coreutils}/bin/head -n 1 "$COMMIT_MSG_FILE")
 
-          if [ ''${#title} -gt $MAX_MSG_LENGTH ]; then
+          if [ ''${#title} -gt $MAX_TITLE_LENGTH ]; then
               # TODO figure out how to use hex colors variable
-              echo -e "\x1b[1;38;5;203mCommit title is ''${#title} characters long, must be equal or shorter than $MAX_MSG_LENGTH characters!\e[0m";
+              echo -e "\x1b[1;38;5;203mCommit title is ''${#title} characters long, must be equal or shorter than $MAX_TITLE_LENGTH characters!\e[0m";
               exit 1
           fi
       }
