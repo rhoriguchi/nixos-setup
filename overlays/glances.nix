d770f02da3ac4b2495a09fafddb367759178666c
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2024-05-16 21:17:30 +0200

    Apply patch to fix pysmart

--- overlays/glances.nix
+++ overlays/glances.nix
@@ -1,4 +1,4 @@
-{ glances, formats, lib, stdenv, python3Packages, makeWrapper }:
+{ glances, formats, lib, stdenv, fetchpatch, python3Packages, makeWrapper }:
 let
   configFile = (formats.ini { }).generate "glances.conf" {
     connections.disable = false;
@@ -23,14 +23,27 @@ let
       hash = "sha256-mEFsi1cTa4GrogBxZfY3F6EHAfExNHv951QVJKum18s=";
     };
   };
+
+  # TODO remove when merged and release https://github.com/truenas/py-SMART/pull/89
+  pysmart = python3Packages.pysmart.overrideAttrs (oldAttrs: {
+    doCheck = false;
+    doInstallCheck = false;
+
+    patches = (oldAttrs.patches or [ ]) ++ [
+      (fetchpatch {
+        url = "https://patch-diff.githubusercontent.com/raw/truenas/py-SMART/pull/89.patch";
+        sha256 = "sha256-es6Zjw0Tzv34Hn3EhUYlM6Kfw6aKd/H3aEJ0ciBxHxQ=";
+      })
+    ];
+  });
 in glances.overrideAttrs (oldAttrs: {
   nativeBuildInputs = oldAttrs.nativeBuildInputs ++ [ makeWrapper ];
 
   propagatedBuildInputs = oldAttrs.propagatedBuildInputs ++ [
     py3nvml
+    pysmart
     python3Packages.batinfo
     python3Packages.docker
-    python3Packages.pysmart
     python3Packages.python-dateutil
     python3Packages.requests
     python3Packages.sparklines
