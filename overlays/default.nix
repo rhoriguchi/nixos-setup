d0b2f04c7cb513a471fc5e8ba7b69202c2b38dad
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2024-05-25 22:16:11 +0200

    Add adguardhome overlay

--- overlays/default.nix
+++ overlays/default.nix
@@ -19,6 +19,16 @@
           sha256 = "sha256-yS9W0MesZx8mTIUr+e08sZIgmEEN9/x+OMQDooJy4mg=";
         }
       }/pkgs/servers/tautulli") { };
+
+    # TODO remove when merged https://nixpk.gs/pr-tracker.html?pr=314672
+    adguardhome = super.callPackage (import "${
+        super.fetchFromGitHub {
+          owner = "NixOS";
+          repo = "nixpkgs";
+          rev = "04a08beac34b9841fe2063b1546a0ca2c8b60583";
+          sha256 = "sha256-5NlyrHu/NXXJO2+eOIYSfCjIl2Sg+cRdoEjYO1Kt4TQ=";
+        }
+      }/pkgs/servers/adguardhome") { };
   })
   (_: super: {
     discord = super.callPackage ./discord.nix { inherit (super) discord; };
