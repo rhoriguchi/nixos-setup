8bf3c3992944cf65b2ec7320f61db7258a66dc73
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2024-06-01 00:25:38 +0200

    Switch firewall to nftables

--- configuration/devices/headless/router/firewall.nix
+++ configuration/devices/headless/router/firewall.nix
@@ -5,27 +5,31 @@ let
 
   serverIp = "192.168.2.2";
 in {
-  networking.nat = {
-    enable = true;
+  networking = {
+    nftables.enable = true;
 
-    externalInterface = externalInterface;
-    internalInterfaces =
-      [ "${internalInterface}" "${internalInterface}.1" "${internalInterface}.2" "${internalInterface}.3" "${internalInterface}.100" ];
+    nat = {
+      enable = true;
 
-    forwardPorts = [
-      # Minecraft
-      {
-        proto = "tcp";
-        destination = "${serverIp}:25565";
-        sourcePort = 25565;
-      }
+      externalInterface = externalInterface;
+      internalInterfaces =
+        [ "${internalInterface}" "${internalInterface}.1" "${internalInterface}.2" "${internalInterface}.3" "${internalInterface}.100" ];
 
-      # Plex
-      {
-        proto = "tcp";
-        destination = "${serverIp}:32400";
-        sourcePort = 32400;
-      }
-    ];
+      forwardPorts = [
+        # Minecraft
+        {
+          proto = "tcp";
+          destination = "${serverIp}:25565";
+          sourcePort = 25565;
+        }
+
+        # Plex
+        {
+          proto = "tcp";
+          destination = "${serverIp}:32400";
+          sourcePort = 32400;
+        }
+      ];
+    };
   };
 }
