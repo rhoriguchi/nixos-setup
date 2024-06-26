d2a8f8ecdf89d1ddfc3574701eb907472445a384
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2024-06-26 21:22:39 +0200

    Set dns server for grimmjow

--- configuration/devices/headless/raspberry-pi-4/grimmjow/default.nix
+++ configuration/devices/headless/raspberry-pi-4/grimmjow/default.nix
@@ -1,7 +1,11 @@
 { secrets, ... }: {
   imports = [ ../common.nix ];
 
-  networking.hostName = "XXLPitu-Grimmjow";
+  networking = {
+    hostName = "XXLPitu-Grimmjow";
+
+    nameservers = [ "1.1.1.1" "1.0.0.1" ];
+  };
 
   fileSystems."/mnt/Data" = {
     device = "/dev/disk/by-uuid/5d4af7c3-9d62-4cc9-9e04-51d83c9bc5b4";
