637a57370535096380a5e7c5e3dad1817ef8dd7f
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2024-07-14 20:28:03 +0200

    Switch pip to uv

--- modules/profiles/python.nix
+++ modules/profiles/python.nix
@@ -1,12 +1,16 @@
 { pkgs, ... }: {
   environment.systemPackages = [
-    pkgs.pipenv
     pkgs.poetry
     pkgs.python3
     pkgs.python3Packages.black
     pkgs.python3Packages.flake8
-    pkgs.python3Packages.pip
     pkgs.python3Packages.pylint
     pkgs.python3Packages.pytest
+    pkgs.uv
   ];
+
+  environment.shellAliases = {
+    pip = "${pkgs.uv}/bin/uv pip";
+    venv = "${pkgs.uv}/bin/uv venv";
+  };
 }
