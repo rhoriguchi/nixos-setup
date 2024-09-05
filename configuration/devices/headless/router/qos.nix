d1e8c36a747c7d84ea8f92df51a5a5150fc3c147
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2024-09-05 03:27:27 +0200

    WIP Add fireqos

--- configuration/devices/headless/router/qos.nix
+++ configuration/devices/headless/router/qos.nix
@@ -1,10 +1,84 @@
 { config, interfaces, ... }:
 let
   externalInterface = interfaces.external;
+  internalInterface = interfaces.internal;
 
   download = 65;
   upload = 24;
+
+  tags = {
+    dns = "101";
+    icmp = "102";
+    plex = "103";
+    resilio = "104";
+    steam = "105";
+    torrent = "106";
+    wifi-calling = "107";
+    wireguard = "108";
+
+    private-vlan = "201";
+    dmz-vlan = "202";
+    iot-vlan = "203";
+    guest-vlan = "204";
+  };
 in {
+  networking.nftables = {
+    enable = true;
+
+    tables.qos = {
+      family = "inet";
+
+      content = ''
+        chain interface-filter-fw {
+          type filter hook forward priority filter; policy accept;
+
+          iifname "${externalInterface}" oifname "${internalInterface}" jump tag-traffic
+          iifname "${internalInterface}" oifname "${externalInterface}" jump tag-traffic
+        }
+
+        chain tag-traffic {
+          ip daddr 192.168.1.0/24 mark set ${tags.private-vlan}
+          ip saddr 192.168.1.0/24 mark set ${tags.private-vlan}
+
+          ip daddr 192.168.2.0/24 mark set ${tags.dmz-vlan}
+          ip saddr 192.168.2.0/24 mark set ${tags.dmz-vlan}
+
+          ip daddr 192.168.3.0/24 mark set ${tags.iot-vlan}
+          ip saddr 192.168.3.0/24 mark set ${tags.iot-vlan}
+
+          ip daddr 192.168.100.0/24 mark set ${tags.guest-vlan}
+          ip saddr 192.168.100.0/24 mark set ${tags.guest-vlan}
+
+
+          meta l4proto { udp, tcp } th sport 53 mark set ${tags.dns}
+          meta l4proto { udp, tcp } th dport 53 mark set ${tags.dns}
+
+          ip protocol icmp mark set ${tags.icmp}
+
+          meta l4proto tcp th dport 32400 mark set ${tags.plex}
+          meta l4proto tcp th sport 32400 mark set ${tags.plex}
+
+          meta l4proto tcp th dport 5555 mark set ${tags.resilio}
+          meta l4proto tcp th sport 5555 mark set ${tags.resilio}
+
+          meta l4proto { tcp, udp } th dport 27015-27050 mark set ${tags.steam}
+          meta l4proto { tcp, udp } th sport 27015-27050 mark set ${tags.steam}
+
+          meta l4proto tcp th dport 6881-6999 mark set ${tags.torrent}
+          meta l4proto tcp th sport 6881-6999 mark set ${tags.torrent}
+
+          meta l4proto tcp th dport 143 mark set ${tags.wifi-calling}
+          meta l4proto tcp th sport 143 mark set ${tags.wifi-calling}
+          meta l4proto udp th dport { 500, 4500 } mark set ${tags.wifi-calling}
+          meta l4proto udp th sport { 500, 4500 } mark set ${tags.wifi-calling}
+
+          meta l4proto udp th dport 51820 mark set ${tags.wireguard}
+          meta l4proto udp th sport 51820 mark set ${tags.wireguard}
+        }
+      '';
+    };
+  };
+
   services.fireqos = {
     enable = true;
 
@@ -19,19 +93,17 @@ in {
       interface ${externalInterface} world-in input rate ${toString download}mbit
         ###### high - prio 1 ######
 
-        class ping prio 1
-          match icmp
+        class icmp prio 1
+          match mark ${tags.icmp}
 
         class dns prio 1
-          match tcp port 53
-          match udp port 53
+          match mark ${tags.dns}
 
         class wireguard commit 1mbit prio 1
-          match udp port 51820
+          match mark ${tags.wireguard}
 
         class wifi_calling prio 1
-          match udp port 500
-          match udp port 4500
+          match mark ${tags.wifi-calling}
 
         ###### medium - prio 2 ######
 
@@ -39,47 +111,45 @@ in {
           match all
 
         class private_vlan prio 2
-          match src 192.168.1.0/24
+          match mark ${tags.private-vlan}
 
         class dmz_vlan prio 2
-          match src 192.168.2.0/24
+          match mark ${tags.dmz-vlan}
 
         class iot_vlan prio 2
-          match src 192.168.3.0/24
+          match mark ${tags.iot-vlan}
 
         class guest_vlan prio 2
-          match src 192.168.100.0/24
+          match mark ${tags.guest-vlan}
 
         ###### low - prio 3 ######
 
         class steam prio 3
-          match dport 27015:27050
+          match mark ${tags.steam}
 
         class torrent prio 3
-          match tcp port 6881:6999
+          match mark ${tags.steam}
 
         class resilio prio 3
-          match tcp port 5555
+          match mark ${tags.resilio}
 
         class plex prio 3
-          match tcp port 32400
+          match mark ${tags.plex}
 
       interface ${externalInterface} world-out output rate ${toString upload}mbit
         ###### high - prio 1 ######
 
-        class ping prio 1
-          match icmp
+        class icmp prio 1
+          match mark ${tags.icmp}
 
         class dns prio 1
-          match tcp port 53
-          match udp port 53
+          match mark ${tags.dns}
 
         class wireguard commit 1mbit prio 1
-          match udp port 51820
+          match mark ${tags.wireguard}
 
         class wifi_calling prio 1
-          match udp port 500
-          match udp port 4500
+          match mark ${tags.wifi-calling}
 
         ###### medium - prio 2 ######
 
@@ -87,30 +157,30 @@ in {
           match all
 
         class private_vlan prio 2
-          match dst 192.168.1.0/24
+          match mark ${tags.private-vlan}
 
         class dmz_vlan prio 2
-          match dst 192.168.2.0/24
+          match mark ${tags.dmz-vlan}
 
         class iot_vlan prio 2
-          match dst 192.168.3.0/24
+          match mark ${tags.iot-vlan}
 
         class guest_vlan prio 2
-          match dst 192.168.100.0/24
+          match mark ${tags.guest-vlan}
 
         ###### low - prio 3 ######
 
         class steam prio 3
-          match dport 27015:27050
+          match mark ${tags.steam}
 
         class torrent prio 3
-          match tcp port 6881:6999
+          match mark ${tags.steam}
 
         class resilio prio 3
-          match tcp port 5555
+          match mark ${tags.resilio}
 
         class plex prio 3
-          match tcp port 32400
+          match mark ${tags.plex}
     '';
   };
 
