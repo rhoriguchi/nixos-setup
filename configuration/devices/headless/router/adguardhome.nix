679d9b96460f5c97c6d3defef991cb21694b24f9
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2024-09-23 02:11:25 +0200

    Fix wrongly added dns entries

--- configuration/devices/headless/router/adguardhome.nix
+++ configuration/devices/headless/router/adguardhome.nix
@@ -123,9 +123,6 @@ in {
           answer = "XXLPitu-Ulquiorra.local";
         }) [ "scanner.00a.ch" ]) ++ (map (domain: {
           inherit domain;
-          answer = "${config.networking.hostName}.local";
-        }) config.services.infomaniak.hostnames) ++ (map (domain: {
-          inherit domain;
           answer = "XXLPitu-Server.local";
         }) [
           "deluge.00a.ch"
