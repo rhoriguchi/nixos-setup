18ff1f393975eaf3b3a9045284401a30b0b18b89
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2024-09-27 23:17:35 +0200

    Remove plex overlay

--- overlays/default.nix
+++ overlays/default.nix
@@ -9,16 +9,6 @@
           hash = "sha256-VlvxAWOKZ/ih7bxKxD2qfp2S8skGABS90hfmP8aV2w0=";
         }
       }/pkgs/tools/system/fancy-motd") { };
-
-    # TODO remove when merged https://nixpk.gs/pr-tracker.html?pr=342270
-    plexRaw = super.python3Packages.callPackage (import "${
-        super.fetchFromGitHub {
-          owner = "NixOS";
-          repo = "nixpkgs";
-          rev = "83c2ffcf9b74617632914f9debf3e02e9943ac53";
-          sha256 = "sha256-hrYizdLhaDXKSgJ9do76xvQS694bZ39ENcMaUeALQkE=";
-        }
-      }/pkgs/servers/plex/raw.nix") { };
   })
   (_: super: {
     discord = super.callPackage ./discord.nix { inherit (super) discord; };
