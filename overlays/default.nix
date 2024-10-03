6f5f1e01178c813a86c99ccbc550a9ac6d7b269b
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2024-10-03 22:38:37 +0200

    Add tautulli overlay

--- overlays/default.nix
+++ overlays/default.nix
@@ -10,6 +10,16 @@
         }
       }/pkgs/tools/system/fancy-motd") { };
 
+    # TODO remove when merged https://nixpk.gs/pr-tracker.html?pr=345402
+    tautulli = super.python3Packages.callPackage (import "${
+        super.fetchFromGitHub {
+          owner = "NixOS";
+          repo = "nixpkgs";
+          rev = "de1ef794a6c3224e0a6feb815f6e95b63ad6d683";
+          sha256 = "sha256-m0BgIKc0NkiJs01WHBsyCF5fDTZ2WmwUIOVD9xRe0U8=";
+        }
+      }/pkgs/servers/tautulli") { };
+
     # TODO remove when merged https://nixpk.gs/pr-tracker.html?pr=346196
     adguardhome = super.callPackage (import "${
         super.fetchFromGitHub {
