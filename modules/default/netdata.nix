3534166b35e6661e2b60541b883609c5385e1143
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2024-10-03 21:51:05 +0200

    Update netdata overlay

--- modules/default/netdata.nix
+++ modules/default/netdata.nix
@@ -1,12 +1,13 @@
 # TODO remove when merged https://nixpk.gs/pr-tracker.html?pr=340073
+# TODO remove when merged https://nixpk.gs/pr-tracker.html?pr=346228
 
 { modulesPath, ... }:
 let
   src = let
     owner = "NixOS";
     repo = "nixpkgs";
-    rev = "4e7df2caa4c1979a656a325fc08ab8a4694ea6ba";
-    sha256 = "sha256:0nksibckqm7r7zm82152plp6isjyjhb2mnlxcr1sp6wl6z8rfr4k";
+    rev = "a5483bacb3ce27264955e32d7303410dde76a7a0";
+    sha256 = "sha256:1j4wi375awwa3lkiy41k4myay27lxhl54pp8cwfawxx4cgkmm307";
   in builtins.fetchTarball {
     name = "nixpkgs";
     url = "https://github.com/${owner}/${repo}/archive/${rev}.tar.gz";
