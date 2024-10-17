1422bbee8c080572492c9db2238358424eac70fb
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2024-10-17 18:01:42 +0200

    Remove flameshot

--- modules/home-manager/gnome/autostart.nix
+++ modules/home-manager/gnome/autostart.nix
@@ -2,10 +2,6 @@
   xdg.configFile = {
     "autostart/${pkgs.discord.pname}.desktop".source = "${pkgs.discord}/share/applications/discord.desktop";
 
-    "autostart/${pkgs.flameshot.pname}.desktop".text =
-      let content = lib.readFile "${pkgs.flameshot}/share/applications/org.flameshot.Flameshot.desktop";
-      in lib.replaceStrings [ "/usr/bin/flameshot" ] [ "flameshot" ] content;
-
     "autostart/${pkgs.signal-desktop.pname}.desktop".source = "${pkgs.signal-desktop}/share/applications/signal-desktop.desktop";
 
     "autostart/${pkgs.wpa_supplicant_gui.pname}.desktop".text =
