5c4f4c0c7711a3ddcbf97016c67a4d569085e4e5
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2024-10-23 22:34:56 +0200

    HS add replace filter discord notification for stadler form karl

--- configuration/devices/headless/server/home-assistant/discord.nix
+++ configuration/devices/headless/server/home-assistant/discord.nix
@@ -1,18 +1,36 @@
 {
-  services.home-assistant.config.automation = [{
-    alias = "Stadler Form Karl low water Discord notification";
-    trigger = [{
-      platform = "state";
-      entity_id = "binary_sensor.stadler_form_karl_low_water";
-      from = "off";
-      to = "on";
-    }];
-    action = [{
-      action = "notify.home_assistant_bot";
-      data = {
-        message = "Stadler Form Karl low water";
-        target = [ "1285383628527763579" ];
-      };
-    }];
-  }];
+  services.home-assistant.config.automation = [
+    {
+      alias = "Stadler Form Karl low water Discord notification";
+      trigger = [{
+        platform = "state";
+        entity_id = "binary_sensor.stadler_form_karl_low_water";
+        from = "off";
+        to = "on";
+      }];
+      action = [{
+        action = "notify.home_assistant_bot";
+        data = {
+          message = "Stadler Form Karl low water";
+          target = [ "1285383628527763579" ];
+        };
+      }];
+    }
+    {
+      alias = "Stadler Form Karl replace filter Discord notification";
+      trigger = [{
+        platform = "state";
+        entity_id = "binary_sensor.stadler_form_karl_replace_filter";
+        from = "off";
+        to = "on";
+      }];
+      action = [{
+        action = "notify.home_assistant_bot";
+        data = {
+          message = "Stadler Form Karl replace filter";
+          target = [ "1285383628527763579" ];
+        };
+      }];
+    }
+  ];
 }
