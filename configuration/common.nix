3840d0d1d81e283d6d7ddeb457b123245f8e7f04
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2024-12-07 02:58:21 +0100

    Move permittedInsecurePackages to root common

--- configuration/common.nix
+++ configuration/common.nix
@@ -5,6 +5,19 @@
 
   networking.firewall.interfaces.${config.services.wireguard-network.interfaceName}.allowedTCPPorts = config.services.openssh.ports;
 
+  nixpkgs.config.permittedInsecurePackages = [ ]
+    # TODO remove when https://github.com/project-chip/connectedhomeip/issues/25688 fixed
+    ++ lib.optionals config.services.home-assistant.enable [
+      "openssl-1.1.1w"
+    ]
+    # TODO remove when https://github.com/NixOS/nixpkgs/issues/360592 fixed
+    ++ lib.optionals config.services.sonarr.enable [
+      "aspnetcore-runtime-6.0.36"
+      "aspnetcore-runtime-wrapped-6.0.36"
+      "dotnet-sdk-6.0.428"
+      "dotnet-sdk-wrapped-6.0.428"
+    ];
+
   services = {
     openssh = {
       enable = true;
