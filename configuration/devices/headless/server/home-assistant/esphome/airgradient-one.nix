3ce8c7df0a019895cec36a4faf7e30263af73c8b
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2024-12-29 13:18:03 +0100

    Remove airgradient automation

--- configuration/devices/headless/server/home-assistant/esphome/airgradient-one.nix
+++ configuration/devices/headless/server/home-assistant/esphome/airgradient-one.nix
@@ -1,60 +1,32 @@
 {
-  services.home-assistant.config = {
-    light = [
-      {
-        platform = "template";
-        lights.airgradient_one_display = {
-          friendly_name = "AirGradient ONE display";
-          value_template = "{{ (states('number.airgradient_one_display_contrast') | int) != 0 }}";
-          level_template = "{{ (states('number.airgradient_one_display_contrast') | int) / 100 * 255 | round }}";
-          turn_on = {
-            service = "number.set_value";
-            target.entity_id = "number.airgradient_one_display_contrast";
-            data.value = 100;
-          };
-          turn_off = {
-            service = "number.set_value";
-            target.entity_id = "number.airgradient_one_display_contrast";
-            data.value = 0;
-          };
-          set_level = {
-            service = "number.set_value";
-            target.entity_id = "number.airgradient_one_display_contrast";
-            data.value = "{{ (brightness / 255 * 100) | round }}";
-          };
+  services.home-assistant.config.light = [
+    {
+      platform = "template";
+      lights.airgradient_one_display = {
+        friendly_name = "AirGradient ONE display";
+        value_template = "{{ (states('number.airgradient_one_display_contrast') | int) != 0 }}";
+        level_template = "{{ (states('number.airgradient_one_display_contrast') | int) / 100 * 255 | round }}";
+        turn_on = {
+          service = "number.set_value";
+          target.entity_id = "number.airgradient_one_display_contrast";
+          data.value = 100;
         };
-      }
-      {
-        platform = "group";
-        name = "AirGradient ONE";
-        entities = [ "light.airgradient_one_display" "light.airgradient_one_led_strip" ];
-      }
-    ];
-
-    automation = [
-      {
-        alias = "Turn lights AirGradient ONE on when sunrise";
-        trigger = [{
-          platform = "sun";
-          event = "sunrise";
-        }];
-        action = [{
-          action = "light.turn_on";
-          target.entity_id = "light.airgradient_one";
-          data.brightness = 255;
-        }];
-      }
-      {
-        alias = "Turn lights AirGradient ONE off when sunset";
-        trigger = [{
-          platform = "sun";
-          event = "sunset";
-        }];
-        action = [{
-          action = "light.turn_off";
-          target.entity_id = "light.airgradient_one";
-        }];
-      }
-    ];
-  };
+        turn_off = {
+          service = "number.set_value";
+          target.entity_id = "number.airgradient_one_display_contrast";
+          data.value = 0;
+        };
+        set_level = {
+          service = "number.set_value";
+          target.entity_id = "number.airgradient_one_display_contrast";
+          data.value = "{{ (brightness / 255 * 100) | round }}";
+        };
+      };
+    }
+    {
+      platform = "group";
+      name = "AirGradient ONE";
+      entities = [ "light.airgradient_one_display" "light.airgradient_one_led_strip" ];
+    }
+  ];
 }
