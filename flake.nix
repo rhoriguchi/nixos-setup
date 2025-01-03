4223526518a6957996d4cff335e8a3f2fd28d9e3
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2025-01-03 23:34:05 +0100

    Add some more pre-commit hooks

--- flake.nix
+++ flake.nix
@@ -293,20 +293,38 @@
 
             hooks = {
               actionlint.enable = true;
+              check-case-conflicts.enable = true;
+              check-executables-have-shebangs.enable = true;
+              check-merge-conflicts.enable = true;
+              check-shebang-scripts-are-executable.enable = true;
+              check-symlinks.enable = true;
               deadnix = {
                 enable = true;
-                excludes = [ "hardware-configuration\\.nix" ];
+                excludes = [ "hardware-configuration\\.nix$" ];
               };
+              end-of-file-fixer = {
+                enable = true;
+                excludes = [ "secrets\\.nix$" ];
+              };
+              fix-byte-order-marker.enable = true;
               markdownlint = {
                 enable = true;
 
                 settings.configuration.MD013 = false;
               };
+              mixed-line-endings = {
+                enable = true;
+                excludes = [ "secrets\\.nix$" ];
+              };
               nixfmt-classic = {
                 enable = true;
-                excludes = [ "secrets\\.nix" ];
+                excludes = [ "secrets\\.nix$" ];
 
                 settings.width = 140;
+              };
+              trim-trailing-whitespace = {
+                enable = true;
+                excludes = [ "secrets\\.nix$" ];
               };
             };
           };
