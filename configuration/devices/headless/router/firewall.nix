529a2fe21945fadc7132b95f21013a25af0e3617
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2025-01-09 22:03:47 +0100

    Router hardening

--- configuration/devices/headless/router/firewall.nix
+++ configuration/devices/headless/router/firewall.nix
@@ -7,6 +7,14 @@ let
   wingoRouterIp = "192.168.0.254";
   serverIp = "192.168.2.2";
 in {
+  boot.kernel.sysctl = {
+    "net.ipv4.conf.all.accept_redirects" = 0;
+    "net.ipv4.conf.default.accept_redirects" = 0;
+
+    "net.ipv4.conf.all.secure_redirects" = 0;
+    "net.ipv4.conf.default.secure_redirects" = 0;
+  };
+
   networking = {
     firewall.interfaces = let rules = { allowedTCPPorts = [ 80 443 ]; };
     in {
