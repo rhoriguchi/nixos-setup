76450fb2426ae3131082eac075f6a0894b18bb48
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2025-01-16 14:20:16 +0100

    Handle case where nothing gets returned

--- configuration/devices/headless/server/home-assistant/weather.nix
+++ configuration/devices/headless/server/home-assistant/weather.nix
@@ -6,14 +6,18 @@ let
   units = config.services.home-assistant.config.homeassistant.unit_system;
 
   apiUrl = "https://api.openweathermap.org/data/2.5";
+  url = "${apiUrl}/weather?appid=${apiKey}&lat=${toString lat}&lon=${toString lon}&units=${units}";
+
+  script = pkgs.writeShellScript "openweather.sh" ''
+    output="$(${pkgs.curl}/bin/curl -s '${url}' | ${pkgs.jq}/bin/jq '.main.temp')"
+    echo "''${output:-0}"
+  '';
 in {
   services.home-assistant.config.command_line = [{
     sensor = {
       name = "OpenWeather current temperature";
       scan_interval = 5 * 60;
-      command = "${pkgs.curl}/bin/curl '${apiUrl}/weather?appid=${apiKey}&lat=${toString lat}&lon=${
-          toString lon
-        }&units=${units}' | ${pkgs.jq}/bin/jq '.main.temp'";
+      command = script;
       value_template = "{{ value | float }}";
       unit_of_measurement = "°C";
       state_class = "measurement";
