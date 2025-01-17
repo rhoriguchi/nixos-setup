a1fab791ea6c92f2b84f076e166076a47df8cfce
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2025-01-17 16:38:37 +0100

    Drop all connections to unmanned nginx domains

--- modules/profiles/nginx.nix
+++ modules/profiles/nginx.nix
@@ -12,6 +12,23 @@
       recommendedProxySettings = true;
       recommendedTlsSettings = true;
       recommendedZstdSettings = true;
+
+      virtualHosts."_" = {
+        default = true;
+        rejectSSL = true;
+
+        locations = {
+          "@blackhole".return = 444;
+
+          "/" = {
+            return = 444;
+
+            extraConfig = ''
+              error_page 400 = @blackhole;
+            '';
+          };
+        };
+      };
     };
 
     logrotate.settings.nginx.enable = false;
