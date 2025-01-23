bb6ac6fa7de88142082aef3a16abd235e437c2ab
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2025-01-23 05:25:45 +0100

    Add fzf-tab

--- modules/home-manager/fzf.nix
+++ modules/home-manager/fzf.nix
@@ -1,21 +1,54 @@
-{ colors, ... }: {
-  programs.fzf = {
-    enable = true;
+{ colors, config, lib, pkgs, ... }: {
+  programs = {
+    fzf = {
+      enable = true;
 
-    enableBashIntegration = true;
-    enableFishIntegration = true;
-    enableZshIntegration = true;
+      enableBashIntegration = true;
+      enableFishIntegration = true;
+      enableZshIntegration = true;
 
-    colors = {
-      hl = colors.bright.accent;
-      "hl+" = colors.normal.accent;
-      info = colors.normal.green;
-      prompt = colors.normal.white;
-      pointer = colors.normal.accent;
+      colors = {
+        hl = colors.bright.accent;
+        "hl+" = colors.normal.accent;
+        info = colors.normal.green;
+        prompt = colors.normal.white;
+        pointer = colors.normal.accent;
+      };
+
+      historyWidgetOptions = [ "--no-multi" ];
+      changeDirWidgetCommand = "";
+      fileWidgetCommand = "";
     };
 
-    historyWidgetOptions = [ "--no-multi" ];
-    changeDirWidgetCommand = "";
-    fileWidgetCommand = "";
+    zsh = {
+      plugins = [{
+        name = pkgs.zsh-fzf-tab.pname;
+        file = "fzf-tab.plugin.zsh";
+        src = "${pkgs.zsh-fzf-tab}/share/fzf-tab";
+      }];
+
+      initExtra = ''
+        # Use FZF_DEFAULT_OPTS
+        zstyle ':fzf-tab:*' use-fzf-default-opts yes
+
+        # Set extra options
+        zstyle ':fzf-tab:*' fzf-flags --bind=tab:accept
+
+        # Force zsh not to show completion menu, which allows fzf-tab to capture the unambiguous prefix
+        zstyle ':completion:*' menu no
+
+        # Preview directory's content when completing cd
+        zstyle ':fzf-tab:complete:cd:*' fzf-preview '${
+          if config.programs.lsd.enable then "${pkgs.lsd}/bin/lsd" else "ls"
+        } --almost-all --color always $realpath'
+
+        ${lib.optionalString config.programs.zoxide.enable ''
+          # Preview directory's content when completing zoxide
+          zstyle ':fzf-tab:complete:__zoxide_z:*' fzf-preview '${
+            if config.programs.lsd.enable then "${pkgs.lsd}/bin/lsd" else "ls"
+          } --almost-all --color always $realpath'
+        ''}
+      '';
+    };
   };
 }
