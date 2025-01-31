eb9f03d13603b30d312c5b9d404d049d9e8b9690
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2025-01-31 20:03:23 +0100

    Small tweak

--- modules/home-manager/terminal/default.nix
+++ modules/home-manager/terminal/default.nix
@@ -1,7 +1,10 @@
-{ config, ... }: {
+{ config, lib, ... }: {
   imports = [ ./ghostty.nix ];
 
   home.sessionVariables.TERMINAL = "ghostty";
 
-  programs.vscode.userSettings."terminal.external.linuxExec" = "${config.programs.ghostty.package}/bin/ghostty";
+  # Don't set if `null`, `userSettings` uses mkMerge so options can't be overwritten
+  programs.vscode.userSettings = lib.optionalAttrs (config.programs.ghostty.package != null) {
+    "terminal.external.linuxExec" = "${config.programs.ghostty.package}/bin/ghostty";
+  };
 }
