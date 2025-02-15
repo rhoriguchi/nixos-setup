e7779c519512dde643c5e8ce65ecceaa90aaf14a
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2025-02-15 18:45:50 +0100

    Some nautilus tweaks

--- modules/home-manager/nautilus.nix
+++ modules/home-manager/nautilus.nix
@@ -1,4 +1,4 @@
-{ config, ... }:
+{ config, lib, ... }:
 let homeDirectory = config.home.homeDirectory;
 in {
   # TODO commented since this causes issues on darwin
@@ -6,9 +6,16 @@ in {
   programs.zsh.shellAliases.open = "nautilus";
 
   xdg.configFile."gtk-3.0/bookmarks".text = ''
-    file://${homeDirectory}/Downloads/Browser
+    file://${homeDirectory}/Downloads/Browser Downloads
     file://${homeDirectory}/Sync
-    file://${homeDirectory}/Sync/Git
-    file://${homeDirectory}/Sync/Series
+    file://${homeDirectory}/Sync/Git Sync/Git
+    file://${homeDirectory}/Sync/Series Sync/Series
+
+    ${let
+      wireguardIps = import ../default/wireguard-network/ips.nix;
+      filteredWireguardIps = lib.filterAttrs (key: _: key != "Ryan-Laptop") wireguardIps;
+    in lib.concatStringsSep "\n" (map (hostname: "sftp://root@${hostname} ${hostname}") (lib.attrNames filteredWireguardIps))}
   '';
+
+  #
 }
