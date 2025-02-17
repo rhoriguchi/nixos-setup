76a08318bb9927a815487a5aa039b7ae4d32ff24
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2025-02-17 03:36:52 +0100

    Use a list for hs server_host

--- configuration/devices/headless/server/home-assistant/proxy.nix
+++ configuration/devices/headless/server/home-assistant/proxy.nix
@@ -46,7 +46,7 @@ in {
     };
 
     home-assistant.config.http = {
-      server_host = "127.0.0.1";
+      server_host = [ "127.0.0.1" ];
       server_port = homeAssistantPort + 1;
       trusted_proxies = [ "127.0.0.1" ];
       use_x_forwarded_for = true;
