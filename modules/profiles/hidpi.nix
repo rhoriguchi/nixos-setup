50b377a65a2509cdb4cd7f2eca1a5fa712670462
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2025-02-23 23:01:31 +0100

    Remove hidpi gimp overlay

--- modules/profiles/hidpi.nix
+++ modules/profiles/hidpi.nix
@@ -1,13 +1,6 @@
 {
   nixpkgs.overlays = [
     (_: super: {
-      gimp = super.gimp.overrideAttrs (oldAttrs: {
-        postInstall = (oldAttrs.postInstall or "") + ''
-          substituteInPlace $out/etc/gimp/2.0/gimprc \
-            --replace-fail "# (icon-size auto)" "(icon-size huge)"
-        '';
-      });
-
       spotify = super.spotify.overrideAttrs (oldAttrs: {
         nativeBuildInputs = oldAttrs.nativeBuildInputs ++ [ super.makeWrapper ];
 
