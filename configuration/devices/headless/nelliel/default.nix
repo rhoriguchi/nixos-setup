041c2d19eaa398964f8582d3925bcec96cd2f4af
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2025-02-23 18:25:11 +0100

    Add new host XXLPitu-Nelliel

---
+++ configuration/devices/headless/nelliel/default.nix
@@ -0,0 +1,23 @@
+{ secrets, ... }: {
+  imports = [
+    ../common.nix
+
+    # TODO update
+    ./hardware-configuration.nix
+  ];
+
+  networking = {
+    hostName = "XXLPitu-Nelliel";
+
+    interfaces.enp1s0.useDHCP = true;
+
+    nameservers = [ "1.1.1.1" "1.0.0.1" ];
+  };
+
+  services.resilio = {
+    enable = true;
+
+    secrets = secrets.resilio.secrets;
+    syncPath = "/var/lib/Sync";
+  };
+}
