04f0e7a0782297a306ec764f3dbe4dbf2eae96ce
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2025-02-24 01:04:00 +0100

    Use `gtk.gtk3.bookmarks` module to configure nautilus and remove alias

--- modules/home-manager/nautilus.nix
+++ modules/home-manager/nautilus.nix
@@ -1,21 +1,18 @@
 { config, lib, ... }:
-let homeDirectory = config.home.homeDirectory;
+let
+  homeDirectory = config.home.homeDirectory;
+
+  wireguardIps = import (lib.custom.relativeToRoot "modules/default/wireguard-network/ips.nix");
+  filteredWireguardIps = lib.filterAttrs (key: _: key != "Ryan-Laptop") wireguardIps;
 in {
-  # TODO commented since this causes issues on darwin
-  # programs.zsh.shellAliases.open = "${pkgs.nautilus}/bin/nautilus";
-  programs.zsh.shellAliases.open = "nautilus";
+  gtk = {
+    enable = true;
 
-  xdg.configFile."gtk-3.0/bookmarks".text = ''
-    file://${homeDirectory}/Downloads/Browser Downloads
-    file://${homeDirectory}/Sync
-    file://${homeDirectory}/Sync/Git Sync/Git
-    file://${homeDirectory}/Sync/Series Sync/Series
-
-    ${let
-      wireguardIps = import (lib.custom.relativeToRoot "modules/default/wireguard-network/ips.nix");
-      filteredWireguardIps = lib.filterAttrs (key: _: key != "Ryan-Laptop") wireguardIps;
-    in lib.concatStringsSep "\n" (map (hostname: "sftp://root@${hostname} ${hostname}") (lib.attrNames filteredWireguardIps))}
-  '';
-
-  #
+    gtk3.bookmarks = [
+      "file://${homeDirectory}/Downloads/Browser Downloads"
+      "file://${homeDirectory}/Sync"
+      "file://${homeDirectory}/Sync/Git Sync/Git"
+      "file://${homeDirectory}/Sync/Series Sync/Series"
+    ] ++ map (hostname: "sftp://root@${hostname} ${hostname}") (lib.attrNames filteredWireguardIps);
+  };
 }
