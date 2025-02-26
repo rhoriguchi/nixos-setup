74d9099cb6215e8421364c6fd727592f17de23aa
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2025-02-26 22:51:23 +0100

    Generate profiles attribute form dir content

--- modules/profiles/default.nix
+++ modules/profiles/default.nix
@@ -1,25 +1,5 @@
-{
-  headless = import ./headless.nix;
-  headful = import ./headful.nix;
-
-  displaylink = import ./displaylink.nix;
-  docker = import ./docker.nix;
-  fancy-motd = import ./fancy-motd.nix;
-  git = import ./git.nix;
-  gnome = import ./gnome.nix;
-  i18n = import ./i18n.nix;
-  java = import ./java.nix;
-  javascript = import ./javascript.nix;
-  keyboard = import ./keyboard.nix;
-  kotlin = import ./kotlin.nix;
-  laptop-power-management = import ./laptop-power-management.nix;
-  nginx = import ./nginx.nix;
-  nix = import ./nix.nix;
-  nvd = import ./nvd.nix;
-  podman = import ./podman.nix;
-  printing = import ./printing.nix;
-  python = import ./python.nix;
-  shell = import ./shell.nix;
-  util = import ./util.nix;
-  zfs = import ./zfs.nix;
-}
+let files = builtins.attrNames (builtins.readDir ./.);
+in builtins.listToAttrs (map (name: {
+  name = builtins.replaceStrings [ ".nix" ] [ "" ] name;
+  value = import (./. + "/${name}");
+}) files)
