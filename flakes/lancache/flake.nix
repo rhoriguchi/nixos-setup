18c97e878948a89bbc2472237358b02e5a8d8daa
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2025-03-01 03:18:23 +0100

    Add flake with lancache domain as src

---
+++ flakes/lancache/flake.nix
@@ -0,0 +1,10 @@
+{
+  description = "Lancache";
+
+  inputs.cache-domains = {
+    url = "github:uklans/cache-domains";
+    flake = false;
+  };
+
+  # outputs = { self, ... }@inputs: { nixosModules.default = import ./module.nix; };
+}
