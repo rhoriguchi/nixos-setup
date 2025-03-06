87eca9fb3efcd45d67ddfd102a9721e2234f6fae
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2025-03-07 00:45:21 +0100

    Remove fancy-motd

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,15 +1,5 @@
 [
   (_: super: {
-    # TODO remove when merged https://nixpk.gs/pr-tracker.html?pr=185611
-    fancy-motd = super.callPackage (import "${
-        super.fetchFromGitHub {
-          owner = "NixOS";
-          repo = "nixpkgs";
-          rev = "4fd5da3781ed6d12a139bee1c84d17772edf130c";
-          hash = "sha256-VlvxAWOKZ/ih7bxKxD2qfp2S8skGABS90hfmP8aV2w0=";
-        }
-      }/pkgs/tools/system/fancy-motd") { };
-
     # TODO remove when merged https://nixpk.gs/pr-tracker.html?pr=384063
     plexRaw = super.python3Packages.callPackage (import "${
         super.fetchFromGitHub {
