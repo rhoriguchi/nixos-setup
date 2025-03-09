c22c66d98d95a487f537119abe16a107d2b04124
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2025-03-09 12:54:41 +0100

    Remove 10.0.0.0/8 and 172.16.0.0/12 from lancache access

--- configuration/devices/headless/router/lancache.nix
+++ configuration/devices/headless/router/lancache.nix
@@ -20,8 +20,6 @@
             proxyPass = "http://127.0.0.1:${toString config.services.lancache.httpPort}";
 
             extraConfig = ''
-              allow 10.0.0.0/8;
-              allow 172.16.0.0/12;
               allow 192.168.0.0/16;
               deny all;
             '';
