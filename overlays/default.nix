9dd6dbfadb2d443929ce29e2512a0dd569b02796
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2025-03-11 19:32:47 +0100

    Remove unneeded overlay

--- overlays/default.nix
+++ overlays/default.nix
@@ -11,20 +11,6 @@
           }
         }/pkgs/desktops/gnome/extensions/unite/default.nix") { };
     };
-
-    vscode-extensions = super.vscode-extensions // {
-      ms-python = super.vscode-extensions.ms-python // {
-        # TODO remove when merged https://nixpk.gs/pr-tracker.html?pr=387839
-        python = super.callPackage (import "${
-            super.fetchFromGitHub {
-              owner = "NixOS";
-              repo = "nixpkgs";
-              rev = "4886e147e1b285057228cbd7ce2348cf8fb4cb45";
-              hash = "sha256-HDRMWYEn1V4kp2wUlwP+fFAvSntBLwTJa2WLQfiVJAc=";
-            }
-          }/pkgs/applications/editors/vscode/extensions/ms-python.python") { };
-      };
-    };
   })
   (_: super: {
     discord = super.callPackage ./discord.nix { inherit (super) discord; };
