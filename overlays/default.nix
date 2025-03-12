e576d76405e102365c4ec716bdfd6e3c121dddd2
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2025-03-12 23:46:04 +0100

    Add plex overlay

--- overlays/default.nix
+++ overlays/default.nix
@@ -11,6 +11,16 @@
           }
         }/pkgs/desktops/gnome/extensions/unite/default.nix") { };
     };
+
+    # TODO remove when merged https://nixpk.gs/pr-tracker.html?pr=389378
+    plexRaw = super.python3Packages.callPackage (import "${
+        super.fetchFromGitHub {
+          owner = "NixOS";
+          repo = "nixpkgs";
+          rev = "e69a001b66b91ef0b0875738ec00270747051f45";
+          sha256 = "sha256-Tt0oUrTZSXHtHz8T5vxNjA8sn/dbLEeQzyjUIAb2GDE=";
+        }
+      }/pkgs/servers/plex/raw.nix") { };
   })
   (_: super: {
     discord = super.callPackage ./discord.nix { inherit (super) discord; };
