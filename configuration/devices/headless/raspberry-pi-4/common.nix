fea86012728c4f3f3b51f80be93db530de62e85d
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2025-03-20 21:42:06 +0100

    Remove raspberry Pi 4 workarounds

--- configuration/devices/headless/raspberry-pi-4/common.nix
+++ configuration/devices/headless/raspberry-pi-4/common.nix
@@ -1,19 +1,7 @@
 { lib, pkgs, ... }: {
   imports = [ ../common.nix ./hardware-configuration.nix ];
 
-  boot = {
-    kernelPackages = lib.mkForce pkgs.linuxKernel.packages.linux_rpi4;
-
-    # fixes connection issue with wpa_supplicant v2.11
-    # https://github.com/raspberrypi/bookworm-feedback/issues/121
-    extraModprobeConfig = ''
-      options brcmfmac feature_disable=0x2000
-    '';
-
-    # https://github.com/NixOS/nixos-hardware/issues/858
-    # https://github.com/NixOS/nixpkgs/issues/344963
-    initrd.systemd.tpm2.enable = false;
-  };
+  boot.kernelPackages = lib.mkForce pkgs.linuxKernel.packages.linux_rpi4;
 
   networking.interfaces = {
     eth0.useDHCP = true; # Ethernet
@@ -21,7 +9,4 @@
   };
 
   environment.systemPackages = [ pkgs.raspberrypi-eeprom ];
-
-  # https://github.com/fwupd/fwupd/wiki/PluginFlag:capsules-unsupported
-  services.fwupd.daemonSettings.DisabledPlugins = [ "test" "test_ble" "invalid" "bios" ];
 }
