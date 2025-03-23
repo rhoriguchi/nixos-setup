be616679be71c72a5bc6f52dfbfd4c78d575c480
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2025-03-23 02:54:13 +0100

    Enable nftables on server

--- configuration/devices/headless/server/default.nix
+++ configuration/devices/headless/server/default.nix
@@ -37,9 +37,6 @@
     hostId = "c270d3cf";
     hostName = "XXLPitu-Server";
 
-    # TODO breaks libvirt guest internet
-    nftables.enable = lib.mkForce false;
-
     interfaces = {
       eno1.useDHCP = true;
       wlp11s0.useDHCP = true;
