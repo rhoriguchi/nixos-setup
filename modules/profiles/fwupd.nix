058555b5eaa571dd6366bfe1a638d6d25b87c419
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2025-04-19 03:49:49 +0200

    Remove gnome-software

--- modules/profiles/fwupd.nix
+++ modules/profiles/fwupd.nix
@@ -1,5 +1,1 @@
-{ config, lib, pkgs, ... }: {
-  services.fwupd.enable = true;
-
-  environment.systemPackages = lib.optional config.services.xserver.desktopManager.gnome.enable pkgs.gnome-software;
-}
+{ services.fwupd.enable = true; }
