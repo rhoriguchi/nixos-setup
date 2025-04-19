86a0a188e64d8a99339ed37dcf1bc2e9bbc0c434
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2025-04-19 17:05:49 +0200

    Set rustdesk keys with nix

--- configuration/devices/headless/server/rustdesk.nix
+++ configuration/devices/headless/server/rustdesk.nix
@@ -1,4 +1,8 @@
-{ secrets, ... }: {
+{ pkgs, secrets, ... }:
+let
+  privateKey = pkgs.writeText "id_ed25519" secrets.rustdesk.privateKey;
+  publicKey = pkgs.writeText "id_ed25519.pub" secrets.rustdesk.publicKey;
+in {
   services = {
     infomaniak = {
       enable = true;
@@ -13,12 +17,15 @@
 
       signal = {
         relayHosts = [ "rustdesk.00a.ch" ];
-        extraArgs = [ "--key" "_" "--mask" "192.168.0.0/16" ];
+        extraArgs = [ "--key" secrets.rustdesk.publicKey "--mask" "192.168.0.0/16" ];
       };
 
-      relay.extraArgs = [ "--key" "_" ];
+      relay.extraArgs = [ "--key" secrets.rustdesk.publicKey ];
     };
   };
+
+  systemd.tmpfiles.rules =
+    [ "L+ /var/lib/rustdesk/id_ed25519 - - - - ${privateKey}" "L+ /var/lib/rustdesk/id_ed25519.pub - - - - ${publicKey}" ];
 
   networking.firewall = {
     allowedTCPPorts = [ 21115 21116 21117 21118 21119 ];
