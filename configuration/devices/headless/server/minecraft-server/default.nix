6edb7f9c50af379d07392b976845a171d2e6c34f
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2025-04-24 12:12:16 +0200

    Use recommended minecraft server flags

--- configuration/devices/headless/server/minecraft-server/default.nix
+++ configuration/devices/headless/server/minecraft-server/default.nix
@@ -105,7 +105,20 @@ in {
           openFirewall = true;
 
           package = pkgs.velocityServers.velocity;
-          jvmOpts = let memory = 1024 * 0.5; in "-Xmx${toString (builtins.floor memory)}M -Xms${toString (builtins.floor memory)}M";
+
+          # https://docs.papermc.io/velocity/tuning/#tune-your-startup-flags
+          jvmOpts = let memory = 1024 * 0.5;
+          in lib.concatStringsSep " " [
+            "-Xms${toString (builtins.floor memory)}M"
+            "-Xmx${toString (builtins.floor memory)}M"
+
+            "-XX:+UseG1GC"
+            "-XX:G1HeapRegionSize=4M"
+            "-XX:+UnlockExperimentalVMOptions"
+            "-XX:+ParallelRefProcEnabled"
+            "-XX:+AlwaysPreTouch"
+            "-XX:MaxInlineLevel=15"
+          ];
 
           restart = "on-abort";
 
@@ -168,7 +181,32 @@ in {
           openFirewall = false;
 
           inherit package;
-          jvmOpts = let memory = 1024 * 4; in "-Xmx${toString (builtins.floor memory)}M -Xms${toString (builtins.floor memory)}M";
+
+          # https://docs.papermc.io/paper/aikars-flags/#recommended-jvm-startup-flags
+          jvmOpts = let memory = 1024 * 6;
+          in lib.concatStringsSep " " [
+            "-Xms${toString (builtins.floor memory)}M"
+            "-Xmx${toString (builtins.floor memory)}M"
+
+            "-XX:+UseG1GC"
+            "-XX:+ParallelRefProcEnabled"
+            "-XX:MaxGCPauseMillis=200"
+            "-XX:+UnlockExperimentalVMOptions"
+            "-XX:+DisableExplicitGC"
+            "-XX:+AlwaysPreTouch"
+            "-XX:G1NewSizePercent=30"
+            "-XX:G1MaxNewSizePercent=40"
+            "-XX:G1HeapRegionSize=8M"
+            "-XX:G1ReservePercent=20"
+            "-XX:G1HeapWastePercent=5"
+            "-XX:G1MixedGCCountTarget=4"
+            "-XX:InitiatingHeapOccupancyPercent=15"
+            "-XX:G1MixedGCLiveThresholdPercent=90"
+            "-XX:G1RSetUpdatingPauseTimePercent=5"
+            "-XX:SurvivorRatio=32"
+            "-XX:+PerfDisableSharedMem"
+            "-XX:MaxTenuringThreshold=1"
+          ];
 
           restart = "on-abort";
 
