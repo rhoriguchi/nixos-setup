fcef05c42e61e799af6e33d336018bef9af379ef
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2025-04-26 23:18:49 +0200

    Update flake.lock

--- overlays/default.nix
+++ overlays/default.nix
@@ -10,26 +10,6 @@
         }
       }/pkgs/by-name/re/resilio-sync/package.nix") { };
 
-    # TODO remove when merged https://nixpk.gs/pr-tracker.html?pr=390165
-    netdata = super.callPackage (import "${
-        super.fetchFromGitHub {
-          owner = "NixOS";
-          repo = "nixpkgs";
-          rev = "547813ca4c118818947eccbb63be2506b5bbcdfd";
-          sha256 = "sha256-vS6cy1MTBcqAt2bWxMgbWCAOFVOzX83l2dSDPKlQymE=";
-        }
-      }/pkgs/tools/system/netdata") { protobuf = super.protobuf_21; };
-
-    # TODO remove when merged https://nixpk.gs/pr-tracker.html?pr=393454
-    adguardhome = super.callPackage (import "${
-        super.fetchFromGitHub {
-          owner = "NixOS";
-          repo = "nixpkgs";
-          rev = "a2412aa76dfd419d2acdd430581bb47410d93471";
-          sha256 = "sha256-ALLPqPr6tAE6IfQBTCweNsL1zJCtQ3LMzKxaSa2INyM=";
-        }
-      }/pkgs/servers/adguardhome") { };
-
     # TODO remove when merged https://nixpk.gs/pr-tracker.html?pr=401131
     prowlarr = super.callPackage (import "${
         super.fetchFromGitHub {
