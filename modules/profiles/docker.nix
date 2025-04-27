7170ff4fc969a08635330ea6c076276e461acbb5
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2025-04-27 19:29:30 +0200

    Use overlay2 storage driver for docker

--- modules/profiles/docker.nix
+++ modules/profiles/docker.nix
@@ -2,6 +2,7 @@
   virtualisation.docker = {
     enable = true;
 
+    storageDriver = "overlay2";
     logDriver = "json-file";
     extraOptions = lib.concatStringsSep " " [ "--log-opt max-file=10" "--log-opt max-size=10m" ];
   };
