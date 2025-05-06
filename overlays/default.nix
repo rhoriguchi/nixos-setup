a5a3f3ca835df4859050b8f08a2d6760c6e256cb
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2025-05-07 01:25:08 +0200

    Add netdata overlay

--- overlays/default.nix
+++ overlays/default.nix
@@ -9,6 +9,16 @@
           sha256 = "sha256-jQNGd1Kmey15jq5U36m8pG+lVsxSJlDj1bJ167BjHQ4=";
         }
       }/pkgs/by-name/re/resilio-sync/package.nix") { };
+
+    # TODO remove when merged https://nixpk.gs/pr-tracker.html?pr=404786
+    netdata = super.callPackage (import "${
+        super.fetchFromGitHub {
+          owner = "NixOS";
+          repo = "nixpkgs";
+          rev = "05616a399c893f0016f6ef12e2ef29797f0d2329";
+          sha256 = "sha256-4FI+mXVZeYr4P3i8AkzEHXUf8p/JS6YTSAjE5dTSoWU=";
+        }
+      }/pkgs/tools/system/netdata") { protobuf = super.protobuf_21; };
   })
   (_: super: {
     discord = super.callPackage ./discord.nix { inherit (super) discord; };
