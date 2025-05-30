483c01ac45470fe8a0bd320e90471199ec7adf5d
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2025-05-30 02:48:43 +0200

    Disable blueman notification

--- modules/profiles/bluetooth.nix
+++ modules/profiles/bluetooth.nix
@@ -2,4 +2,13 @@
   hardware.bluetooth.enable = true;
 
   environment.systemPackages = [ pkgs.blueman ];
+
+  programs.dconf = {
+    enable = true;
+
+    profiles.user.databases = [
+      # Blueman
+      { settings."org/blueman/general".plugin-list = [ "!ConnectionNotifier" ]; }
+    ];
+  };
 }
