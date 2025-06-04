0bca7d0b7a4e8efb929aba8bd01db573f2edb4dc
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2025-06-04 22:55:40 +0200

    Upgrade mini-graph-card

--- overlays/hs/lovelace-module/mini-graph-card.nix
+++ overlays/hs/lovelace-module/mini-graph-card.nix
@@ -1,13 +1,13 @@
 { stdenv, fetchurl }:
 stdenv.mkDerivation rec {
   pname = "hs-lovelace-module-mini-graph-card";
-  version = "0.12.1";
+  version = "0.13.0";
 
   src = let
     owner = "kalkih";
     repo = "mini-graph-card";
     tag = "v${version}";
-    hash = "sha256-NfqjTTwHleXajLNlMEQgbVYfiEtsC12aCbFEuH9RGMQ=";
+    hash = "sha256-TYuYbzzWk8D3dx0vVXQAi8OcRey0UK7AZ5BhUL4t+r0=";
   in fetchurl {
     url = "https://github.com/${owner}/${repo}/releases/download/${tag}/mini-graph-card-bundle.js";
     inherit hash;
