adfaf9265ecdbfeff75a7c68bc0169017529ec14
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2025-06-04 22:19:21 +0200

    Update flake.lock

--- overlays/default.nix
+++ overlays/default.nix
@@ -9,16 +9,6 @@
           sha256 = "sha256-jQNGd1Kmey15jq5U36m8pG+lVsxSJlDj1bJ167BjHQ4=";
         }
       }/pkgs/by-name/re/resilio-sync/package.nix") { };
-
-    # TODO remove when merged https://nixpk.gs/pr-tracker.html?pr=411467
-    adguardhome = super.callPackage (import "${
-        super.fetchFromGitHub {
-          owner = "NixOS";
-          repo = "nixpkgs";
-          rev = "a98a48a016bb21140f2c2cda316e07a3ab94f4fb";
-          sha256 = "sha256-7CUD5UscK6J/Rd4YS/PYDMO7W+6oBUx9MQl2Z1fVhIw=";
-        }
-      }/pkgs/servers/adguardhome") { };
   })
   (_: super: {
     discord = super.callPackage ./discord.nix { inherit (super) discord; };
