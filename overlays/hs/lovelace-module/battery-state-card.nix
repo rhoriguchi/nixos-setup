ff519812a129f37d427f6d47d2784a2526d011e1
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2025-06-28 18:30:49 +0200

    Use finalAttrs pattern instead of rec

--- overlays/hs/lovelace-module/battery-state-card.nix
+++ overlays/hs/lovelace-module/battery-state-card.nix
@@ -1,12 +1,12 @@
 { stdenv, fetchurl }:
-stdenv.mkDerivation rec {
+stdenv.mkDerivation (finalAttrs: {
   pname = "hs-lovelace-module-battery-state-card";
   version = "3.2.1";
 
   src = let
     owner = "maxwroc";
     repo = "battery-state-card";
-    tag = "v${version}";
+    tag = "v${finalAttrs.version}";
     hash = "sha256-BGBecR099e2wbyEtlWvPDdXTiriMdXPlNqNHGpFkZRY=";
   in fetchurl {
     url = "https://github.com/${owner}/${repo}/releases/download/${tag}/battery-state-card.js";
@@ -17,6 +17,6 @@ stdenv.mkDerivation rec {
 
   installPhase = ''
     mkdir -p $out
-    cp $src $out/${pname}.js
+    cp $src $out/${finalAttrs.pname}.js
   '';
-}
+})
--- overlays/hs/lovelace-module/fold-entity-row.nix
+++ overlays/hs/lovelace-module/fold-entity-row.nix
@@ -1,17 +1,17 @@
 { stdenv, fetchFromGitHub }:
-stdenv.mkDerivation rec {
+stdenv.mkDerivation (finalAttrs: {
   pname = "hs-lovelace-module-fold-entity-row";
   version = "2.3.2";
 
   src = fetchFromGitHub {
     owner = "thomasloven";
     repo = "lovelace-fold-entity-row";
-    rev = "v.${version}";
+    rev = "v.${finalAttrs.version}";
     hash = "sha256-pbH2XNVrZEq3U9Kugq0X5U/CT9LNaWP9su4qWk6oob0=";
   };
 
   installPhase = ''
     mkdir -p $out
-    cp fold-entity-row.js $out/${pname}.js
+    cp fold-entity-row.js $out/${finalAttrs.pname}.js
   '';
-}
+})
