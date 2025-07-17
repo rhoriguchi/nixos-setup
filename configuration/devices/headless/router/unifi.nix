554fd8e7ef2fcc9b527ac95c5a1a8e573eae900b
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2025-07-17 23:30:38 +0200

    Enable snmp for unifi devices with netdata

--- configuration/devices/headless/router/unifi.nix
+++ configuration/devices/headless/router/unifi.nix
@@ -1,17 +1,28 @@
-{ pkgs, secrets, ... }: {
-  services.unpoller = {
-    enable = true;
+{ lib, pkgs, secrets, ... }: {
+  services = {
+    unpoller = {
+      enable = true;
 
-    unifi.defaults = {
-      user = "unifipoller";
-      pass = pkgs.writeText "unifipoller-password" secrets.unpoller.users.unifipoller.password;
+      unifi.defaults = {
+        user = "unifipoller";
+        pass = pkgs.writeText "unifipoller-password" secrets.unpoller.users.unifipoller.password;
 
-      url = "https://unifi.local";
-      verify_ssl = false;
+        url = "https://unifi.local";
+        verify_ssl = false;
+      };
+
+      influxdb.disable = true;
+
+      prometheus.http_listen = "127.0.0.1:9130";
     };
 
-    influxdb.disable = true;
-
-    prometheus.http_listen = "127.0.0.1:9130";
+    netdata.configDir."go.d/snmp.conf" = (pkgs.formats.yaml { }).generate "snmp.conf" {
+      jobs = map (name: {
+        inherit name;
+        hostname = lib.toLower "${lib.replaceStrings [ " " ] [ "" ] name}.local";
+        community = "public";
+        options.version = 2;
+      }) [ "Cabinet - U7 Pro" "Cabinet - USW Pro XG 8 PoE" "Kitchen - U6 LR" "Tv cabinet - US 8 60W" ];
+    };
   };
 }
