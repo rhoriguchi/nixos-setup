7a59296eed20335d9649cdbc5ca859a8b1a3ae53
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2025-08-06 00:05:46 -0300

    Add tier as hotspot router

--- modules/default/wireguard-network/ips.nix
+++ modules/default/wireguard-network/ips.nix
@@ -4,6 +4,7 @@
   XXLPitu-Router = "10.123.123.1";
 
   XXLPitu-Server = "10.123.123.2";
+  XXLPitu-Tier = "10.123.123.3";
   XXLPitu-Grimmjow = "10.123.123.4";
   XXLPitu-Ulquiorra = "10.123.123.5";
 
