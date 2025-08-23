fd8a3ee37beb30e1297ce681c8434d112756310c
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2025-08-23 18:56:44 +0200

    Make recovery easier in rescue shell

--- configuration/common.nix
+++ configuration/common.nix
@@ -1,5 +1,10 @@
 { config, lib, pkgs, ... }: {
-  boot.kernelPackages = lib.mkDefault pkgs.linuxPackages_latest;
+  boot = {
+    kernelPackages = lib.mkDefault pkgs.linuxPackages_latest;
+
+    # Enable root user in rescue shell
+    kernelParams = [ "systemd.setenv=SYSTEMD_SULOGIN_FORCE=1" ];
+  };
 
   time.timeZone = "Europe/Zurich";
 
