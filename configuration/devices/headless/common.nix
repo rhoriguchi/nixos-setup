5eca52a14451124e0eaf734d158eb91c9fd9a9a5
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2025-09-10 02:51:09 +0200

    Revert "Remove polkit agent from all headless devices"
    This reverts commit dfbfbb43fa20351de495babdf8a91e2aae350fb9.

--- configuration/devices/headless/common.nix
+++ configuration/devices/headless/common.nix
@@ -3,8 +3,6 @@
 
   documentation.enable = false;
 
-  security.polkit.enable = lib.mkForce false;
-
   networking.networkmanager = {
     ethernet.macAddress = "permanent";
     wifi.macAddress = "permanent";
