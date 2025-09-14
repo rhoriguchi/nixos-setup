ff90a6bfad7e375835b6370a881c97483833ae94
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2025-09-14 13:58:04 +0200

    Update flake.lock

--- overlays/default.nix
+++ overlays/default.nix
@@ -9,16 +9,6 @@
           sha256 = "sha256-jQNGd1Kmey15jq5U36m8pG+lVsxSJlDj1bJ167BjHQ4=";
         }
       }/pkgs/by-name/re/resilio-sync/package.nix") { };
-
-    # TODO remove when merged https://nixpk.gs/pr-tracker.html?pr=441598
-    tautulli = super.python3Packages.callPackage (import "${
-        super.fetchFromGitHub {
-          owner = "NixOS";
-          repo = "nixpkgs";
-          rev = "79a89d5f721a18a15fb99960674db45736b118c0";
-          sha256 = "sha256-i1W3KP7jFeU6DhDQoMpeR0xxMlLHo9NIY17snrvq/iA=";
-        }
-      }/pkgs/servers/tautulli") { };
   })
   (_: super: {
     discord = super.callPackage ./discord.nix { inherit (super) discord; };
