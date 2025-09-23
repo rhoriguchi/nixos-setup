e3af428613a6743c6090f7088461deb849e1a09e
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2025-09-23 14:32:10 +0200

    Add plex overlay

--- overlays/default.nix
+++ overlays/default.nix
@@ -9,6 +9,16 @@
           sha256 = "sha256-jQNGd1Kmey15jq5U36m8pG+lVsxSJlDj1bJ167BjHQ4=";
         }
       }/pkgs/by-name/re/resilio-sync/package.nix") { };
+
+    # TODO remove when merged https://nixpk.gs/pr-tracker.html?pr=445527
+    plexRaw = super.python3Packages.callPackage (import "${
+        super.fetchFromGitHub {
+          owner = "NixOS";
+          repo = "nixpkgs";
+          rev = "01374ab240719dab48d2eec7bab357c3f5b17cef";
+          sha256 = "sha256-cN8LdLctC59z/dzvqUlCCNIriRlfVw96mcSPR90ELXI=";
+        }
+      }/pkgs/servers/plex/raw.nix") { };
   })
   (_: super: {
     discord = super.callPackage ./discord.nix { inherit (super) discord; };
