9f75cfe022d211e5ff92187f47ed6524b27bc0ac
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2025-10-15 02:02:26 +0200

    Switch to hyprland

--- flake.nix
+++ flake.nix
@@ -36,6 +36,11 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
+    hyprland = {
+      url = "github:hyprwm/Hyprland?ref=v0.51.1";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
     nix-github-actions = {
       url = "github:nix-community/nix-github-actions";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -80,7 +85,9 @@
       nixosModules = {
         default.imports = [
           inputs.headplane.nixosModules.headplane
+          inputs.hyprland.nixosModules.default
           inputs.nix-minecraft.nixosModules.minecraft-servers
+
           ./modules/default
         ];
 
@@ -92,6 +99,10 @@
           ./modules/home-manager
         ];
         home-manager-gnome.imports = [ ./modules/home-manager-gnome ];
+        home-manager-hyprland.imports = [
+          inputs.hyprland.homeManagerModules.default
+          ./modules/home-manager-hyprland
+        ];
       };
 
       overlays = {
@@ -100,6 +111,7 @@
             inputs.deploy-rs.overlays.default
             inputs.firefox-addons.overlays.default
             inputs.headplane.overlays.default
+            inputs.hyprland.overlays.default
             inputs.nix-minecraft.overlay
 
             (_: super: { borg-exporter-image = inputs.borg-exporter.defaultPackage.${super.system}; })
@@ -142,7 +154,7 @@
                   inputs.nixos-hardware.nixosModules.dell-xps-13-9350
 
                   self.nixosModules.profiles.headful
-                  self.nixosModules.profiles.gnome
+                  self.nixosModules.profiles.hyprland
 
                   self.nixosModules.profiles.laptop-power-management
                   self.nixosModules.profiles.podman
@@ -166,7 +178,7 @@
 
                       users.rhoriguchi.imports = [
                         self.nixosModules.home-manager
-                        self.nixosModules.home-manager-gnome
+                        self.nixosModules.home-manager-hyprland
                       ];
                     };
                   }
