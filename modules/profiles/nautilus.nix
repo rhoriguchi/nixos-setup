bcd4705fbafe9689b3d13ff18b9e3974b5054eb9
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2025-11-19 04:07:13 +0100

    Update flake.lock

--- modules/profiles/nautilus.nix
+++ modules/profiles/nautilus.nix
@@ -6,28 +6,25 @@
     gvfs.enable = true;
   };
 
-  programs = {
-    file-roller.enable = true;
+  programs.dconf = {
+    enable = true;
 
-    dconf = {
-      enable = true;
-
-      profiles.user.databases = [
-        {
-          settings = {
-            "org/gnome/desktop/search-providers".sort-order = [ "org.gnome.Nautilus.desktop" ];
-            "org/gnome/nautilus/preferences".open-folder-on-dnd-hover = true;
-            "org/gtk/settings/file-chooser".show-hidden = true;
-          };
-        }
-      ];
-    };
+    profiles.user.databases = [
+      {
+        settings = {
+          "org/gnome/desktop/search-providers".sort-order = [ "org.gnome.Nautilus.desktop" ];
+          "org/gnome/nautilus/preferences".open-folder-on-dnd-hover = true;
+          "org/gtk/settings/file-chooser".show-hidden = true;
+        };
+      }
+    ];
   };
 
   environment = {
     sessionVariables.NAUTILUS_4_EXTENSION_DIR = "${config.system.path}/lib/nautilus/extensions-4";
 
     systemPackages = [
+      pkgs.file-roller
       pkgs.ffmpegthumbnailer
       pkgs.gnutar
       pkgs.nautilus
