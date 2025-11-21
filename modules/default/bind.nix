626f841a665e42149e132487644564de3ac63553
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2025-11-21 01:28:49 +0100

    Update nixos/bind overlay

--- modules/default/bind.nix
+++ modules/default/bind.nix
@@ -6,8 +6,8 @@ let
     let
       owner = "NixOS";
       repo = "nixpkgs";
-      rev = "33eef88f24766c52b72a5f5709c0bf3f24e244db";
-      sha256 = "sha256:18r19ivh73gx5lqc7d2373vcysqzxyhjbmha8gw2k62064zfqxyy";
+      rev = "14a5b3acfffaca73e4045c625fb1399128366e43";
+      sha256 = "sha256:11vsvh7pfns1rlgpic73a599r60r5jp6sj2q0hzq0ckzj98gf20p";
     in
     builtins.fetchTarball {
       name = "nixpkgs";
