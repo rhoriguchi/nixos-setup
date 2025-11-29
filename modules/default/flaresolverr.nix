64fb5c7b5c606ea33087bf58c10535e9fb864d17
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2025-11-29 04:14:43 +0100

    Upgrade flaresolverr

--- modules/default/flaresolverr.nix
+++ modules/default/flaresolverr.nix
@@ -38,7 +38,7 @@ in
   config = lib.mkIf cfg.enable {
     # https://hub.docker.com/r/flaresolverr/flaresolverr
     virtualisation.oci-containers.containers.flaresolverr = {
-      image = "flaresolverr/flaresolverr:v3.4.5";
+      image = "flaresolverr/flaresolverr:v3.4.6";
 
       ports = [
         "127.0.0.1:${toString cfg.port}:8191"
