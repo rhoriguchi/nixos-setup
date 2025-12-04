f4233b1725098ad4324114cdb8c552be7aaf22ab
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2025-12-04 01:12:23 +0100

    Add some aliases for nix-output-monitor

--- modules/home-manager/nix-shell.nix
+++ modules/home-manager/nix-shell.nix
@@ -1,11 +1,27 @@
+{ config, ... }:
+let
+  package = config.programs.nix-your-shell.nix-output-monitor.package;
+in
 {
-  programs.nix-your-shell = {
-    enable = true;
+  programs = {
+    nix-your-shell = {
+      enable = true;
 
-    enableFishIntegration = true;
-    enableNushellIntegration = true;
-    enableZshIntegration = true;
+      enableFishIntegration = true;
+      enableNushellIntegration = true;
+      enableZshIntegration = true;
 
-    nix-output-monitor.enable = true;
+      nix-output-monitor.enable = true;
+    };
+
+    zsh.shellAliases = {
+      "nix build" = "${package}/bin/nom build";
+      "nix develop" = "${package}/bin/nom develop";
+      nix-build = "${package}/bin/nom-build";
+
+      # TODO commented interferes with nix-your-shell
+      # "nix shell" = "${package}/bin/nom shell";
+      # nix-shell = "${package}/bin/nom-shell";
+    };
   };
 }
