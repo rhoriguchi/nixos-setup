81c98a3e7e66ecf9416f5df33a93809ad0f4d4e5
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2026-01-08 01:02:33 +0100

    Add repl aliases

---
+++ modules/home-manager/repl.nix
@@ -0,0 +1,15 @@
+{
+  config,
+  osConfig,
+  ...
+}:
+let
+  homeDirectory = config.home.homeDirectory;
+in
+{
+  programs.zsh.shellAliases = {
+    "repl flake" =
+      ''${osConfig.system.build.nixos-rebuild}/bin/nixos-rebuild repl --flake "${homeDirectory}/Sync/Git/nixos-setup"'';
+    "repl pkgs" = ''${config.nix.package}/bin/nix repl --expr "import <nixpkgs> {}"'';
+  };
+}
