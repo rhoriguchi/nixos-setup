502686db17e60aae0d579be039b7ebed388c177f
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2026-01-10 16:10:07 +0100

    Enable alloy live debugging

--- modules/profiles/alloy.nix
+++ modules/profiles/alloy.nix
@@ -1,3 +1,16 @@
 {
+  config,
+  lib,
+  ...
+}:
+{
   services.alloy.extraFlags = [ "--disable-reporting" ];
+
+  environment.etc = lib.optionalAttrs config.services.alloy.enable {
+    "alloy/debug.alloy".text = ''
+      livedebugging {
+        enabled = true
+      }
+    '';
+  };
 }
