bfb30f55afafb258a2e5089bbcbd8e34650cdc83
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2026-01-14 22:03:08 +0100

    Scrap sonarr logs with alloy

---
+++ modules/default/sonarr.nix
@@ -0,0 +1,64 @@
+{ config, lib, ... }:
+let
+  cfg = config.services.sonarr;
+in
+{
+  config = lib.mkIf cfg.enable {
+    systemd.services.alloy.serviceConfig = {
+      SupplementaryGroups = [ cfg.user ];
+      BindReadOnlyPaths = [
+        "${cfg.dataDir}/logs"
+      ];
+    };
+
+    environment.etc = lib.optionalAttrs config.services.alloy.enable {
+      "alloy/loki.sonarr.alloy".text = ''
+        loki.relabel "sonarr" {
+          forward_to = [loki.relabel.default.receiver]
+
+          rule {
+            target_label = "job"
+            replacement = "sonarr"
+          }
+
+          rule {
+            action = "labeldrop"
+            regex = "filename"
+          }
+        }
+
+        loki.process "sonarr" {
+          forward_to = [loki.relabel.sonarr.receiver]
+
+          stage.regex {
+            expression = "${
+              lib.escape [
+                "\\"
+              ] ''^\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}\.\d\|(?P<severity>[[:alpha:]]+)|''
+            }"
+          }
+
+          stage.template {
+            source = "severity"
+            template = "{{ ToLower .severity }}"
+          }
+
+          stage.labels {
+            values = {
+              severity = "",
+            }
+          }
+        }
+
+        loki.source.file "sonarr" {
+          targets = [
+            {__path__ = "${cfg.dataDir}/logs/sonarr.txt"},
+            {__path__ = "${cfg.dataDir}/logs/sonarr.debug.txt"},
+          ]
+
+          forward_to = [loki.process.sonarr.receiver]
+        }
+      '';
+    };
+  };
+}
