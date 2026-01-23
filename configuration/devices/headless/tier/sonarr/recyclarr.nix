fd6a073e2b53819210940449b5cbb9efbc6ec449
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2026-01-23 22:31:46 +0100

    Recyclarr add anime templates

--- configuration/devices/headless/tier/sonarr/recyclarr.nix
+++ configuration/devices/headless/tier/sonarr/recyclarr.nix
@@ -1,25 +1,48 @@
-{ config, secrets, ... }:
+{
+  config,
+  lib,
+  secrets,
+  ...
+}:
 let
-  createConfig = type: {
-    base_url = "http://${
-      config.containers."sonarr-${type}".localAddress
-    }:${toString config.services.sonarr.settings.server.port}/${type}";
-    api_key = secrets.sonarr.apiKey;
+  createConfig =
+    type:
+    {
+      base_url = "http://${
+        config.containers."sonarr-${type}".localAddress
+      }:${toString config.services.sonarr.settings.server.port}/${type}";
+      api_key = secrets.sonarr.apiKey;
 
-    media_naming = {
-      series = "default";
-      season = "default";
-      episodes = {
-        rename = true;
-        standard = "default";
-        daily = "default";
-        anime = "default";
+      media_naming = {
+        series = "default";
+        season = "default";
+        episodes = {
+          rename = true;
+          standard = "default";
+          daily = "default";
+          anime = "default";
+        };
       };
-    };
 
-    # > recyclarr list qualities sonarr
-    quality_definition.type = "${type}";
-  };
+      # > recyclarr list qualities sonarr
+      quality_definition.type = "${type}";
+
+      delete_old_custom_formats = true;
+    }
+    // lib.optionalAttrs (type == "anime") {
+      # > recyclarr config list templates
+      include = [
+        {
+          template = "sonarr-quality-definition-anime";
+        }
+        {
+          template = "sonarr-v4-custom-formats-anime";
+        }
+        {
+          template = "sonarr-v4-quality-profile-anime";
+        }
+      ];
+    };
 in
 {
   services.recyclarr = {
