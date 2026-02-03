74e5741b36318b780781e4921d56361f15de8b47
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2026-02-03 17:02:45 +0100

    Remove prismlauncher

--- modules/profiles/gaming.nix
+++ modules/profiles/gaming.nix
@@ -1,12 +1,7 @@
-{ pkgs, ... }:
 {
   programs.steam = {
     enable = true;
     localNetworkGameTransfers.openFirewall = true;
     remotePlay.openFirewall = true;
   };
-
-  environment.systemPackages = [
-    pkgs.prismlauncher # Minecraft
-  ];
 }
