fb3ae9ac1708aeb2db2b239dad39d06225891848
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2026-02-09 01:43:01 +0100

    Upgrade card-mod

--- overlays/hs/lovelace-module/card-mod.nix
+++ overlays/hs/lovelace-module/card-mod.nix
@@ -1,13 +1,13 @@
 { stdenv, fetchFromGitHub }:
 stdenv.mkDerivation (finalAttrs: {
   pname = "hs-lovelace-module-card-mod";
-  version = "4.2.0";
+  version = "4.2.1";
 
   src = fetchFromGitHub {
     owner = "thomasloven";
     repo = "lovelace-card-mod";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-Dvm2i8ll7Fyuw/+7+3a50HJAmWF4PoxnyPcWExP47e8=";
+    hash = "sha256-16aA9VaSuWZ1i9UBGp9vb7N1H5vfcLOiqppjPoqphM0=";
   };
 
   installPhase = ''
