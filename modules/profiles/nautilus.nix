26f53a05645891d1c3a6d6fa5a931fbdee3099a4
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2026-02-11 13:39:42 +0100

    Remove unneeded env variable

--- modules/profiles/nautilus.nix
+++ modules/profiles/nautilus.nix
@@ -1,9 +1,4 @@
-{
-  config,
-  lib,
-  pkgs,
-  ...
-}:
+{ lib, pkgs, ... }:
 {
   services = {
     gnome.sushi.enable = true;
@@ -26,17 +21,13 @@
   };
 
   environment = {
-    sessionVariables = {
-      NAUTILUS_4_EXTENSION_DIR = "${config.system.path}/lib/nautilus/extensions-4";
-
-      # Required for audio / video information https://github.com/NixOS/nixpkgs/issues/53631
-      GST_PLUGIN_SYSTEM_PATH_1_0 = lib.makeSearchPathOutput "lib" "lib/gstreamer-1.0" [
-        pkgs.gst_all_1.gst-plugins-good
-        pkgs.gst_all_1.gst-plugins-bad
-        pkgs.gst_all_1.gst-plugins-ugly
-        pkgs.gst_all_1.gst-libav
-      ];
-    };
+    # Required for audio / video information https://github.com/NixOS/nixpkgs/issues/53631
+    sessionVariables.GST_PLUGIN_SYSTEM_PATH_1_0 = lib.makeSearchPathOutput "lib" "lib/gstreamer-1.0" [
+      pkgs.gst_all_1.gst-plugins-good
+      pkgs.gst_all_1.gst-plugins-bad
+      pkgs.gst_all_1.gst-plugins-ugly
+      pkgs.gst_all_1.gst-libav
+    ];
 
     systemPackages = [
       pkgs.file-roller
