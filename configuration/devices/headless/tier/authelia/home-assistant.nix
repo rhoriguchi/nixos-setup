241f8343d37a325cb2d28e91bb6163429c1c74c3
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2026-02-14 01:03:51 +0100

    Add an authorization policy to authelia for oidc for home assistant

--- configuration/devices/headless/tier/authelia/home-assistant.nix
+++ configuration/devices/headless/tier/authelia/home-assistant.nix
@@ -1,20 +1,38 @@
 {
   # https://github.com/christiaangoossens/hass-oidc-auth/blob/main/docs/provider-configurations/authelia.md
-  services.authelia.instances.main.settings.identity_providers.oidc.clients = [
-    {
-      client_id = "home-assistant";
-      client_name = "Home Assistant";
-      public = true;
-      require_pkce = true;
-      pkce_challenge_method = "S256";
-      authorization_policy = "one_factor";
-      redirect_uris = [ "https://home-assistant.00a.ch/auth/oidc/callback" ];
-      scopes = [
-        "openid"
-        "profile"
-        "groups"
+  services.authelia.instances.main.settings.identity_providers.oidc = {
+    authorization_policies.home-assistant = {
+      default_policy = "deny";
+
+      rules = [
+        {
+          policy = "one_factor";
+          subject = [
+            "group:admin"
+
+            "group:home-assistant"
+          ];
+        }
       ];
-      id_token_signed_response_alg = "RS256";
-    }
-  ];
+    };
+
+    clients = [
+      {
+        client_id = "home-assistant";
+        client_name = "Home Assistant";
+        public = true;
+        require_pkce = true;
+        pkce_challenge_method = "S256";
+        redirect_uris = [ "https://home-assistant.00a.ch/auth/oidc/callback" ];
+        scopes = [
+          "openid"
+          "profile"
+          "groups"
+        ];
+        id_token_signed_response_alg = "RS256";
+
+        authorization_policy = "home-assistant";
+      }
+    ];
+  };
 }
