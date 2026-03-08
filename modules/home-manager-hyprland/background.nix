250ad569248369195428df201e05967a3cf9f5bc
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2026-03-08 20:07:21 +0100

    Hyprpaper remove splash text

--- modules/home-manager-hyprland/background.nix
+++ modules/home-manager-hyprland/background.nix
@@ -3,11 +3,15 @@
   services.hyprpaper = {
     enable = true;
 
-    settings.wallpaper = [
-      {
-        monitor = "";
-        path = "${pkgs.wallpaper}/share/icons/hicolor/3840x2160/apps/wallpaper.jpg";
-      }
-    ];
+    settings = {
+      splash = false;
+
+      wallpaper = [
+        {
+          monitor = "";
+          path = "${pkgs.wallpaper}/share/icons/hicolor/3840x2160/apps/wallpaper.jpg";
+        }
+      ];
+    };
   };
 }
