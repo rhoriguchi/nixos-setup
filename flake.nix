b9895c9f4047ba504a267d4b206331b3c0343211
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2026-03-28 00:16:47 +0100

    Update hyprland

--- flake.nix
+++ flake.nix
@@ -32,7 +32,7 @@
     };
 
     hyprland = {
-      url = "github:hyprwm/Hyprland?ref=v0.54.2";
+      url = "github:hyprwm/Hyprland?ref=v0.54.3";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
