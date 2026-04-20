ca3ada7a447966a4824e5a1ce8ce138a773b9092
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2026-04-20 19:06:59 +0200

    Update netdata overlay

--- overlays/default.nix
+++ overlays/default.nix
@@ -35,8 +35,8 @@
       super.fetchFromGitHub {
         owner = "NixOS";
         repo = "nixpkgs";
-        rev = "a8e5651c66e8a0f715a7e44fb9828419f618357f";
-        sha256 = "sha256-zxMB0wph2pHtmjeM8sY9UafLmlLVVLrfOkN7cCLYPT8=";
+        rev = "2a682cc0cafc12542fee6a79a1dd7f32005feac5";
+        sha256 = "sha256-iNSn5p2XwIL2dkP1uV/xxIHcmIiVThoNSAkKKpFiDOc=";
       }
     }/pkgs/tools/system/netdata") { protobuf = super.protobuf_21; };
 
