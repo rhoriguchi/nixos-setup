7d1aef19eed5c4f029105a4ad3a011e212e40a97
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2026-04-21 14:51:10 +0200

    Add state field to fitness park scraper

--- configuration/devices/headless/tier/fitnesspark.nix
+++ configuration/devices/headless/tier/fitnesspark.nix
@@ -10,88 +10,105 @@ let
   locations = {
     "508" = {
       locationId = "29";
+      state = "Zurich";
       name = "Puls5";
-      displayName = "Zürich Puls 5";
+      displayName = "Zurich Puls 5";
     };
     "680" = {
       locationId = "57";
+      state = "Aargau";
       name = "Trafo_Baden";
       displayName = "Baden Trafo";
     };
     "682" = {
       locationId = "3";
+      state = "Basel";
       name = "Basel_Heuwaage";
       displayName = "Basel Heuwaage";
     };
     "684" = {
       locationId = "4";
+      state = "Bern";
       name = "Oberhofen Haupteingang";
-      displayName = "Oberhofen Hallenbad";
+      displayName = "Oberhofen indoor pool centre";
     };
     "686" = {
       locationId = "59";
+      state = "Bern";
       name = "Time-Out";
       displayName = "Ostermundigen Time-Out";
     };
     "690" = {
       locationId = "54";
+      state = "Lucerne";
       name = "National_Luzern";
-      displayName = "Luzern National";
+      displayName = "Lucerne National";
     };
     "694" = {
       locationId = "52";
+      state = "Zug";
       name = "Eichstaette_Zug";
       displayName = "Zug Eichstätte";
     };
     "696" = {
       locationId = "6";
+      state = "Zurich";
       name = "Winterthur";
       displayName = "Winterthur";
     };
     "698" = {
       locationId = "31";
+      state = "Zurich";
       name = "Glattpark";
-      displayName = "Zürich Glattpark";
+      displayName = "Zurich Glattpark";
     };
     "700" = {
       locationId = "1";
+      state = "Zurich";
       name = "Regensdorf";
       displayName = "Regensdorf";
     };
     "702" = {
       locationId = "30";
+      state = "Zurich";
       name = "Stadelhofen";
-      displayName = "Zürich Stadelhofen";
+      displayName = "Zurich Stadelhofen";
     };
     "704" = {
       locationId = "34";
+      state = "Zurich";
       name = "Sihlcity";
-      displayName = "Zürich Sihlcity";
+      displayName = "Zurich Sihlcity";
     };
     "706" = {
       locationId = "32";
+      state = "Zurich";
       name = "Stockerhof";
-      displayName = "Zürich Stockerhof";
+      displayName = "Zurich Stockerhof";
     };
     "856" = {
       locationId = "105";
+      state = "Bern";
       name = "Bern_City";
       displayName = "Bern City";
     };
     "2934" = {
       locationId = "12";
+      state = "Zurich";
       name = "Milandia";
       displayName = "Greifensee Milandia";
     };
     "3014" = {
       locationId = "4";
+      state = "Bern";
       name = "Oberhofen Haupteingang";
       displayName = "Oberhofen";
     };
     "6778" = {
       locationId = "56";
+      state = "Lucerne";
       name = "Allmend_Luzern";
-      displayName = "Luzern Allmend";
+      displayName = "Lucerne Allmend";
     };
   };
 in
@@ -111,9 +128,10 @@ in
 
       get_visitors() {
         local park_id="$1"
-        local location_id="$2"
-        local name="$3"
-        local display_name="$4"
+        local state="$2"
+        local location_id="$3"
+        local name="$4"
+        local display_name="$5"
 
         value=$(${pkgs.curl}/bin/curl -s "https://www.fitnesspark.ch/wp/wp-admin/admin-ajax.php?action=single_park_update_visitors&park_id=$park_id&location_id=$location_id&location_name=FP_$name")
 
@@ -121,13 +139,13 @@ in
           value="-1"
         fi
 
-        echo "fitnesspark_visitors{id=\"$park_id\", name=\"$display_name\"} $value" >> ${metricsDir}/metrics.tmp
+        echo "fitnesspark_visitors{id=\"$park_id\", state=\"$state\", name=\"$display_name\"} $value" >> ${metricsDir}/metrics.tmp
       }
 
       ${lib.concatStringsSep "\n" (
         lib.mapAttrsToList (
           parkId: location:
-          ''get_visitors "${parkId}" "${location.locationId}" "${
+          ''get_visitors "${parkId}" "${lib.toLower location.state}" "${location.locationId}" "${
             lib.replaceStrings [ " " ] [ "%20" ] location.name
           }" "${location.displayName}"''
         ) locations
