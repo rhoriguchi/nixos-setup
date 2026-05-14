a3e612d76b5575e97f3bbcc67861b048615702c5
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2026-05-14 18:19:46 +0200

    Update recyclarr to work again

--- configuration/devices/headless/tier/arr/recyclarr.nix
+++ configuration/devices/headless/tier/arr/recyclarr.nix
@@ -32,17 +32,30 @@ let
       delete_old_custom_formats = true;
     }
     // lib.optionalAttrs (type == "anime") {
-      # https://github.com/recyclarr/config-templates/blob/master/sonarr/templates/anime-sonarr-v4.yml
-      include = [
-        {
-          template = "sonarr-quality-definition-anime";
-        }
-        {
-          template = "sonarr-v4-custom-formats-anime";
-        }
-        {
-          template = "sonarr-v4-quality-profile-anime";
-        }
+      # > recyclarr list custom-format-groups sonarr
+      custom_format_groups = {
+        add = [
+          # [Release Groups] Anime
+          {
+            trash_id = "f206572b1147d0221bb1c96765b349e8";
+            select_all = true;
+          }
+          # [Streaming Services] Anime
+          {
+            trash_id = "f54985e5e96747cef58731f1cf4c9181";
+            select_all = true;
+          }
+        ];
+
+        skip = [
+          "6f0872eebfc95b1f93474b7ac866ced0" # [Unwanted] Unwanted Formats German
+          "a23c8675c79118544fd74153394fa589" # [Unwanted] Unwanted Formats French
+        ];
+      };
+
+      # > recyclarr list quality-profiles sonarr
+      quality_profiles = [
+        { trash_id = "20e0fc959f1f1704bed501f23bdae76f"; } # [Anime] Remux-1080p
       ];
     };
 in
