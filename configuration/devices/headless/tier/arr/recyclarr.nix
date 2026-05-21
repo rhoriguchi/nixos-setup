025e243cb1ff3ee92b437eca8f8940a59060bcc3
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2026-05-21 23:53:06 +0200

    Improve recyclarr config

--- configuration/devices/headless/tier/arr/recyclarr.nix
+++ configuration/devices/headless/tier/arr/recyclarr.nix
@@ -5,6 +5,19 @@
   ...
 }:
 let
+  createCustomFormatGroup =
+    customFormatGroup:
+    map (trashId: {
+      trash_id = trashId;
+      select_all = true;
+    }) customFormatGroup;
+
+  sonarrCommonCustomFormatGroups = [
+    "59c3af66780d08332fdc64e68297098f" # [Unwanted] Unwanted Formats
+    "6f0872eebfc95b1f93474b7ac866ced0" # [Unwanted] Unwanted Formats German
+    "a23c8675c79118544fd74153394fa589" # [Unwanted] Unwanted Formats French
+  ];
+
   createConfig =
     type:
     {
@@ -33,30 +46,28 @@ let
     }
     // lib.optionalAttrs (type == "anime") {
       # > recyclarr list custom-format-groups sonarr
-      custom_format_groups = {
-        add = [
-          # [Release Groups] Anime
-          {
-            trash_id = "f206572b1147d0221bb1c96765b349e8";
-            select_all = true;
-          }
-          # [Streaming Services] Anime
-          {
-            trash_id = "f54985e5e96747cef58731f1cf4c9181";
-            select_all = true;
-          }
-        ];
-
-        skip = [
-          "6f0872eebfc95b1f93474b7ac866ced0" # [Unwanted] Unwanted Formats German
-          "a23c8675c79118544fd74153394fa589" # [Unwanted] Unwanted Formats French
-        ];
-      };
+      custom_format_groups.add = createCustomFormatGroup (
+        [
+          "f206572b1147d0221bb1c96765b349e8" # [Release Groups] Anime
+          "f54985e5e96747cef58731f1cf4c9181" # [Streaming Services] Anime
+        ]
+        ++ sonarrCommonCustomFormatGroups
+      );
 
       # > recyclarr list quality-profiles sonarr
       quality_profiles = [
         { trash_id = "20e0fc959f1f1704bed501f23bdae76f"; } # [Anime] Remux-1080p
       ];
+    }
+    // lib.optionalAttrs (type == "series") {
+      # > recyclarr list custom-format-groups sonarr
+      custom_format_groups.add = createCustomFormatGroup (
+        [
+          "b4a4353e3b3e6789dbef07677ff23686" # [Release Groups] HQ
+          "abe720fab2d27682adc2a735136cec02" # [Streaming Services] General
+        ]
+        ++ sonarrCommonCustomFormatGroups
+      );
     };
 in
 {
