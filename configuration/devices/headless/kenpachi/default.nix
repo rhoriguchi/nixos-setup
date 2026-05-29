39b7da9924bc6a85edde4ee231f0e9ac70253552
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2026-05-30 00:39:46 +0200

    Turn of wifi on kenpachi

--- configuration/devices/headless/kenpachi/default.nix
+++ configuration/devices/headless/kenpachi/default.nix
@@ -1,4 +1,3 @@
-{ secrets, ... }:
 {
   imports = [
     ../common.nix
@@ -13,17 +12,6 @@
 
   networking = {
     hostName = "XXLPitu-Kenpachi";
-
-    # TODO remove when no more at home
-    wireless = {
-      enable = true;
-
-      extraConfig = ''
-        p2p_disabled=1
-      '';
-
-      networks."63466727-IoT" = secrets.wifis."63466727-IoT";
-    };
 
     nameservers = [
       "1.1.1.1"
