61be729abd2b70acf92ee567af75846463fceebc
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2026-06-19 18:57:06 +0200

    Add tautulli overlay

--- overlays/default.nix
+++ overlays/default.nix
@@ -19,6 +19,16 @@
         sha256 = "sha256-XyfwZpc79a+uqKx41bzCvK0UKJWlGkYqtqhAKPZbDus=";
       }
     }/pkgs/by-name/ga/gamedig/package.nix") { };
+
+    # TODO remove when merged https://nixpkgs-tracker.ocfox.me/?pr=533353
+    tautulli = prev.callPackage (import "${
+      prev.fetchFromGitHub {
+        owner = "NixOS";
+        repo = "nixpkgs";
+        rev = "8a7b4affbe737688bfc6e1283b729a848fa770b3";
+        sha256 = "sha256-Yd9rQV9+atnCCDDHFuA0IDrIXQHI01fcvI0YfdmKExU=";
+      }
+    }/pkgs/by-name/ta/tautulli/package.nix") { };
   })
   (_: prev: {
     hs = prev.callPackage ./hs { };
