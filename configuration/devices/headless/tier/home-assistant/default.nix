e312c2820b2f2930e90a422a6588aaf914b499be
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2026-07-26 22:39:13 +0200

    Remove permittedInsecurePackages for hs matter

--- configuration/devices/headless/tier/home-assistant/default.nix
+++ configuration/devices/headless/tier/home-assistant/default.nix
@@ -8,9 +8,6 @@
 {
   imports = libCustom.getImports ./.;
 
-  # TODO remove when https://github.com/project-chip/connectedhomeip/issues/25688 fixed
-  nixpkgs.config.permittedInsecurePackages = [ "openssl-1.1.1w" ];
-
   services.home-assistant = {
     enable = true;
 
