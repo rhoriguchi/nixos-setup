8f2654f782f393b92e7afa71b18805ac7fd5851b
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2026-08-29 19:34:19 +0200

    Pin repl-pkgs alias to flake nixpkgs input

--- modules/home-manager/repl.nix
+++ modules/home-manager/repl.nix
@@ -3,6 +3,7 @@
   programs.zsh.shellAliases = {
     "repl-flake" =
       ''${osConfig.system.build.nixos-rebuild}/bin/nixos-rebuild repl --flake "${config.home.homeDirectory}/Sync/Git/nixos-setup"'';
-    "repl-pkgs" = ''${config.nix.package}/bin/nix repl --expr "import <nixpkgs> {}"'';
+    "repl-pkgs" =
+      ''${config.nix.package}/bin/nix repl --expr "import (builtins.getFlake \"${config.home.homeDirectory}/Sync/Git/nixos-setup\").inputs.nixpkgs {}"'';
   };
 }
