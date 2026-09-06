9eab2432d3c7f00d3017a8493a79d29d0f85a02f
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2026-09-06 04:20:45 +0200

    Use journald log-driver for docker

--- modules/profiles/docker.nix
+++ modules/profiles/docker.nix
@@ -11,11 +11,7 @@
     enable = true;
 
     storageDriver = "overlay2";
-    logDriver = "json-file";
-    extraOptions = lib.concatStringsSep " " [
-      "--log-opt max-file=10"
-      "--log-opt max-size=10m"
-    ];
+    logDriver = "journald";
   };
 
   environment.systemPackages = [ pkgs.docker-compose ];
