aee9fd8f710ea12e07156a6f0c3b0c0c06dfafca
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2026-09-15 15:18:39 +0200

    Use upstream flashrom overlay

--- overlays/default.nix
+++ overlays/default.nix
@@ -20,33 +20,15 @@
       }
     }/pkgs/by-name/ga/gamedig/package.nix") { };
 
-    # TODO remove when fixed upstream https://github.com/NixOS/nixpkgs/issues/558302
-    flashrom = prev.flashrom.overrideAttrs (old: {
-      patches = (old.patches or [ ]) ++ [
-        # tests/chip.c: setup_bad_chip() stores a pointer to a stack-local
-        # `mock_chip` into flashctx->chip; it dangles once the helper
-        # returns, corrupting later stack allocations and causing spurious
-        # test failures on aarch64-linux.
-        (prev.writeText "flashrom-fix-dangling-chip-bad-pointer.patch" (
-          prev.lib.concatStringsSep "\n" [
-            "--- a/tests/chip.c"
-            "+++ b/tests/chip.c"
-            "@@ -781,7 +781,8 @@ static void setup_bad_chip(struct flashrom_flashctx *flashctx)"
-            " \tg_test_write_injector = NULL;"
-            " \tg_test_read_injector = NULL;"
-            " \tg_test_erase_injector[0] = NULL;"
-            " "
-            "-\tstruct flashchip mock_chip = chip_bad;"
-            "+\tstatic struct flashchip mock_chip;"
-            "+\tmock_chip = chip_bad;"
-            " \tconst char *param = \"\"; /* Default values for all params. */"
-            " "
-            " \tsetup_chip(flashctx, &mock_chip, param, NULL);"
-            ""
-          ]
-        ))
-      ];
-    });
+    # TODO remove when merged https://github.com/NixOS/nixpkgs/pull/563332
+    flashrom = prev.callPackage (import "${
+      prev.fetchFromGitHub {
+        owner = "ruiiiijiiiiang";
+        repo = "nixpkgs";
+        rev = "45cad503a7a0873ff8fa0542d749b495631808b0";
+        sha256 = "sha256-kXAWCXGTwQsxMqm2OGyXjmBDAqCLZbcKZbiLKuMWzm4=";
+      }
+    }/pkgs/by-name/fl/flashrom/package.nix") { };
   })
 
   # TODO remove when resolved
